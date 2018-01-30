cae56e82efe5be48fd6e5fb436cd73881bf53d9e
sarikaricky <sarikaresume@gmail.com>
2018-01-30 10:40:40 -0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-/module.exports = {
+module.exports = {
   development: {
     client: 'sqlite3',
     connection: { filename: './database/blogdb.sqlite3' }, // change this if you want a different name for the database
