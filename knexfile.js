2748efbc2d26911ea1e697afd8f569aa266b2ea2
Mark Marchant <jtla3@yahoo.com>
2018-01-29 16:37:05 -0800

    new commit

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     connection: { filename: './database/blogdb.sqlite3' }, // change this if you want a different name for the database
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
-      directory: './database/migrations',
+      directory: './/migrations',
       tableName: 'dbmigrations',
     },
     seeds: { directory: './database/seeds' },
