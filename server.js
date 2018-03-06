390c213439e80913c50edeaebca83ccfffd7eac6
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-03-06 17:47:36 -0500

    Functional Users endpoints

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(bodyParser.json());
+
+server.get('/', (req, res) => {
+  res.status(200).send({ api: 'running...' });
+})
+
+server.use('/users', userRouter);
 
 const port = 3333;
 server.listen(port, function() {
