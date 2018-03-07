400755d8439d77a6e008dde726606f11f2b133a6
Samuel Cha <sunchipssam@gmail.com>
2018-03-07 13:51:18 -0800

    Switched pos of select

--- db/knex.js
+++ db/knex.js
@@ -33,10 +33,10 @@ module.exports = {
 
   join3_where_select: (ref, tbl1, tbl2, tbl3, cond, sel) => {
     return knex(ref.tbl)
-      .select(sel)
       .join(tbl1.id, ref.col1, '=', tbl1.col)
       .join(tbl2.id, ref.col2, '=', tbl2.col)
       .join(tbl3.id, ref.col3, '=', tbl3.col)
-      .where(cond);
+      .where(cond)
+      .select(sel);
   },
 };
