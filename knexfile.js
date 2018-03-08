1e62646fadf61d720873c32731f76d3d948ba03b
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-03-07 19:02:51 -0500

    Changed knexfile.js to implement sqlite3 as development, changed MySQL to production

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,22 @@
 // Update with your config settings.
 
 module.exports = {
-	development: {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/db.sqlite3'
+    },
+    migrations: {
+			directory: "./database/migrations",
+			tableName: "knex_migrations",
+		},
+		seeds: {
+			directory: "./database/seeds",
+    },
+    useNullAsDefault: true
+  },
+
+	production: {
 		client: "MySQL",
 		connection: {
 			database: "friendBook",
