1458d08bba44a8e994dc2f49d100e453d7c0eceb
Julian Kohlman <juliankohlman@gmail.com>
2018-03-07 21:57:10 -0500

    updated users/get/:id route

--- users/userController.js
+++ users/userController.js
@@ -1,24 +1,24 @@
 const knex = require('../database/dbConfig');
 
 const user_db = {
-    getAll: function() {
-        return knex('users');
-    }, 
-    getID: function(id) {
-        return knex('users').where({ id });
-    },
-    addUser: function(user) {
-        return knex.insert(user).into('users');
-    },
-    updateUser: function(id, user) {
-        return knex('users').where({ id }).update(user);
-    },
-    eraseUser: function(id) {
-        return knex('users').where({ id }).del();
-    }, 
-    getPostByID: function(UserId) {
-        return knex('posts').where('userId', id);
-    }
+  getAll: function() {
+    return knex('users');
+  },
+  getID: function(id) {
+    return knex('users').where('userId', id);
+  },
+  addUser: function(user) {
+    return knex.insert(user).into('users');
+  },
+  updateUser: function(id, user) {
+    return knex('users').where({ id }).update(user);
+  },
+  eraseUser: function(id) {
+    return knex('users').where({ id }).del();
+  },
+  getPostByID: function(UserId) {
+    return knex('posts').where('userId', id);
+  }
 }
 
 module.exports = user_db;
--- users/userRouter.js
+++ users/userRouter.js
@@ -5,79 +5,80 @@ const user_db = require('./userController.js');
 const userRouter = express.Router()
 
 userRouter.post('/', (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    user_db.addUser(user)
-        .then((ids) => {
-            res.status(201).json(ids);
-        })
-        .catch((error) => {
-            res.status(500).json({ message: 'Could not add a User' })
-        })
+  user_db.addUser(user)
+    .then((ids) => {
+      res.status(201).json(ids);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Could not add a User' })
+    })
 
 });
 
 userRouter.get('/', (req, res) => {
-    user_db.getAll()
-        .then((users) => {
-            res.status(200).json(users);
-        })
-        .catch((error) => {
-            res.status(500).json({ message: 'Could not get Users' });
-        });
+  user_db.getAll()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Could not get Users' });
+    });
 });
 
 userRouter.get('/:id', (req, res) => {
-    const id = req.params.id;
-    user_db.getID(id)
-        .then((user) => {
-            user.length > 0 ? res.status(200).json(user) : res.status(404).json({ message: `User with id ${id} does not exist` });
-        })
-        .catch((error) => {
-            res.status(500).json({ message: 'Error finding the user' });
-        });
+  const userId = req.params.id;
+  console.log(userId);
+  user_db.getID(userId)
+    .then((user) => {
+      user.length > 0 ? res.status(200).json(user) : res.status(404).json({ message: `User with id ${userId} does not exist` });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error finding the user' });
+    });
 });
 
 userRouter.get('/:id/posts', (req, res) => {
-    const { id } = req.params;
-    user_db.getPostByID(id)
-        .then((posts) => {
-            user.length > 0 ? res.status(200).json(posts) : res.status(404).json({ message: `Posts connected with this id ${id} do not exist` });
-        })
-        .catch((error) => {
-            res.status(500).json({ message: 'Error finding the posts' });
-        })
+  const { id } = req.params;
+  user_db.getPostByID(id)
+    .then((posts) => {
+      user.length > 0 ? res.status(200).json(posts) : res.status(404).json({ message: `Posts connected with this id ${id} do not exist` });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error finding the posts' });
+    })
 });
 
 userRouter.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const  user  = req.body;
-    user_db.updateUser(id, user)
-        .then((user) => {
-            if (!user) {
-                res.status(404).json({ messsage: `Unable to update user by this id ${id}` });
-            } else {
-                res.status(201).json(user);
-            }
-        })
-        .catch((error) => {
-            res.status(500).json({ message: 'Error updating User' });
-        });
+  const { id } = req.params;
+  const user = req.body;
+  user_db.updateUser(id, user)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ messsage: `Unable to update user by this id ${id}` });
+      } else {
+        res.status(201).json(user);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error updating User' });
+    });
 });
 
 userRouter.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    user_db.eraseUser(id)
-        .then((user) => {
-            if (!user) {
-                res.status(404).json({ message: `Unable to delete user by this id ${id}` });
-            } else {
-                res.status(200).json(user);
-            }
-        })
-        .catch((error) => {
-            res.status(500).json({ message: 'Error deleting User' });
-        });
+  const { id } = req.params;
+  user_db.eraseUser(id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ message: `Unable to delete user by this id ${id}` });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error deleting User' });
+    });
 });
 
 module.exports = userRouter;
