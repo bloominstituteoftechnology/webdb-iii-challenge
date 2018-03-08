b9783255b5310d3d83737ffef9c350294b2d1ac6
U-Honolulu\Troy <me@bitcointroy.co>
2018-03-08 17:08:21 -0600

    Hello World, list of posts on the App page

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const bodyParser = require("body-parser");
 const userRouter = require("./users/userRouter");
 const postRouter = require("./posts/postRouter");
 const tagRouter = require("./tags/tagRouter");
+const cors = require("cors");
 
 const server = express();
 
 server.use(bodyParser.json());
+server.use(cors());
 
 server.get("/", (req, res) => {
-  res.status(200).send({ api: "running..." });
+	res.status(200).send({ api: "running..." });
 });
 
-server.use("/users", userRouter);
-server.use("/posts", postRouter);
-server.use("/tags", tagRouter);
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+server.use("/api/tags", tagRouter);
 
 const port = 3333;
 server.listen(port, function() {
-  console.log(`Server listening on ${port}`);
+	console.log(`Server listening on ${port}`);
 });
