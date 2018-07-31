af6567dae2eaba8b07c898aef8e567aa3e47fbda
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-07-31 17:30:04 -0400

    Recreated .sqlite3 file and GET requests working

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
 
+const db = require('./data/db');
+
 const server = express();
 
 server.use(express.json());
 
 // endpoints here
+
+server.get('/', (req, res) => {
+    res.send('up and running...')
+})
+
+server.get('/users', (req, res) => {
+    db('users')
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => res.status(500).json(err));
+})
+
+server.get('/posts', (req, res) => {
+    db('posts')
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => res.status(500).json(err));
+})
+
+server.get('/tags', (req, res) => {
+    db('tags')
+        .then(tags => {
+            res.status(200).json(tags);
+        })
+        .catch(err => res.status(500).json(err));
+})
 
 const port = 3333;
 server.listen(port, function() {
