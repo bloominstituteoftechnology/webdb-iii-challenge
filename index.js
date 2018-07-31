81d450fc27b0fb386d27cfd70b5f6410bcebfa8b
Alec <afroenygam@gmail.com>
2018-07-31 18:52:03 -0400

    CRUD for tags table updated

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ server.put('/users/:id', async (req, res) => {
 })
 //USERS CRUD END
 
+//POSTS CRUD START
 server.get('/posts', async (req, res) => {
   try {
     const posts = await db('posts');
@@ -86,7 +87,48 @@ server.put('/posts/:id', async (req, res) => {
     res.status(500).send(`Try again and ${err}`);
   }
 })
+//POSTS CRUD END
 
+//TAGS CRUD START
+server.get('/tags', async (req, res) => {
+  try {
+    const tags = await db('tags');
+    res.status(200).json(tags);
+  } catch(err) {
+    res.status(500).send(`You done goofed with ${err}`);
+  }
+});
 
+server.post('/tags', async (req,  res) => {
+  const tag = req.body;
+  try {
+    const ids = await db.insert(tag).into('tags');
+    const id = ids[0];
+    res.status(201).json({id, ...tag});
+  } catch(err) {
+    res.status(500).send(`Ya done goofed with ${err}`);
+  }
+})
+
+server.delete('/tags/:id', async (req, res) => {
+  try {
+    const result = await db('tags').where('id', req.params.id).del();
+    res.status(200).json(result);
+  } catch(err) {
+    res.status(500).send(`Try again and ${err}`);
+  }
+})
+
+server.put('/tags/:id', async (req, res) => {
+  const tag = req.body;
+  try {
+    const index = await db('tags').where('id', req.params.id).update(tag);
+    if (index > 0) return res.status(200).json(await db('tags').where('id', req.params.id));
+    res.status(200).send("If didn't trigger");
+  } catch(err) {
+    res.status(500).send(`Try again and ${err}`);
+  }
+})
+//TAGS CRUD END
 const port = 3000;
 server.listen(port, () => {console.log(`Server is listening on port ${port}`)})
