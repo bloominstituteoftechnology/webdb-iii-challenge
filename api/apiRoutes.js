4f0ce8e40db6c9d82e92441e439c3ef5d9c356e9
DirupT <iamcooled@gmail.com>
2018-07-31 15:03:16 -0500

    added posts to database

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoutes = require('./users/userRoutes');
+const postRoutes = require('./posts/postRoutes');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
