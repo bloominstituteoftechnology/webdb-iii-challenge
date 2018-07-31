2787255111962c5d8e9f55cc4a8a8e9c477dcf70
DirupT <iamcooled@gmail.com>
2018-07-31 15:18:55 -0500

    added get post by id to posts

--- api/posts/postRoutes.js
+++ api/posts/postRoutes.js
@@ -24,4 +24,14 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/:id', async (req, res, next) => {
+    try {
+        const response = await db('posts').where('id', Number(req.params.id)).first();
+        if (!response) return next({ code: 404, message: "The post with the specified ID does not exist." });
+        return res.status(200).json(response);
+    } catch (err) {
+        next({ code: 500, error: "The post information could not be retrieved." });
+    }
+})
+
 module.exports = router;
