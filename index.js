e39356a596db869eb31328b350e0560be8f3c508
Luis <luis@Luiss-MacBook-Air.local>
2018-07-31 13:11:10 -0700

    created endpoint to get users from DB

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
-
+const db = require('./data/db')
 const server = express()
 server.use(express.json())
 
 server.get('/', (req, res) => {
   res.send('up & running...')
+})
+
+server.get('/users', (req, res) => {
+  db('users')
+    .then((user) => {
+      res.status(200).json(user)
+    })
+    .catch((err) => res.status(500).json(err))
 })
 
 server.listen(8000, () => console.log('API RUNNING'))
