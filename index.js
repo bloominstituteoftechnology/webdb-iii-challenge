40389e74b0b1bb37a0db41dabb2c53bc01624d13
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-31 14:25:18 -0700

    CRUD for posts and users

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ server.get('/users', (req, res) => {
         res.status(500).json(err);
     })
 })
+server.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.get(id).then(u => {
+        res.status(200).json(u);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
+})
+// server.get('/users/:id/posts', (req, res) => {
+//     const { id } = req.params;
+//     db.someFunction(id).then(u => {
+//         res.status(200).json(u);
+//     }).catch(err => {
+//         res.status(500).json(err);
+//     })
+// })
 
 server.post('/users', (req, res) => {
     const info = req.body;
@@ -31,9 +47,45 @@ server.post('/users', (req, res) => {
     })
 })
 
+server.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id).then(ids => {
+        if(!ids) {
+            res.status(404).json({ error: 'The user with specified ID does not exist'})
+        }
+        res.status(200).json(ids);
+    })
+    .catch(err  => {
+        res.status(500).json(err)
+    })
+})
+server.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    db.update( id, { name }).then(ids => {
+        const id = ids[0]
+        if(!name)  {
+            res.status(400).json({ error: 'Please provide user name'})
+        } else if (!ids) {
+            res.status(404).json({ error: 'The user with specified ID does not exist' })
+        } else {
+            res.status(200).json(id, ...{ name })
+        }
+    })
+
+})
+
 // **** posts *****
 server.get('/posts', (req, res) => {
     db('Posts').then(post => {
+        res.status(200).json(post);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
+})
+server.get('/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.get(id).then(post => {
         res.status(200).json(post);
     }).catch(err => {
         res.status(500).json(err);
@@ -49,6 +101,88 @@ server.post('/posts', (req, res) => {
     }).catch(err => {
         res.status(500).json(err)
     })
+})
+server.delete('/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id).then(ids => {
+        if(!ids) {
+            res.status(404).json({ error: 'The post with specified ID does not exist'})
+        }
+        res.status(200).json(ids);
+    })
+    .catch(err  => {
+        res.status(500).json(err)
+    })
+})
+server.put('/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { post } = req.body;
+    db.update( id, { post }).then(ids => {
+        const id = ids[0]
+        if(!post)  {
+            res.status(400).json({ error: 'Please provide post text'})
+        } else if (!ids) {
+            res.status(404).json({ error: 'The post with specified ID does not exist' })
+        } else {
+            res.status(200).json(id, ...{ post })
+        }
+    })
+
+})
+
+// **** tags ****
+server.get('/tags', (req, res) => {
+    db('tags').then(tag => {
+        res.status(200).json(tag);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
+})
+server.get('/tags/:id', (req, res) => {
+    const { id } = req.params;
+    db.get(id).then(tag => {
+        res.status(200).json(tag);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+server.post('/tags', (req, res) => {
+    const info = req.body;
+    db.insert(info).into('tags')
+    .then(ids => {
+        const id = ids[0]
+        res.status(201).json({ id, ...info});
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
+server.delete('/tags/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id).then(ids => {
+        if(!ids) {
+            res.status(404).json({ error: 'The tag with specified ID does not exist'})
+        }
+        res.status(200).json(ids);
+    })
+    .catch(err  => {
+        res.status(500).json(err)
+    })
+})
+server.put('/tags/:id', (req, res) => {
+    const { id } = req.params;
+    const { tag } = req.body;
+    db.update( id, { tag }).then(ids => {
+        const id = ids[0]
+        if(!tag)  {
+            res.status(400).json({ error: 'Please provide tag text'})
+        } else if (!ids) {
+            res.status(404).json({ error: 'The tag with specified ID does not exist' })
+        } else {
+            res.status(200).json(id, ...{ tag })
+        }
+    })
+
 })
 
 server.listen(port, () => { console.log(`Server is listening on port ${port}`)})
