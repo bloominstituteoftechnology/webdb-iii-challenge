775178d141b42d61864b257f26f1825aabd0fa96
Austin Merando <ajmerando1@yahoo.com>
2018-07-31 16:06:19 -0700

    post and get for users done

---
+++ data/routers/userRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const db = require('../db');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('users')
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => res.status(500).json(err))
+})
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db('users')
+    .where({id: Number(id)})
+        .then(posts => {
+            if(posts.length === 0){throw new Error('id not found')}
+            res.status(200).json(posts)
+        })
+        .catch(err => res.status(500).json(err.message))
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    db.insert(user).into('users').then(ids => {
+        const id = ids[0];
+        res.status(200).json({ id, ...user })
+    })
+        .catch(err => res.status(500).json(err))
+})
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const db = require('./data/db');
+
+const apiRoutes = require('./data/routers/apiRoutes');
+
+const server = express();
+
+server.use(express.json());
+server.use(morgan('combined'));
+server.use(helmet());
+
+server.use('/api', apiRoutes)
+
+const port = 3300;
+server.listen(port, function () {
+    console.log(`\n=== Web API Listening on http://localhost:${port}... *.* ===\n`);
+});
