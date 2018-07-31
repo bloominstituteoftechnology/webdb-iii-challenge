287a61aae3285201f1e1f715f1893e5df9a8e3a4
Kevin <brackkevin@gmail.com>
2018-07-31 17:07:07 -0500

    Create CRUD endpoints for users table in db tested-ok

--- index.js
+++ index.js
@@ -4,9 +4,69 @@ const db = require("./data/db");
 
 const server = express();
 
+// use middleware
 server.use(express.json());
 
 // endpoints here
+server.get("/", (req, res) => {
+  res.send("up and running");
+});
+
+// users
+server.get("/users", (req, res) => {
+  db("users")
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  db("users")
+    .where("id", Number(id))
+    .first()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/users", (req, res) => {
+  const user = req.body;
+  db.insert(user)
+    .into("users")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...user });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put("/users/:id", (req, res) => {
+  const user = req.body;
+  const id = req.params.id;
+  db("users")
+    .where("id", id)
+    .update(user)
+    .then(ids => {
+      const id = ids[0];
+      res.status(200).json({ id, ...user });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  db("users")
+    .where("id", id)
+    .del()
+    .then(ids => {
+      const id = ids[0];
+      res.status(200).json("USER DELETED SUCCESSFULLY");
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 8000;
 server.listen(port, function() {
