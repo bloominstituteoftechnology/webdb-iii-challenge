c65ae61d9b85f8373e1b40257a2d137a6df606e0
Tyson Gushiken <tysonism@gmail.com>
2018-07-31 14:51:04 -0700

    Create GET and POST endpoints for Tags

--- tags/tagRoutes.js
+++ tags/tagRoutes.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const db = require('../data/db');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  db('tags')
+    .then( tags => {
+      res.status(200).json(tags);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+router.post('/', (req, res) => {
+  const tag = req.body;
+
+  db.insert(tag).into('tags').then(ids => {
+      const id = ids[0]
+      res.status(201).json({id, ...tag});
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
