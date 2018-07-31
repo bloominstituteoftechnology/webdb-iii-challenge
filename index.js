b60effe7b610102b42a06305aa40761dd78d4c91
chainchompa <chainchompa@gmail.com>
2018-07-31 18:01:09 -0400

    able to get users, posts, and tags

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ server.get('/', (req, res) => {
   res.send('up and running...');
 });
 
+server.get('/users', (req, res) => {
+	db('users')
+	.then(response => {
+		res.status(200).json(response);
+	})
+	.catch(err => res.status(500).json(err));
+});
+
+server.get('/posts', (req, res) => {
+	db('posts')
+	.then(response => {
+		res.status(200).json(response);
+	})
+	.catch(err => res.status(500).json(err));
+});
+
+server.get('/tags', (req, res) => {
+	db('tags')
+	.then(response => {
+		res.status(200).json(response);
+	})
+	.catch(err => res.status(500).json(err));
+});
+
 const port = 8000;
 
 server.listen(port, function() {
