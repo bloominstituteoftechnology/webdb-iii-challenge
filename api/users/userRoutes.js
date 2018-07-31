f1c44bff389d9ec33ebb5d2b2b62d283bf47d42a
DirupT <iamcooled@gmail.com>
2018-07-31 14:07:43 -0500

    added get to users

--- api/users/userRoutes.js
+++ api/users/userRoutes.js
@@ -4,8 +4,15 @@ const db = require('../../data/db');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('users');
+router.get('/', async (req, res, next) => {
+    try {
+        const response = await db('users');
+        return res.status(200).json(response);
+    } catch (err) {
+        next({ code: 500, error: 'The users information could not be retrieved.' });
+    }
 })
+
+router.use(errorHandler);
 
 module.exports = router;
