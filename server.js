0f476c1711b4368183555cb2364aa2a07533a857
Vu Cao <cdhvu@yahoo.com>
2018-07-31 13:16:26 -0700

    Created enpoint for GET users and POST users

--- server.js
+++ server.js
@@ -2,8 +2,42 @@ const express = require('express');
 
 const server = express();
 
+const db = require('./data/db')
+
+//middleware
+server.use(express.json());
+
+function sendError(code, message, error) {
+    return {
+        code: code,
+        message: message,
+        error: error
+    }
+}
+
 server.get('/', (req, res) => {
     res.send('Welcome to my Lambda Forum')
+})
+
+server.get('/users', async (req, res, next) => {
+    try {
+        const response = await(db('Users'));
+        res.status(200).json(response);
+    } catch(error) {
+        next(sendError(500, 'Failed to get users information', error.message))
+    }
+})
+
+server.post('/users', async (req, res, next) => {
+    const newUser = req.body;
+    try {
+        const response = await (db('Users').insert(newUser));
+        const id = response[0];
+        res.status(201).json({id, ...newUser});
+    } catch(error) {
+        console.log(error)
+        next(sendError(500, 'Failed to get users information', error.message))
+    }
 })
 
 const port = 8000;
