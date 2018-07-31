81cb4c4e96c4881d03c98a459ea2e04ece3b5e78
Sukhada Gholba <sukhadagholba@gmail.com>
2018-07-31 15:12:44 -0700

    configured server file to address DELETE request for users to remove a user from db

--- server.js
+++ server.js
@@ -39,4 +39,29 @@ server.post('/api/users', (req, res) => {
 
 });
 
+server.delete('/api/users/:id', (req, res)=> {
+	console.log(req.params.id);
+	const id = req.params.id; 
+	
+	 db('users')
+	.where('id', id)
+	.del()	
+	.then(response => {
+		
+		if(response===1) {
+                let responseObject ={};
+                responseObject.message = `Successfully deleted post with id ${id}`;
+
+                res.status(200).json(responseObject);
+                }
+
+		else res.status(404).json({ error: "The post with the specified ID does not exist."});
+	})
+	
+	.catch(err => res.status(500).json(err));
+
+});
+
+
+
 server.listen(5000, () => console.log('API running on port 5000'));
