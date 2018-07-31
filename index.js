3fa55921a1ee10069d30f637005ee15904f8c74c
Kevin <brackkevin@gmail.com>
2018-07-31 17:46:27 -0500

    Create CRUD endpoints for post table tested-ok

--- index.js
+++ index.js
@@ -68,6 +68,64 @@ server.delete("/users/:id", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+// posts
+server.get("/posts", (req, res) => {
+  db("posts")
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  db("posts")
+    .where("id", Number(id))
+    .first()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/posts", (req, res) => {
+  const post = req.body;
+  db.insert(post)
+    .into("posts")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...post });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put("/posts/:id", (req, res) => {
+  const post = req.body;
+  const id = req.params.id;
+  db("posts")
+    .where("id", id)
+    .update(post)
+    .then(ids => {
+      const id = ids[0];
+      res.status(200).json({ id, ...post });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  db("posts")
+    .where("id", id)
+    .del()
+    .then(ids => {
+      const id = ids[0];
+      res.status(200).json("POST DELETED SUCCESSFULLY");
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// tags
+
 const port = 8000;
 server.listen(port, function() {
   console.log(`\n=== WEB API LISTENING ON HTTP://LOCALHOST:${port} ===\n`);
