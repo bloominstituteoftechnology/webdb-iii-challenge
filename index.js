c98c42a718c2881f717a054c387f51bb67ea9fc3
chainchompa <chainchompa@gmail.com>
2018-07-31 18:11:24 -0400

    able to post new users, posts, and tags

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ server.get('/users', (req, res) => {
 	.catch(err => res.status(500).json(err));
 });
 
+server.post('/users', (req, res) => {
+  const user = req.body;
+  db.insert(user).into('users').then(ids => {
+    const id = ids[0];
+    res.status(201).json({ id, ...user})
+  }).catch(err => res.status(500).json(err));
+});
+
 server.get('/posts', (req, res) => {
 	db('posts')
 	.then(response => {
@@ -25,12 +33,28 @@ server.get('/posts', (req, res) => {
 	.catch(err => res.status(500).json(err));
 });
 
+server.post('/posts', (req, res) => {
+  const post = req.body;
+  db.insert(post).into('posts').then(ids => {
+    const id = ids[0];
+    res.status(201).json({ id, ...post})
+  }).catch(err => res.status(500).json(err));
+});
+
 server.get('/tags', (req, res) => {
 	db('tags')
 	.then(response => {
 		res.status(200).json(response);
 	})
 	.catch(err => res.status(500).json(err));
+});
+
+server.post('/tags', (req, res) => {
+  const tag = req.body;
+  db.insert(tag).into('tags').then(ids => {
+    const id = ids[0];
+    res.status(201).json({ id, ...tag})
+  }).catch(err => res.status(500).json(err));
 });
 
 const port = 8000;
