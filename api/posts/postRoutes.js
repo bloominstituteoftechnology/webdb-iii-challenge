9f915916aa1451f931e1a5036fb5b831cd5e93c2
DirupT <iamcooled@gmail.com>
2018-07-31 15:07:50 -0500

    added get to posts

--- api/posts/postRoutes.js
+++ api/posts/postRoutes.js
@@ -3,4 +3,13 @@ const db = require('../../data/db');
 
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+    try {
+        const response = await db('posts');
+        return res.status(200).json(response);
+    } catch (err) {
+        return next({ code: 500, error: 'The posts information could not be retrieved.' });
+    }
+})
+
 module.exports = router;
