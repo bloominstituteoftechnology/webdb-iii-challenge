73300e9312a1a3e1c3a18ea94c05b54c41e8d0e7
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-01 18:16:37 -0500

    initial commit: initialized package.json and knex, implemented endpoints for  USERS & POSTS

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = express('express');
+const knex = require('knex');
+
+const server = express;
+
+server.use(express.json());
+
+// endpoints here
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
+
+
+
