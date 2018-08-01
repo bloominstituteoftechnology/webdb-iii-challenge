b28941d2b2c7cce320a2a338332808e96d1bbe2e
Kevin Tena <kevten09@gmail.com>
2018-08-01 17:05:45 -0500

    Added POST and GET for users

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ const server = express();
 server.use(express.json());
 
 server.post('/users', (req, res) => {
-const user = req.body;
-  db.insert(user)
-  .into('Users')
-  .then(ids => {
-      res.status(201).json(ids);
-  })
-  .catch(err => {
-      res.status(500).json(err);
-  })
+    const user = req.body;
+    db.insert(user)
+        .into('Users')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
 });
+
+server.get('/users', (req, res) => {
+    db.select()
+        .from('Users')
+        .then(users => {
+            res.status(201).json(users);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
 
 server.listen(8000, () => console.log('Running on port 8000'));
