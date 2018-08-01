5e5b5cad8ce4101a397cc29a143b4b4125c6771e
Tyson Gushiken <tysonism@gmail.com>
2018-08-01 15:04:09 -0700

    Create PUT and DELETE Post endpoints

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -11,7 +11,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  db('posts').where('id', req.params.id)
+  db('posts')
+    .where('id', req.params.id)
     .then(post => {
       if (post.length === 0) {
         res.status(200).send({ error: 'A post with this id does not exist.'});
@@ -24,9 +25,59 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   const post = req.body;
 
-  db.insert(post).into('posts').then(ids => {
+  db.insert(post).into('posts')
+    .then(ids => {
       const id = ids[0]
       res.status(201).json({id, ...post});
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+router.put('/:id', (req, res) => {
+  const { userId, text } = req.body;
+  const { id } = req.params;
+
+  db('posts')
+    .where({ id })
+    .update({ userId, text })
+    .then(count => {
+      if (count) {
+        db('posts')
+          .where({ id })
+          .then(post => {
+            res.status(200).json(post);
+          })
+          .catch(err => res.status(500).json(err));
+      } else {
+        res.status(200).send({ error: 'A post with this id does not exist.'});
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  let deletedPost;
+
+  db('posts')
+    .where({ id })
+    .then(post => {
+      if (post.length === 0) {
+        res.status(200).send({ error: 'A post with this id does not exist.'});
+      }
+      deletedPost = post;
+    })
+    .catch(err => res.status(500).json(err));
+
+  db('posts')
+    .where({ id })
+    .del()
+    .then(count => {
+      if (count) {
+        res.status(200).json(deletedPost);
+      } else {
+        res.status(200).send({ error: 'A post with this id does not exist.'});
+      }
     })
     .catch(err => res.status(500).json(err));
 });
