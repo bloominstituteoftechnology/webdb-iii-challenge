7b0e2f86dc2aadd5752a4409069bf773d568458b
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-01 16:32:48 -0400

    user GET :id, put, delete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,25 +3,75 @@ const db = require('../data/db');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    db
-      .get()
-      .then(users => {res.json(users)})
-      .catch(error => {res.status(500).json({ error: 'The users information could not be retrieved.' })
-    })
+  db('users')
+    .then( user => res.status(200).json(user) )
+    .catch( err => res.status(500).json(err) )
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  db('users')
+    .where({id})
+    .then( user => res.status(200).json(user) )
+    .catch( err => res.status(500).json(err) )
 })
   
-router.post('/', (req, res, next) => {
-    const { name } = req.body;
-    const user = { name }
-    if (name.length > 128) return res.status(400).json({ error: 'Name is too long!' });
-    db
-        .insert(user) 
-        .then(response => { 
-            db
-              .get()
-              .then(users => {res.json(users)})
-              .catch(error => {res.status(500).json({ error: 'Unable to post project information!' })})
-        })
+router.get('/:id/posts', (req,res) => {
+  const id = req.params.id
+  db
+    .select('*')
+    .from('posts')
+    .leftJoin('users', 'posts.user_id','users.id')
+    .where({'users.id' : id})
+    .then( data => res.status(200).json(data) )
+    .catch( err => res.status(400).json({err}) )
+})
+
+router.post('/', (req,res) => {
+  const body = req.body
+  if (!body.name) res.status(400).json({err: 'Field required.'})
+  let id = 0;
+  db.insert(body)
+    .into('users')
+    .then( data => id = data[0] )
+    .then( () => {
+      db('users')
+        .where({id})
+        .then( user => res.status(200).json(user) )
+        .catch( err => res.status(500).json(err) )
+    })
+    .catch( err => res.status(500).json(err) )
+})
+
+router.put('/:id', (req,res) => {
+  const id = req.params.id
+  const body = req.body
+  if (!body.name) res.status(400).json({err: 'Field required.'})
+  else {
+    db('users')
+      .where({id})
+      .update(body)
+      .then(user => {
+        if (user > 0) res.status(200).json({message: 'User updated!'}) 
+        else res.status(400).json({err: 'ID not found!'})
+      })
+      .catch( err => res.status(500).json(err))
+  }
+})
+
+router.delete('/:id', (req,res) => {
+  const id = req.params.id
+  db('users')
+    .where({id})
+    .del()
+    .then( data => {
+      if (data === 1){
+        res.status(201).json({message: 'User has been deleted!'})
+      }else {
+        res.status(400).json({message: 'User not found!'})
+      }
+    })
+    .catch(err => res.status(500).json(err))
 })
 
 module.exports = router;
