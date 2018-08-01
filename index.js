43e7979e64ddf04e3cbb89954983a077192099d0
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-01 13:58:45 -0500

    Ahmed Belhadj - RDBMS API Full
    [POST] /posts
    [GET] /posts
    [POST] /tags
    [GET] /tags

--- index.js
+++ index.js
@@ -23,6 +23,42 @@ server.get("/users", (req, res) => {
     .catch(error => res.status(500).json(error));
 });
 
+// Posts
+server.post("/posts", (req, res) => {
+  const post = req.body;
+  db.insert(post)
+    .into("posts")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...post });
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+server.get("/posts", (req, res) => {
+  db("posts")
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json(error));
+});
+
+// Tags
+server.post("/tags", (req, res) => {
+  const tag = req.body;
+  db.insert(tag)
+    .into("tags")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...tag });
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+server.get("/tags", (req, res) => {
+  db("tags")
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json(error));
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
