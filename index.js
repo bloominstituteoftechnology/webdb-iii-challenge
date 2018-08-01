4abb549fb947ea74a9254834e9dad6c382ee8757
Kevin <brackkevin@gmail.com>
2018-08-01 15:09:39 -0500

    Create CRUD endpoints for tags db tested-ok

--- index.js
+++ index.js
@@ -125,6 +125,57 @@ server.delete("/posts/:id", (req, res) => {
 });
 
 // tags
+server.get("/tags", (req, res) => {
+  db("tags")
+    .then(tag => {
+      res.status(200).json(tag);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/tags/:id", (req, res) => {
+  const id = req.params.id;
+  db("tags")
+    .where("id", Number(id))
+    .then(tag => res.status(200).json(tag))
+    .catch(err => res.status(500).json(err));
+});
+
+server.post("/tags", (req, res) => {
+  const tag = req.body;
+  db.insert(tag)
+    .into("tags")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...tag });
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+server.put("/tags/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  db("tags")
+    .where("id", id)
+    .update(changes)
+    .then(ids => {
+      const id = ids[0];
+      res.status(200).json({ id, ...changes });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete("/tags/:id", (req, res) => {
+  const id = req.params.id;
+  db("tags")
+    .where("id", id)
+    .del()
+    .then(ids => {
+      const id = ids[0];
+      res.status(200).json("TAG DELETED SUCCESSFULLY");
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 8000;
 server.listen(port, function() {
