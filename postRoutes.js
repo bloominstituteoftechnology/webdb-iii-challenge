6866a83f1cf8afae9a25b44b9ae62ab3073f155a
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-01 14:31:42 -0700

    configured server to address UPDATE request to edit a post

--- postRoutes.js
+++ postRoutes.js
@@ -67,4 +67,38 @@ postrouter.delete('/:id', (req, res) => {
 
   });
 
+
+postrouter.put('/:id', (req, res) => {
+  const {text} = req.body;
+
+  const id =  req.params.id;
+  const post = {text};
+
+if (!text) {
+                res.status(400).json({errorMessage: "Please provide text for the post."});
+}
+
+else{
+ 
+	db('posts')
+        .where('id', id)
+        .update({
+         text: text
+        })
+
+        .then(response => {
+                if(response===0)  res.status(404).json({ message: "The post with the specified ID does not exist." });
+                else{
+                        let responseObject ={};
+                        responseObject.message= `Successfully updated post text with id ${id}`
+                        res.status(200).json(responseObject);
+                }
+        })
+
+        .catch(error => {
+        res.status(500).json({ message: "Couldn't update the post" });
+        })
+}
+});
+
 module.exports = postrouter;
