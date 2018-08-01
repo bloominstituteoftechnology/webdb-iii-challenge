8b186f8cf46d5c7a1971460a24f9cc5abb8063d4
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-01 16:43:35 -0500

    Add User put method

--- project/index.js
+++ project/index.js
@@ -21,7 +21,12 @@ server.get("/users/:id/posts", (req,res) => {
     const { id } = req.params;
     db.select().from('Users').where('userId', id).innerJoin('Posts', 'Users.id', 'Posts.userId')
       .then(response => (res.send(response)))
-})
+});
+server.put("/users/:id", (req, res) => {
+    const { id, name } = req.body;
+    db.update({"name": name}).into('Users').where('id', id)
+      .then(response => (res.json(response)))
+});
 
 
 server.listen(port, () => { console.log(`Server is running on port ${port}`)});
