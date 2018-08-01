834f1f8bd71fdaceb2dc7efef78be69dd50f4478
Simon <Simon_Freelance@hotmail.com>
2018-08-01 16:03:12 -0400

    GET request for individual ID completed

--- api/tagRouter.js
+++ api/tagRouter.js
@@ -14,3 +14,19 @@ router.get('/', (req, res, next) => {
         next(err);
     })
 })
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    db('tags')
+    .where('id', id)
+    .then(response => {
+        if(response.length === 0) {
+            throw {code: codes.NOT_FOUND, message: 'Tag with that id does not exist'}
+        }
+        res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+        next(err);
+    })
+})
+
+module.exports = router;
