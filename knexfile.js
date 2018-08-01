2a2147e4890d786d67edaa65c48aeb509cb35924
Austin Merando <ajmerando1@yahoo.com>
2018-08-01 16:37:50 -0700

    added seed data for users

--- knexfile.js
+++ knexfile.js
@@ -7,39 +7,8 @@ module.exports = {
     connection: {
       filename: './data/apifull.sqlite3'
     },
-    useNullasDefault: true
+    useNullasDefault: true,
+    seeds: { directory: './data/seeds' },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
