b403404c4a527c2738fb0b300795e067fc2bb9d6
Kevin Tena <kevten09@gmail.com>
2018-08-01 16:59:23 -0500

    Set up the server and database

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/users', (req, res) => {
+const user = req.body;
+  db.insert(user)
+  .into('Users')
+  .then(ids => {
+      res.status(201).json(ids);
+  })
+  .catch(err => {
+      res.status(500).json(err);
+  })
+});
+
+server.listen(8000, () => console.log('Running on port 8000'));
