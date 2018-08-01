9750fc8b12cc5b31349cf901389dcafa939109cb
Kevin <brackkevin@gmail.com>
2018-08-01 16:40:50 -0500

    Create a router for /users. Abstracted away user routes to router

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const db = require("./data/db");
 
+// Routers
+const userRouter = require("./routers/users");
+
 const server = express();
 
 // use middleware
@@ -13,60 +16,7 @@ server.get("/", (req, res) => {
 });
 
 // users
-server.get("/users", (req, res) => {
-  db("users")
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-server.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  db("users")
-    .where("id", Number(id))
-    .first()
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-server.post("/users", (req, res) => {
-  const user = req.body;
-  db.insert(user)
-    .into("users")
-    .then(ids => {
-      const id = ids[0];
-      res.status(201).json({ id, ...user });
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-server.put("/users/:id", (req, res) => {
-  const user = req.body;
-  const id = req.params.id;
-  db("users")
-    .where("id", id)
-    .update(user)
-    .then(ids => {
-      const id = ids[0];
-      res.status(200).json({ id, ...user });
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-server.delete("/users/:id", (req, res) => {
-  const id = req.params.id;
-  db("users")
-    .where("id", id)
-    .del()
-    .then(ids => {
-      const id = ids[0];
-      res.status(200).json("USER DELETED SUCCESSFULLY");
-    })
-    .catch(err => res.status(500).json(err));
-});
+server.use("/users", userRouter);
 
 // posts
 server.get("/posts", (req, res) => {
