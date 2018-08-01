1150594d8603047ad010d2530e051de0aee0affc
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-01 16:07:39 -0400

    setupi api router, rerouted

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-const postRoutes = require('./routes/postRoutes');
-const tagRoutes = require('./routes/tagRoutes');
+const api = require('./routes/apiRouter');
 
 const server = express();
-const router = express.Router();
 
-server.use(express());
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/tags', tagRoutes);
+server.use(express.json());
+
 
 server.get('/', (req, res) => {
     res.send('up and running...');
 });
+
+server.use('/api', api)
 
 const port = 8000;
 server.listen(port, function() {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,33 +2,26 @@ const express = require('express');
 const db = require('../data/db');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     db
       .get()
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(error => {
-        res
-          .status(500)
-          .json({ error: "The users information could not be retrieved." });
-      });
-  });
+      .then(users => {res.json(users)})
+      .catch(error => {res.status(500).json({ error: 'The users information could not be retrieved.' })
+    })
+})
   
-router.post("/", (req, res) => {
-    const user = req.body;
+router.post('/', (req, res, next) => {
+    const { name } = req.body;
+    const user = { name }
+    if (name.length > 128) return res.status(400).json({ error: 'Name is too long!' });
     db
-      .insert(user)
-      .into("Users")
-      .then(ids => {
-        const id = ids[0];
-        res.status(200).json({ id, ...user });
-      })
-      .catch(error => {
-        res.status(500).json({
-          error: "There was an error while saving the user to the database."
-        });
-    });
-});
+        .insert(user) 
+        .then(response => { 
+            db
+              .get()
+              .then(users => {res.json(users)})
+              .catch(error => {res.status(500).json({ error: 'Unable to post project information!' })})
+        })
+})
 
 module.exports = router;
