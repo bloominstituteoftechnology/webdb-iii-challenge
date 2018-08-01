602536a80496e3554976037da14fc90462c5a4fb
CLN <cln@Sags-MacBook-Pro.local>
2018-08-01 18:03:06 -0400

    got all of tag endpoints and fixing the nuking deletes finishing post and users, .

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ server.get('/users', (req, res) => {
         res.status(200).json(user)
     }).catch(err => res.status(500).json(err))
 });
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    db('users')
+    .where('id', '=', id)
+    .then(user => {
+        res.status(200).json(user)
+    }).catch(err => res.status(500).json(err))
+});
 
 server.post('/users', (req, res) => {
     const user = req.body;
@@ -23,20 +31,117 @@ server.post('/users', (req, res) => {
         res.status(201).json({id, ...user})
     }).catch(err => res.status(500).json(err))
 })
-server.get('/posts', (req, res) => {
+server.delete('/users/:id', (req, res) => {//not working properly
+    const id = req.params.id;
+    // posts = posts.filter(p => p.id != id)
+    db('users')
+    .where('id', '=', id)
+    .del(id)
+      .then(user => {
+        if (user === 0) {
+          res.status(404)
+          .json({ error: "The u with the specified ID does not exist." })
+        }
+        res.status(200).json(user)
+      }).catch(error => {
+        res.status(500)
+          .json({ error: "error 2." })
+      });
+  })
+server.get('/posts', (req, res) => {// get all posts
     db('posts').then(pid => {
         res.status(200).json(pid)
     }).catch(err => res.status(500).json(err))
 });
 
-server.post('/posts', (req, res) => {
-    const zoo = req.body;
+server.post('/posts', (req, res) => {// post new post
+    const post = req.body;
 
-    db.insert(zoo).into('posts').then(ids => {
+    db.insert(post).into('posts').then(ids => {
         const id = ids[0];
-        res.status(201).json({id, ...zoo})
+        res.status(201).json({id, ...post})
     }).catch(err => res.status(500).json(err))
 })
+server.get('/posts/:id', (req, res) => {//get tag by id
+    const id = req.params.id;
+    db('posts')
+    .where('id', '=', id)
+    .then(post => {
+        res.status(200).json(post)
+    }).catch(err => res.status(500).json(err))
+});
+server.put('/posts/:id', (req, res) => {// update tags
+    const changes = req.body;
+    const id = req.params.id;
+  
+    db('posts')
+      .where('id', '=', id) // or .where({ id: id })
+      .update(changes)
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+      
+  });
+
+server.get('/tags', (req, res) => {// get all tags
+    db('tags').then(tag => {
+        res.status(200).json(tag)
+    }).catch(err => res.status(500).json(err))
+});
+
+server.post('/tags', (req, res) => {// post a new tag
+    const tag = req.body;
+
+    db.insert(tag).into('tags').then(ids => {
+        const id = ids[0];
+        res.status(201).json({id, ...tag})
+    }).catch(err => res.status(500).json(err))
+})
+server.get('/tags/:id', (req, res) => {//get tag by id
+    const id = req.params.id;
+    db('tags')
+    .where('id', '=', id)
+    .then(tag => {
+        res.status(200).json(tag)
+    }).catch(err => res.status(500).json(err))
+});
+server.delete('/tags/:id', (req, res) => {//delete tag
+    const id = req.params.id;
+    
+    db('tags')
+    .where('id', '=', id)
+    .del()
+      .then(tag => {
+        if (tag === 0) {
+          res.status(404)
+          .json({ error: "The tag with the specified ID does not exist." })
+        }
+        res.status(200).json(tag)
+      }).catch(error => {
+        res.status(500)
+          .json({ error: "error 2." })
+      });
+  })
+  server.put('/tags/:id', (req, res) => {// update tags
+    const changes = req.body;
+    const id = req.params.id;
+  
+    db('tags')
+      .where('id', '=', id) // or .where({ id: id })
+      .update(changes)
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+      
+  });
 
 const port = 3500;
 server.listen(port, function () {
