4589bcb86ee7a5f2e01486b28b74fbd624ada715
Simon <Simon_Freelance@hotmail.com>
2018-08-01 15:58:18 -0400

    PUT request for postRouter completed

--- api/postRouter.js
+++ api/postRouter.js
@@ -38,7 +38,18 @@ router.post('/', (req,res, next) => {
         next(err);
     })
 })
-
+router.put('/:id', (req,res,next) => {
+    const { id } = req.params;
+    db('posts')
+    .where('id', id)
+    .update(req.body)
+    .then(response => {
+        res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+        next(err);
+    });
+});
 router.delete('/:id', (req, res, next) => {
     const { id } = req.params;
     db('posts')
