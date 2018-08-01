2abf9c971aa5498f3b2d64551421052f93fb5317
CLN <cln@Sags-MacBook-Pro.local>
2018-08-01 00:27:01 -0400

    users and posts posting,.

--- server.js
+++ server.js
@@ -3,11 +3,42 @@ const express = require('express');
 const db = require('./data/db')
 
 const server = express();
-server.get('/', (req, res) => {
-    res.send('up and running...')
-    });
+server.use(express.json());
 
-    const port = 3500;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+server.get('/', (req, res) => {
+    res.send('up and running... 35')
+});
+
+server.get('/users', (req, res) => {
+    db('users').then(user => {
+        res.status(200).json(user)
+    }).catch(err => res.status(500).json(err))
+});
+
+server.post('/users', (req, res) => {
+    const user = req.body;
+
+    db.insert(user).into('users').then(users =>{
+        const id = users[0];
+        res.status(201).json({id, ...user})
+    }).catch(err => res.status(500).json(err))
+})
+server.get('/posts', (req, res) => {
+    db('posts').then(pid => {
+        res.status(200).json(pid)
+    }).catch(err => res.status(500).json(err))
+});
+
+server.post('/posts', (req, res) => {
+    const zoo = req.body;
+
+    db.insert(zoo).into('posts').then(ids => {
+        const id = ids[0];
+        res.status(201).json({id, ...zoo})
+    }).catch(err => res.status(500).json(err))
+})
+
+const port = 3500;
+server.listen(port, function () {
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
