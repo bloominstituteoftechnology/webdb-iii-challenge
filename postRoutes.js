00666178db4745e460ea54d03b6e6e791e4b1b82
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-01 14:20:20 -0700

    configured server to address POST request to add a new post to the posts db

--- postRoutes.js
+++ postRoutes.js
@@ -5,6 +5,7 @@ const db = require('./data/db');
 
 
 postrouter.get('/', (req, res) => {
+	
 	db('posts')
         .then(response => {
                 res.status(200).json(response);
@@ -13,6 +14,37 @@ postrouter.get('/', (req, res) => {
         .catch(err => res.status(500).json(err));
 
 });
+
+postrouter.post('/', (req, res) => {
+
+        const {text, userId} = req.body;
+        const post = {text, userId};
+
+        if (!text || !userId) {
+                res.status(400).json({errorMessage: "Please provide text and userId for the post."});
+        }
+
+        else if(isNaN(parseInt(userId))){
+                res.status(400).json({errorMessage: "Please provide a number for userId."});
+        }
+
+        else{
+
+	db.insert(post)
+        .into('posts')
+        .then(ids => {
+                const id= ids[0];
+		const message ="Successfully added a new post";
+                res.status(200).json({id, ...post, message});
+        })
+
+
+        .catch(error => {
+        res.status(500).json({ message: "There was an error while saving the post to the database" });
+        })
+	}
+});
+
 
 
 module.exports = postrouter;
