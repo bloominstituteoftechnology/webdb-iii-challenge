36b562cc8a5a5311d932f8ea3cc82e750ab9ca5e
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-01 16:21:09 -0500

    Add pet posts by id

--- project/index.js
+++ project/index.js
@@ -7,12 +7,21 @@ server.use(express.json());
 
 const port = 8000;
 
-
-server.get("/api/users", (req, res) => {
-  db.select('name').from('Users')
+//users
+server.get("/users", (req, res) => {
+  db.select().from('Users')
     .then(response => (res.send(response)));
 });
-
+server.get("/users/:id", (req,res) =>{
+  const { id } = req.params;
+  db.select().from('Users').where('id', id)
+    .then(response => (res.send(response)));
+});
+server.get("/users/:id/posts", (req,res) => {
+    const { id } = req.params;
+    db.select().from('Users').where('userId', id).innerJoin('Posts', 'Users.id', 'Posts.userId')
+      .then(response => (res.send(response)))
+})
 
 
 server.listen(port, () => { console.log(`Server is running on port ${port}`)});
