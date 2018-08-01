4746eaabd2b0e263cb974db65d7dbaffb3fbc2b4
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-01 13:52:42 -0500

    Ahmed Belhadj - RDBMS API Full
    [POST] /users
    [GET] /users

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const server = express();
 server.use(express.json());
 
 // endpoints here
+// Users
+server.post("/users", (req, res) => {
+  const user = req.body;
+  db.insert(user)
+    .into("users")
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ id, ...user });
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+server.get("/users", (req, res) => {
+  db("users")
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json(error));
+});
 
 const port = 3300;
 server.listen(port, function() {
