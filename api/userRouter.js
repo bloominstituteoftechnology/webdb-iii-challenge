25aa925b47328ebda5d58c949cb10343dd07e683
Simon <Simon_Freelance@hotmail.com>
2018-08-02 15:35:16 -0400

    GET request for finding all posts for specified user

--- api/userRouter.js
+++ api/userRouter.js
@@ -1,66 +1,86 @@
 const codes = require("./../data/statusCodes");
 
 const express = require("express");
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    db('users')
+router.get("/", (req, res, next) => {
+  db("users")
     .then(response => {
-        res.status(codes.OK).json(response);
+      res.status(codes.OK).json(response);
     })
     .catch(err => {
-        next(err);
-    })
-})
-router.get('/:id', (req, res, next) => {
-    const { id } = req.params;
-    db('users')
-    .where('id', id)
-    .then(response => {
-        if(response.length === 0) {
-            throw {code: codes.NOT_FOUND, message: 'User with that id does not exist'}
-        }
-        res.status(codes.OK).json(response);
-    })
-    .catch(err => {
-        next(err);
-    })
-})
-router.post('/', (req,res, next) => {
-    db('users')
-    .insert(req.body)
-    .then(response => {
-        res.status(codes.CREATED).json(response);
-    })
-    .catch(err => {
-        next(err);
-    })
-})
-router.put('/:id', (req,res,next) => {
-    const { id } = req.params;
-    db('users')
-    .where('id', id)
-    .update(req.body)
-    .then(response => {
-        res.status(codes.OK).json(response);
-    })
-    .catch(err => {
-        next(err);
+      next(err);
     });
 });
-router.delete('/:id', (req, res, next) => {
-    const { id } = req.params;
-    db('users')
-    .where('id', id)
-    .del().then(response => {
-        if(response === 0) {
-            throw {code: codes.NOT_FOUND, message: 'User does not exist with that id'}
-        }
-        res.status(200).json(response);
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  db("users")
+    .where("id", id)
+    .then(response => {
+      if (response.length === 0) {
+        throw {
+          code: codes.NOT_FOUND,
+          message: "User with that id does not exist"
+        };
+      }
+      res.status(codes.OK).json(response);
     })
     .catch(err => {
-        next(err);
+      next(err);
+    });
+});
+router.get("/:id/posts", (req, res, next) => {
+  const { id } = req.params;
+  db('posts')
+  .join('users', 'users.id', '=', 'posts.userId')
+  .select('posts.*', 'users.*')
+  .where('posts.userId', id)
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+router.post("/", (req, res, next) => {
+  db("users")
+    .insert(req.body)
+    .then(response => {
+      res.status(codes.CREATED).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+router.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  db("users")
+    .where("id", id)
+    .update(req.body)
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+router.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  db("users")
+    .where("id", id)
+    .del()
+    .then(response => {
+      if (response === 0) {
+        throw {
+          code: codes.NOT_FOUND,
+          message: "User does not exist with that id"
+        };
+      }
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
     });
 });
 module.exports = router;
