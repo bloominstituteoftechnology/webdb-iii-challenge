00c4df6b81c54c835b168443820463fad910ee65
Vu Cao <cdhvu@yahoo.com>
2018-08-01 18:07:35 -0700

    Created all endpoints for Posts and Tags. MVP Completed.

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ server.get('/users/:id', async (req, res, next) => {
     try {
         const response = await(db('Users').where({id: Number(id)}));
         if (response.length === 0) {
-            return next(sendError(404, 'Failed to retrieve user information', 'The user for this specific id does not exists.'))
+            return next(sendError(404, 'Failed to retrieve user information', 'The user for this specific id does not exist.'))
         }
         res.status(200).json(response[0]);
     } catch(error) {
@@ -66,7 +66,7 @@ server.delete('/users/:id', async (req, res, next) => {
         const user = await(db('Users').select().where('id', Number(id)));
         const response = await(db('Users').where('id', id).del());
         if (response === 0) {
-            return next(sendError(404, 'Failed to remove user.', 'The user for this specific id does not exists.'))
+            return next(sendError(404, 'Failed to remove user.', 'The user for this specific id does not exist.'))
         }
         res.status(200).json(user[0]);
     } catch(error) {
@@ -86,7 +86,7 @@ server.put('/users/:id', async (req, res, next) => {
         const response = await(db('Users').where('id', id).update(changes));
         const user = await(db('Users').where('id', Number(id)));
         if (response === 0) {
-            return next(sendError(404, 'Failed to update user', 'The user for this specific id does not exists.'))
+            return next(sendError(404, 'Failed to update user', 'The user for this specific id does not exist.'))
         }
         res.status(200).json(user);
     } catch(error) {
@@ -133,7 +133,7 @@ server.get('/posts/:id', async (req, res, next) => {
         const postRes = await(getPostQuery);
         const tagRes = await(getTagQuery);
         if (postRes.length === 0) {
-            return next(sendError(404, 'Failed to retrieve post information', 'The post for this specific id does not exists.'))
+            return next(sendError(404, 'Failed to retrieve post information', 'The post for this specific id does not exist.'))
         }
 
         let post = postRes[0];
@@ -160,8 +160,119 @@ server.post('/posts', async (req, res, next) => {
         next(sendError(500, 'Failed to add post to database.', error.message))
     }
 })
-// =================================== TAGS ENDPOINTS ================================== 
 
+server.delete('/posts/:id', async (req, res, next) => {
+    const id = Number(req.params.id);
+
+    try {
+        const post = await(db('Posts').where({ id }));
+        const response = await(db('Posts').where({ id }).del());
+        if (response === 0) {
+            return next(sendError(404, 'Failed to remove post.', 'The post for this specific id does not exist.'))
+        }
+        res.status(200).json(post[0]);
+    } catch(error) {
+        next(sendError(500, 'Failed to remove post.', error.message))
+    }
+})
+
+server.put('/posts/:id', async (req, res, next) => {
+    if ((!req.body.text && !req.body.userid)) {
+        return next(sendError(400, 'Failed to update post.', 'Please provide userid and text for post.'))
+    }
+    
+    const id = Number(req.params.id);
+    const changes = req.body;
+    console.log(changes);
+    try {
+        const response = await(db('Posts').where({ id }).update(changes));
+        const post = await(db('Posts').where({ id }));
+        if (!response) {
+            return next(sendError(404, 'Failed to update post', 'The post for this specific id does not exist.'))
+        }
+        res.status(200).json(post);
+    } catch(error) {
+        next(sendError(500, 'Failed to update post.', error.message))
+    }
+})
+// =================================== TAGS ENDPOINTS ================================== 
+server.get('/tags', async (req, res, next) => {
+    try {
+        const response = await (db('Tags').select());
+        res.status(200).json(response);
+    } catch (error) {
+        next(sendError(500, 'Failed to get tags information.', error.message))
+    }
+})
+
+server.get('/tags/:id', async (req, res, next) => {
+    const id = Number(req.params.id);
+
+    try {
+        const response = await (db('Tags').where({ id }));
+
+        if (response.length === 0) {
+            return next(sendError(404, 'Failed to retrieve tags information', 'The tag for this specific id does not exist.'))
+        }
+
+        res.status(200).json(response);
+    } catch (error) {
+        next(sendError(500, 'Failed to get tag information.', error.message))
+    }
+})
+
+server.post('/tags', async (req, res, next) => {
+    if (!req.body.tag) {
+        return next(sendError(400, 'Failed to save tag to database.', 'Please provide tag name.'))
+    }
+
+    try {
+        const response = await (db('Tags').insert(req.body));
+        const id = response[0];
+        const newTag = await (db('Tags').where({ id }));
+        res.status(200).json(newTag);
+    } catch (error) {
+        next(sendError(500, 'Failed to save tag to database.', error.message))
+    }
+})
+
+server.delete('/tags/:id', async (req, res, next) => {
+    const id = Number(req.params.id);
+
+    try {
+        const tag = await (db('Tags').where({ id }));
+        if (!tag) {
+            return next(sendError(404, 'Failed to remove tag.', 'The tag for this specific id does not exist.'))
+        }
+
+        await (db('Tags').where({ id }).del());
+        res.status(200).json(tag);
+    } catch (errore) {
+        next(sendError(500, 'Failed to remove tag.', error.message))
+    }
+})
+
+server.put('/tags/:id', async (req, res, next) => {
+    if (!req.body.tag) {
+        return next(sendError(400, 'Failed to save tag to database.', 'Please provide tag name.'))
+    }
+
+    const id = Number(req.params.id);
+    const changes = req.body;
+    try {
+        const response = await (db('Tags').where({ id }).update(changes));
+        if (!response) {
+            return next(sendError(404, 'Failed to update tag.', 'The tag for this specific id does not exist.'))
+        } else {
+            const newTag = await db('Tags').where({ id });
+            res.status(200).json(newTag);
+        }
+    } catch (error) {
+        next(sendError(500, 'Failed to update tag.', error.message))
+    }
+})
+
+// ========================== Error Handler ===========================
 server.use((err, req, res, next) => {
     res.status(err.code).send({message: err.message, error: err.error})
 })
