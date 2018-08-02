28c0ea2a6715c85f26c0f8c8a6db03fd687bfb7a
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-02 13:09:35 -0600

    implemented knex into project

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const db = require('./data/db');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 const server = express();
 server.use(express.json());
