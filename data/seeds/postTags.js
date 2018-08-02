5c1ced29dab0da214b3a42b2a5966e5e8359c0f7
cmok4290 <cmok4290@gmail.com>
2018-08-02 11:44:35 -0400

    fixed seed table name

--- data/seeds/postTags.js
+++ data/seeds/postTags.js
@@ -1,10 +1,10 @@
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("post_tags")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("post_tags").insert([
         {
           id: 1,
           postId: 1,
