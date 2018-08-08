7d35acb827d9444dfcd723445b50fbaba438f558
Katie Gorbell <katiegorbell@gmail.com>
2018-08-08 17:25:10 -0500

    got server up and running, reconfigured DB: users table created, working on posts

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/RDBMS-Full.db'
+    },
+    useNullAsDefault: true
   },
 
   staging: {
