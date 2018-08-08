188f8382ac4730053721c371811304e7cd2ccb46
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-08 14:33:11 -0700

    Write POST users endpoint

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ server.get('/', (req, res) => {
   res.send('up and running...');
 });
 
+server.post('/users', (req, res) => {
+  const user = req.body;
+  db.insert(user)
+    .into('characters')
+    .then(ids => {
+      res.status(201).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
