c972bf7b6120b4c2e15a091bd31dfbc71def33fb
unknown <may.alex43@gmail.com>
2018-08-09 11:49:26 -0400

    forgot to commit last night

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ server.get('/users', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+server.get('/users/:id', (req, res) => {
+    const {id}= req.params;
+    db('users')
+    .where({id})
+    .then(users=>{
+        res.status(200).json(users);
+    })
+    .catch(err=> res.status(500).json(err));
+});
+
 server.post('/users', (req, res) => {
     const user=req.body;
     db.insert(user)
@@ -60,7 +70,42 @@ server.delete('/users/:id', (req, res)=> {
         });
 });
 
-  const port = 3300;
+///////////////////// Posts ///////////////////////
+
+server.get('/posts', (req, res)=> {
+    db('posts')
+    .then(post=> {
+        res.status(200).json(post);
+    })
+    .catch(error=> {
+        res.status(500).json(error)
+    })
+});
+
+server.get('/posts/:id', (req, res) => {
+    const {id}= req.params;
+    db('posts')
+    .where({id})
+    .then(posts=>{
+        res.status(200).json(posts);
+    })
+    .catch(err=> res.status(500).json(err));
+});
+
+server.post('/posts', (req, res)=> {
+    const posts= req.body;
+    db.insert(posts)
+    .into('posts')
+    .then(ids=> {
+        const id= ids[0];
+        res.status(200).json(id, ... posts)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+});
+
+const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
