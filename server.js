b0a85a80a6b6698478f3b983d84ef4f021a5c558
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-05 16:54:44 -0400

    Cohort routes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
 
-
-const knex = require('knex');
-const knexConfig = require('./knexfile');
-
-const db = knex(knexConfig.development);
+const apiRoute = require('./api');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+
+server.use('/api', apiRoute);
 
 server.listen(8000, () => console.log('<= API RUNNING = PORT: 8000 =>'));
