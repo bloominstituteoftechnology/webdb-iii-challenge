fddb0d91ce461cd575136c49aaa5ccee7cf7bef7
Lauren <llwor94@gmail.com>
2018-09-05 10:16:13 -0700

    students migration set up

--- data/migrations/20180905100604_create_students.js
+++ data/migrations/20180905100604_create_students.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('students', table => {
+    table.increments();
+    table.string('name', 128).notNullable();
+    table
+      .integer('cohort_id')
+      .notNullable()
+      .references('id')
+      .inTable('cohorts');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('students');
 };
