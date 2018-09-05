e2e26d2ded0df8765b389115a16b52c5b0f0ea67
Lauren <llwor94@gmail.com>
2018-09-05 10:09:43 -0700

    files set up

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/db.sqlite3',
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    useNullAsDefault: true,
+  },
+};
