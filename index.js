10dfa6f95d7f23290b17c71a5dc089673065d4bd
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-05 17:33:08 -0400

    student endpoints

--- index.js
+++ index.js
@@ -23,7 +23,18 @@ function checkIfCohortExists (req, res, next) {
     .catch(err => res.status(500).json(err));
 }
 
-//endpoints
+function VerifyCohort (req, res, next) {
+    const id = req.body.cohort_id;
+    db('cohorts').where({ id: id })
+    .then(cohort => {
+        if (cohort.length === 0) {
+        res.status(404).json({ message: "Please enter a valid cohort ID." });
+        } else next();
+    })
+    .catch(err => res.status(500).json(err));
+}
+
+//cohort endpoints
 server.post('/api/cohorts', (req, res) => {
     const cohort = req.body;
     if (!cohort.name) {
@@ -97,6 +108,71 @@ server.put('/api/cohorts/:id', (req, res) => {
             res.status(200).json({ message: "The cohort was successfully updated." });
         } else {
             res.status(404).json({ message: "The cohort with the specified ID does not exist." });
+        }
+        })
+        .catch(err => res.status(500).json(err));
+})
+
+
+//student endpoints
+server.post('/api/students', VerifyCohort, (req, res) => {
+    const student = req.body;
+    if (!student.name) {
+        res.status(400).json({ error: "Please provide a name for the student." })
+    } else
+        db.insert(student)
+        .into('students')
+        .then(ids => {
+        res.status(201).json(ids);
+        })
+        .catch(err => res.status(500).json({ error: "There was an error saving the student." }))
+})
+    
+server.get('/api/students', (req, res) => {
+    db('students')
+    .then(students => {
+        res.status(200).json(students)
+    })
+    .catch(err => res.status(500).json(err));
+})
+
+server.get('/api/students/:id', (req, res) => {
+    const {id} = req.params;
+    db('students').where({ id: id })
+    .then(student => {
+        if (student.length === 0) {
+        res.status(404).json({ message: "The student with the specified ID does not exist." });
+        } else 
+        res.status(200).json(student);
+    })
+    .catch(err => res.status(500).json(err));
+})
+
+server.delete('/api/students/:id', (req, res) => {
+    const {id} = req.params;
+    db('students').where({ id: id }).del()
+    .then(count => {
+        if (count) {
+        res.status(204).end();
+        } else {
+        res.status(404).json({ message: "The student with the specified ID does not exist." });
+        }
+    })
+    .catch(err => res.status(500).json(err));
+})
+
+server.put('/api/students/:id', VerifyCohort, (req, res) => {
+    const {id} = req.params;
+    const student = req.body;
+    if (!student.name) {
+        res.status(400).json({ error: "Please provide a name for the student." })
+    } else
+        db('students').where({ id: id }).update(student)
+        .then(count => {
+        if (count) {
+            res.status(200).json({ message: "The student was successfully updated." });
+        } else {
+            res.status(404).json({ message: "The student with the specified ID does not exist." });
         }
         })
         .catch(err => res.status(500).json(err));
