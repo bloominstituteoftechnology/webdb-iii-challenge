3b93b6cf7b07ca797e22526324cb7720bfa6b2eb
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-09-05 13:13:14 -0700

    implement CRUD for students

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const cohortRoutes = require('./cohortRoutes.js');
-// const studentRoutes = require('./studentRoutes.js');
+const studentRoutes = require('./studentRoutes.js');
 
 const router = express.Router();
 
 router.use('/cohorts', cohortRoutes);
-// router.use('/students', studentRoutes);
+router.use('/students', studentRoutes);
 
 module.exports = router;
