1c2f729c979e0fbe5219085526ff87cea19848be
sliw <slioweng@gmail.com>
2018-09-06 04:35:02 +0800

    server working

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const knex = require('knex')
 const knexConfig = require('./knexfile')
 
 const server = express()
-const db = knex(knexConfig)
+const db = knex(knexConfig.development)
 
 server.use(express.json())
 
-server.get('/', (req, res) => {
+server.get('/cohorts', async (req, res) => {
+  const cohorts = await db.select().table('cohorts')
+  res.status(200).json(cohorts)
+})
+
+server.listen('3300', () => {
+  console.log(`\n=== Web API Listening on http://localhost:3300 ===\n`)
 })
