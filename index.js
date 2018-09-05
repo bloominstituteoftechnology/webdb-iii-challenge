0fefffbd48463a455581f43e6725956fa6d65ebd
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-05 17:41:08 -0500

    created students table using migrate:make

--- index.js
+++ index.js
@@ -16,7 +16,30 @@ server.get('/', (req, res) => {
   res.send('API Running?');
 });
 
-
+// add a cohort
+server.post('/api/cohorts', (req, res) => {
+    const cohort = req.body;
+  
+    // console.log(cohort);
+  
+    // insert into cohorts () values ()
+    // db('cohorts').insert(cohort).then().catch()
+    db.insert(cohort)
+      .into('cohorts')
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => res.status(500).json(err));
+});
+  
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+      // .select('name')
+      .then(cohorts => {
+        res.status(200).json(cohorts);
+      })
+      .catch(err => res.status(500).json(err));
+});
 
 const port = 3400;
 server.listen(port, function() {
