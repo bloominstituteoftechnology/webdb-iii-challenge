f4a37d043186ee3aa1ffd04a3641219ee14ed442
Ashwin <aswinjeganath@gmail.com>
2018-09-06 02:44:18 +0530

    Impelement route handler for retrieving all students

--- routes.js
+++ routes.js
@@ -108,4 +108,83 @@ router.route('/cohorts/:id/students').get(function(req, res, next) {
     .catch(next);
 });
 
+router
+  .route('/students')
+  .get(function(_, res, next) {
+    db('students')
+      .then(data => {
+        res.status(200).json(data);
+      })
+      .catch(next);
+  })
+  .post(function(req, res, next) {
+    if (!req.body.name)
+      return sendError(res, 400, 'You need to provide a name');
+
+    db('students')
+      .insert({ name: req.body.name })
+      .then(data => {
+        res
+          .status(201)
+          .json({ message: 'Cohort created successfully', id: data });
+      })
+      .catch(next);
+  });
+
+router
+  .route('/cohorts/:id')
+  .get(function(req, res, next) {
+    db('cohorts')
+      .where({ id: req.params.id })
+      .then(data => {
+        if (!data)
+          return sendError(
+            res,
+            404,
+            'The cohort with specified ID cannot be found',
+          );
+        res.status(200).json(data);
+      })
+      .catch(next);
+  })
+  .put(function(req, res, next) {
+    if (!req.body.name)
+      return sendError(res, 400, 'You need to provide a name to update');
+
+    db('cohorts')
+      .update({ name: req.body.name })
+      .where({ id: req.params.id })
+      .then(data => {
+        if (!data)
+          return sendError(
+            res,
+            404,
+            'The cohort with the specified ID cannot be found',
+          );
+
+        res
+          .status(200)
+          .json({ message: 'Record updated successfully', count: data });
+      })
+      .catch(next);
+  })
+  .delete(function(req, res, next) {
+    db('cohorts')
+      .delete()
+      .where({ id: req.params.id })
+      .then(data => {
+        if (!data)
+          return sendError(
+            res,
+            404,
+            'The cohort with the specified ID cannot be found',
+          );
+
+        res
+          .status(200)
+          .json({ message: 'Record deleted successfully', count: data });
+      })
+      .catch(next);
+  });
+
 module.exports = router;
