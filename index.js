098e348f0d39ac6295ff9432058ad92668141db6
Josue Peralta <jperal77@gmail.com>
2018-09-05 18:21:47 -0500

    added seed and GET endpoint for cohorts

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const knexConfig = require('./knexfile');
 const server = express();
 const db = knex(knexConfig.development);
 
-server.use(helmet());
+server.use(he  lmet());
 server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Hello FSW12');
+} );
+
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+        .select('name')
+        .then(cohorts => {
+            res.status(200).json(cohorts);
+        })
+        .catch(err => res.status(500).json(err));
 } );
 
 server.listen(8000, () => console.log('Running on port 8000'));
