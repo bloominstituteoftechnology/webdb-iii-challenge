e31da7fa68ecfe3efda8f24c7b0801040ce3d1c0
Carey <careycbaldwin@gmail.com>
2018-09-05 19:05:23 -0400

    created and tested post request and added a few cohorts

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ server.get('/', (req,res) => {
 res.send('API is Running');
 });
 
+// add a course
+server.post('/api/cohorts', (req, res) => {
+    const cohort = req.body;
+    // console.log(cohort);
+    db.insert(cohort)
+      .into('cohorts')
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+
+
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
