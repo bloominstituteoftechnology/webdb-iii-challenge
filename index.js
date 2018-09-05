c4da4cef79f9ccf630fa07f378e939fcde30e0b6
LaikaFusion <newrad4@gmail.com>
2018-09-05 16:49:16 -0400

    missed some

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 const express = require('express');
+const helmet = require('helmet');
+const dbhelpers = require("./dbhelpers/helpers");
 
 
 const server = express();
 
+server.use(express.json());
+server.use(helmet());
 
-
+server.get("/api/:whichtable", async (req, res) => {
+  let table = "cohorts";
+  // if(req.params.whichtable ==="b"){
+  //   table = "bears";
+  // }
+  try {
+    const results = await dbhelpers.get(null,table);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+server.get("/api/:whichtable/:cohortID", async (req, res) => {
+  let table = "cohorts";
+  // if(req.params.whichtable ==="b"){
+  //   table = "bears";
+  // }
+  if (!Number(req.params.cohortID)) {
+    res.status(400).json({ errorMessage: "ID not a number" });
+    return;
+  }
+  try {
+    const results = await dbhelpers.get(req.params.cohortID,table);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+server.get("/api/cohorts/:cohortID/students", async (req, res) => {
+  
+  if (!Number(req.params.cohortID)) {
+    res.status(400).json({ errorMessage: "ID not a number" });
+    return;
+  }
+  try {
+    const results = await dbhelpers.studentsIn(req.params.cohortID);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 server.use("/", (req, res) =>
--- seeds/students.js
+++ seeds/students.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('students').del()
-    .then(function () {
+  return knex("students")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('students').insert([
-        {id: 1, name: 'Fred',cohort_id: 1},
-        {id: 2, name: 'Joe',cohort_id: 1},
-        {id: 3, name: 'Selma',cohort_id: 3}
+      return knex("students").insert([
+        { id: 1, name: "Fred", cohort_id: 1 },
+        { id: 2, name: "Joe", cohort_id: 1 },
+        { id: 3, name: "Selma", cohort_id: 3 }
       ]);
     });
 };
