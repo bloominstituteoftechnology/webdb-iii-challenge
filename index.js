11988d552294b1f738465379154e19abd7b175af
sliw <slioweng@gmail.com>
2018-09-06 04:40:33 +0800

    CRUD working

--- index.js
+++ index.js
@@ -12,6 +12,42 @@ server.get('/cohorts', async (req, res) => {
   res.status(200).json(cohorts)
 })
 
+server.get('/cohorts/:id', async (req, res) => {
+  const cohort = await db.select().table('cohorts').where('id', req.params.id)
+
+  if (cohort.length === 0) {
+    res.status(500).json({ message: `cohort ${req.params.id} is not found in database` })  
+  } else {
+    res.status(200).json(cohort)
+  }
+})
+
+server.post('/cohorts', (req, res) => {
+  db.insert(req.body)
+    .into('cohorts')
+    .then(id => res.status(201).json(id[0]))
+    .catch(error => res.status(500).json(error))
+})
+
+server.delete('/cohorts/:id', (req, res) => {
+  db.table('cohorts')
+    .where('id', req.params.id)
+    .del()
+    .then(id => res.status(201).json({ message: "deleted successfully" }))
+    .catch(error => res.status(500).json(error))
+})
+
+server.put('/cohorts/:id', (req, res) => {
+  db.table('cohorts')
+    .where('id', req.params.id)
+    .update({
+      id: req.params.id,
+      ...req.body
+    })
+    .then(id => res.status(201).json({ message: "updated successfully" }))
+    .catch(error => res.status(500).json(error))
+})
+
 server.listen('3300', () => {
   console.log(`\n=== Web API Listening on http://localhost:3300 ===\n`)
 })
