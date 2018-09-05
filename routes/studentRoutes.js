319fb2e95c45fa2b8e741e883108fb46c2e8d250
Lauren <llwor94@gmail.com>
2018-09-05 11:58:16 -0700

    student put and post methods working

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -22,4 +22,52 @@ router.get('/:id', (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.post('/', (req, res, next) => {
+  const body = req.body;
+  if (!(body.name && body.cohort_id)) {
+    return next({ code: 400 });
+  }
+  db('cohorts')
+    .where({ id: body.cohort_id })
+    .then(cohort => {
+      if (cohort.length < 1) {
+        return next({ code: 404 });
+      }
+    });
+  db.insert(body)
+    .into('students')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => next(err));
+});
+
+router.put('/:id', (req, res, next) => {
+  const body = req.body;
+  if (!(body.name && body.cohort_id)) {
+    return next({ code: 400 });
+  }
+  db('cohorts')
+    .where({ id: body.cohort_id })
+    .then(cohort => {
+      if (cohort.length < 1) {
+        return next({ code: 404 });
+      }
+    });
+  db('students')
+    .where({ id: req.params.id })
+    .update(body)
+    .then(response => {
+      if (!response) {
+        return next({ code: 404 });
+      }
+      res.status(200).json({
+        id: req.params.id,
+        name: body.name,
+        cohort_id: body.cohort_id,
+      });
+    })
+    .catch(err => next(err));
+});
+
 module.exports = router;
