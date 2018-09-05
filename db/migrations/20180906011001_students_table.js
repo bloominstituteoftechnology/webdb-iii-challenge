e0f3e1f8aa673c04ccf1c93ac5c3b64213c2d266
kamalnrf <kamalnrf@hotmail.com>
2018-09-06 01:21:46 +0530

    Add students db

--- db/migrations/20180906011001_students_table.js
+++ db/migrations/20180906011001_students_table.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-    
+    return knex.schema.createTable(`students`, table => {
+        table.increments(`id`).primary();
+        table
+            .string(`name`, 128)
+            .notNullable()
+        table
+            .integer(`cohort_id`)
+            .unsigned()
+            .notNullable()
+            .references(`id`)
+            .inTable(`cohorts`)        
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable(`students`)
 };
