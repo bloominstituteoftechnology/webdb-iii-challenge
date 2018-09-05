a649ceb93d7b80d6e1c70784eb732d3e308c8509
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-05 17:22:14 -0400

    students route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 //routes
 const cohortRoute = require('./cohorts');
-// const studentRoute = require('./students');
+const studentRoute = require('./students');
 
 const router = express.Router();
 
 router.use('/cohorts', cohortRoute);
-// router.use('/students', studentRoute);
+router.use('/students', studentRoute);
 
 module.exports = router;
