684ee3ddd163e939bc13872d7e38d47b07e8db9b
Alberto-Funk <laptopreneur@gmail.com>
2018-09-05 14:35:58 -0700

    students table

---
+++ db/migrations/20180905142411_students_table.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  // implement change you want in db
+  return knex.schema.createTable('students', function(tbl) {
+    // generates a primary key called id and makes it auto-increment
+    tbl.increments();
+
+    tbl
+      .string('name', 128)
+      .notNullable()
+      .unique('name');
+
+    // foreign key
+    tbl
+    .integer('cohort_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('cohorts');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  // undo change made to db, rollback
+  return knex.schema.dropTable('students');
+};
