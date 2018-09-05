5375a16a56e41b92b1f49a709bd7795777ac3b7d
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-05 16:14:04 -0500

    Working on Tables
    Trying to create the tables so that they show up in the DB Browser, I've created a GET /cohorts but all I get is an error on Postman

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ server.get("/", (req, res) => {
   res.send("Running...");
 });
 
+server.get("/cohorts", (req, res)=>{
+    db("cohorts")
+    .then(cohorts => res.status(200).json(cohorts))
+    .catch(err => res.status(500).json(err))
+})
+
 server.listen(4000, function(){
     console.log("//======Server 4k=========//")
 });
