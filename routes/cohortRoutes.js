ddb1e687375eade8f0d6fd5974c64f8949dab5ca
Lauren <llwor94@gmail.com>
2018-09-05 11:45:53 -0700

    all cohort routes working

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -34,4 +34,47 @@ router.get('/:id/students', (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.post('/', (req, res, next) => {
+  const body = req.body;
+  if (!body.name) {
+    return next({ code: 400 });
+  }
+  db.insert(body)
+    .into('cohorts')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => next(err));
+});
+
+router.put('/:id', (req, res, next) => {
+  let body = req.body;
+  if (!body.name) {
+    return next({ code: 400 });
+  }
+  db('cohorts')
+    .where({ id: req.params.id })
+    .update(body)
+    .then(response => {
+      if (!response) {
+        return next({ code: 404 });
+      }
+      res.status(200).json({ id: req.params.id, name: body.name });
+    })
+    .catch(err => next(err));
+});
+
+router.delete('/:id', (req, res, next) => {
+  db('cohorts')
+    .where({ id: req.params.id })
+    .del()
+    .then(data => {
+      if (!data) {
+        return next({ code: 404 });
+      }
+      res.status(200).json({ id: req.params.id });
+    })
+    .catch(err => next(err));
+});
+
 module.exports = router;
