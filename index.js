c3ad8e09d4c1ff4eff3042e6400c19d19655e460
szincone <sawyerzincone@gmail.com>
2018-09-05 14:47:44 -0600

    all files added, REVERT HERE, trying rollback, second seed command won't run

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ server.use(express.json());
 //   res.send("API Running...");
 // });
 
+// cohorts//////////////////////
 // GET
-server.get("/", (req, res) => {
+server.get("/cohorts", (req, res) => {
   db("cohorts")
     .then(cohorts => {
       res.status(200).json(cohorts);
@@ -25,6 +26,20 @@ server.get("/", (req, res) => {
       res.status(500).json({ error: "The cohorts could not be retrieved." });
     });
 });
+// end cohorts//////////////////////
+
+// students//////////////////////
+// GET
+server.get("/students", (req, res) => {
+  db("students")
+    .then(students => {
+      res.status(200).json(students);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The students could not be retrieved." });
+    });
+});
+// end students//////////////////////
 
 const port = 3300;
 server.listen(port, function() {
