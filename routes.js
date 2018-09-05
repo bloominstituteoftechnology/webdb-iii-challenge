9d9611c58362bd59d4ac659a119febf1757fcca1
Ashwin <aswinjeganath@gmail.com>
2018-09-06 01:43:06 +0530

    Implement route to retrieve all cohorts

--- routes.js
+++ routes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('knex')(require('./knexfile').development);
+
+router.route('/cohorts').get(function(_, res, next) {
+  db('cohorts')
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(next);
+});
+
 module.exports = router;
