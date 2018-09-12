6ca5c625bd2c8738a83944f2a28587f8210ecf27
rverdi642 <rverdi642@gmail.com>
2018-09-12 17:50:53 -0500

    just need to finish a result based on a foreign key

---
+++ index.js
@@ -0,0 +1,188 @@
+const express = require('express')
+server = express()
+// cors = require('cors')
+port = 8000
+
+const helmet = require ('helmet')
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
+// const db = knex({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: './data.sqlite3',
+//   },
+//   useNullAsDefault: true,
+// });
+
+
+server.use(helmet())
+server.use(express.json())
+
+// server.get('/',(req,res)  => {
+//     res.send('API Up & Running');
+// });
+
+server.get('/api/cohorts', async (req, res, next) => {
+    try {
+      const cohort = await db(`SELECT * FROM cohorts`); // <=
+      res.send(cohort);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  // server.get('/api/cohorts', (req, res) => {
+    
+  //     db('cohorts').then(cohorts => {
+  //         res.status(200).json(cohorts);
+  //     }) 
+    
+  //    .catch (err => res.status(500).json(err))  
+  // });
+
+  server.get('/api/cohorts/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+  
+    db('cohorts')
+      .where('id', '=', id) // or .where({ id: id })
+     
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
+
+server.post('/api/cohorts', (req, res) => {
+    const character = req.body;
+  
+    db.insert(character)
+      .into('cohorts')
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+  server.put('/api/cohorts/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+  
+    db('cohorts')
+      .where('id', '=', id) // or .where({ id: id })
+      .update(changes)
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
+  server.delete('/api/cohorts/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('cohorts')
+      .where({ id }) // or .where(id, '=', id)
+      .del()
+      .then(count => {
+        // count === number of records deleted
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+server.get('/api/students', (req, res) => {
+    
+      db('students').then(students => {
+          res.status(200).json(students);
+      }) 
+    
+     .catch (err => res.status(500).json(err))  
+  });
+
+  server.get('/api/students/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+  
+    db('students')
+      .where('id', '=', id) // or .where({ id: id })
+     
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
+
+server.post('/api/students', (req, res) => {
+    const character = req.body;
+  
+    db.insert(character)
+      .into('students')
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+  server.put('/api/students/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+  
+    db('students')
+      .where('id', '=', id) // or .where({ id: id })
+      .update(changes)
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
+  server.delete('/api/students/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db('students')
+      .where({ id }) // or .where(id, '=', id)
+      .del()
+      .then(count => {
+        // count === number of records deleted
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
+
+
+
+
+
+
+server.listen(port, () => console.log(`== Server Running on Port ${port} ==`))
