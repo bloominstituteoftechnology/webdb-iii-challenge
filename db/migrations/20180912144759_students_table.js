1e18b0ae6c54450b6bdd063fae55da096642418d
keegn <keegan.t.burkett@gmail.com>
2018-09-12 15:55:33 -0500

    created seed for students

--- db/migrations/20180912144759_students_table.js
+++ db/migrations/20180912144759_students_table.js
@@ -1,15 +1,22 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('students', function(tbl) {
+  return knex.schema.createTable("students", function(tbl) {
     tbl.increments(); // generates a primary key called id and makes it autoincrement
-    tbl.string('name', 128).notNullable().unique('name');
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique("uq_student_name");
     // cohort_id references the id in the cohorts table
-    tbl.integer("cohort_id").unsigned().notNullable().references("id").inTable("cohorts");
+    tbl
+      .integer("cohort_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("cohorts");
   });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('students');
+  return knex.schema.dropTable("students");
 };
 
 // knex migrate:make students_table
