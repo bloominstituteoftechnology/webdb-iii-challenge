0611bf1997797f2e25be5a8f08511dd6de15a5df
keegn <keegan.t.burkett@gmail.com>
2018-09-12 13:13:20 -0500

    added first cohort using postman post request

--- db/migrations/20180912123624_cohorts_table.js
+++ db/migrations/20180912123624_cohorts_table.js
@@ -1,8 +1,8 @@
 exports.up = function(knex, Promise) {
   // implement the change we want in our db
-  return knex.schema.createTable("cohorts", function(tbl) {
+  return knex.schema.createTable('cohorts', function(tbl) {
     tbl.increments(); // generates a primary key called id and makes it autoincrement
-    tbl.string("name", 128).notNullable();
+    tbl.string('name', 128).notNullable().unique('name';
   });
 };
 
--- index.js
+++ index.js
@@ -17,6 +17,74 @@ server.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+// post - create a new cohort in the cohorts table
+server.post("/api/cohorts", (req, res) => {
+    const cohort = req.body;
+    // insert into cohorts
+    db.insert(cohort)
+      .into("cohorts")
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+  
+  // get - read the array of cohort objects
+  server.get("/api/cohorts", (req, res) => {
+  
+    db("cohorts")
+      .select("name")
+      .then(cohorts => {
+        res.status(200).json(cohorts);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+  
+  // get by id - read the cohort associated with the given id
+  server.get("/api/cohorts/:id", (req, res) => {
+    const { id } = req.params;
+  
+    db("cohorts")
+      .where("id", "=", id)
+      .then(cohorts => {
+        res.status(200).json(cohorts);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+  
+  // put
+  server.put("/api/cohorts/:id", (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+  
+    db("cohorts")
+      .where("id", "=", id) // or .where({ id: id })
+      .update(changes)
+      .then(count => {
+        // count === number of records updated
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+  // delete
+  server.delete("/api/cohorts/:id", (req, res) => {
+    const { id } = req.params;
+  
+    db("cohorts")
+      .where({ id }) // or .where(id, '=', id)
+      .del()
+      .then(count => {
+        // count === number of records deleted
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
 
 // Port setup
 const port = 3300;
