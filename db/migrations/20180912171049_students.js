f501456a7fd9c71d38352f126c2cd5217b05a7e2
vanceleon <vanceleon44@gmail.com>
2018-09-12 18:10:30 -0400

    adding latest and setup seeds

--- db/migrations/20180912171049_students.js
+++ db/migrations/20180912171049_students.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("students", function(tbl) {
+    tbl.increments();
+
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique("uq_students_id");
+
+    tbl
+      .integer("cohort_id")
+      .notNullable()
+      .references("id")
+      .inTable("cohort");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("cohort");
 };
