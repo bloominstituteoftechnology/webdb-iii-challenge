f4b8cfa5150846fc4c1813bfc16cf706af99c4d1
bri79319 <bri79319@gmail.com>
2018-09-13 13:40:13 -0400

    changes

--- db/seeds/01-students.js
+++ db/seeds/01-students.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex("students").insert([
-        { name: "Larry", cohort_id: "AD-12" },
-        { name: "Jerry", cohort_id: "DQ-31" },
-        { name: "Tim", cohort_id: "UP-78" }
+        { name: "Larry", cohort_id: "12" },
+        { name: "Jerry", cohort_id: "31" },
+        { name: "Tim", cohort_id: "78" }
       ]);
     });
 };
