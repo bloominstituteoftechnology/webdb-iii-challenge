6415b4296c8fc6c8ca347e38a24cd033b029b031
Synch3 <jonsimo@aol.com>
2018-09-13 10:39:21 -0400

    Not really working

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api', require('./api'));
+
+
+const port = 8000;
+server.listen(port, () => console.log(`==== Server listening on port ${port} ====`));
