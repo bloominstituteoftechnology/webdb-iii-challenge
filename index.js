8f2e0cb0c09ca4bd41f41f62f079faeaa5985d19
Kairi <kairijb@gmail.com>
2018-09-13 03:31:20 -0500

    created routes

--- index.js
+++ index.js
@@ -15,7 +15,46 @@ server.get('/', (req, res) => {
   res.send('Api Online')
 })
 
-
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+    .then(cohorts => {
+      res.status(200).json(cohorts)
+    })
+    .catch(err => res.status(500).json(err))
+  })
+  
+  server.post('/api/cohorts', (req, res) => {
+    db.insert(cohorts)
+    .into('cohorts')
+    .then(id => {
+      res.status(201).json(id)
+    })
+    .catch(err => res.status(500).json(err))
+  })
+  
+  server.put('/api/cohorts/:id', (req, res) => {
+    const {id} = req.params
+  
+    db('cohorts')
+    .where({id}).update(req.body)
+    .then(count => {
+      res.status(200).json(count)
+    })
+    .catch(err => res.status(500).json(err))
+  })
+  
+  server.delete('/api/cohorts/:id', (req, res) => {
+    const { id } = req.params;
+     db("cohorts")
+      .where({ id })
+      .del()
+      .then(count => {
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  })
 
 
 
