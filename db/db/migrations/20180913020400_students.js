6e9001bc6a69a93775a17bb9a52ab68a1121eec7
Kairi <kairijb@gmail.com>
2018-09-13 03:17:55 -0500

    added cohort id to student table that references id in cohort

--- db/db/migrations/20180913020400_students.js
+++ db/db/migrations/20180913020400_students.js
@@ -3,7 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('students', function(tbl) {
       tbl.increments();
 
-      tbl.string('name', 128).notNullable().unique('name')
+      tbl.string('name', 128).notNullable().unique('name');
+
+      tbl.integer('cohort_id').unsigned().notNullable().references('id').inTable('cohorts');
   })
 };
 
