4634fd4b4892e7bc7a69e8dab585b88fe4a9323f
cubsfan46350 <cubsfan.46350@msn.com>
2018-09-13 19:31:12 -0500

    successfully implemented post and get requests

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ server.use(express.json());
 const db = knex(dBConfig.development);
 
 //endpoints
+
+server.post('/api/cohorts', (req, res) => {
+    const cohort = req.body;
+    db.insert(cohort).into('cohorts').then(res => res.status(201).json(res))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts').then(cohorts => {
+        res.status(200).json(cohorts);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+
+server.listen(3000, console.log('Listening on Port 3000'));
