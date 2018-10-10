9df4a8d8acd9e06b37a45106c55cb2faef60299c
m-j-hacker <mjhacker@gmail.com>
2018-10-10 14:39:34 -0500

    Began filling out cohorts folder with routes and helper functions

---
+++ cohorts/cohortsRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const cohorts = require('./cohortsModel.js');
+
+const router = express.Router();
+
+// get list of cohorts
+
+// get a cohort by id
+
+// get list of all students in given cohort
+
+// create a cohort
+
+// update a cohort
+
+// delete a cohort
