ed2f21e98cb21ec4463dd431f4b9325cf188e1c1
Timothy Hoang <timh1203@gmail.com>
2018-10-10 16:27:50 -0400

    Add students seed

---
+++ data/seeds/02-students.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('students')
+    .truncate()
+    .then(function () {
+      return knex('students').insert([
+        {name: 'George Washington', cohort_id: '1'},
+        {name: 'James Polk', cohort_id: '1'},
+        {name: 'John Adams', cohort_id: '1'},
+        {name: 'Thomas Jefferson', cohort_id: '2'},
+        {name: 'Abraham Lincoln', cohort_id: '3'},
+      ]);
+    });
+};
