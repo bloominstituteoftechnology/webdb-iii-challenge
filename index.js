d7cdb6d682d7a0b2d07f6f4ba0e4368ef9d396cc
BrianARuff <thelonewolf198927@gmail.com>
2018-10-10 17:00:33 -0400

    PUT and DELETE routing / db interactions implemented

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-const knexConfig = require('./knexfile');
-
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 
 // init server
 const server = express();
@@ -14,42 +13,62 @@ const db = knex(knexConfig.development);
 server.use(express.json());
 server.use(helmet());
 
-server.post('/api/cohorts', (req, res) => {
+server.post("/api/cohorts", (req, res) => {
   const { name } = req.body;
   const cohort = { name };
   db.insert(cohort)
-    .into('cohorts')
+    .into("cohorts")
     .then(postedCohort => res.status(201).json(postedCohort))
     .catch(err => res.status(500).json(err));
 });
 
-server.get('/api/cohorts', (req, res) => {
-  db('cohorts')
+server.get("/api/cohorts", (req, res) => {
+  db("cohorts")
     .then(cohorts => res.status(200).json(cohorts))
     .catch(err => res.status(500).json(err));
 });
 
-server.get('/api/cohorts/:id', (req, res) => {
+server.get("/api/cohorts/:id", (req, res) => {
   const { id } = req.params;
-  db('cohorts')
-    .where({id})
+  db("cohorts")
+    .where({ id })
     .first()
     .then(cohort => res.status(200).json(cohort))
     .catch(err => res.status(500).json(err));
 });
 
-server.get('/api/cohorts/:id/students', (req, res) => {
+server.get("/api/cohorts/:id/students", (req, res) => {
   const { id } = req.params;
-  db('cohorts')
-    .where({id})
+  db("cohorts")
+    .where({ id })
     .first()
     .then(cohort => {
-      db('students')
-        .where({cohort_id: id})
+      db("students")
+        .where({ cohort_id: id })
         .then(students => res.status(200).json(students))
         .catch(err => res.status(500).json(err));
     })
     .catch(err => res.status(500).json(err));
+});
+
+server.put("/api/cohorts/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const cohort = { name };
+  db("cohorts")
+    .where({ id })
+    .update(cohort)
+    .then(bool => res.status(200).json(bool))
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/cohorts/:id', (req, res) => {
+  const { id } = req.params;
+  db('cohorts')
+    .where({id})
+    .del()
+    .then(bool => res.status(200).json(bool))
+    .catch(err => res.status(200).json(err));
 })
 
 // init server listener
