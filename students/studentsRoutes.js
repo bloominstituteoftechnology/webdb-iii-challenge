689c325bfb9735fb58d7ada1a5dd192470d338be
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-10 18:26:36 -0400

    GET student of a specific ID endpoint is complete

--- students/studentsRoutes.js
+++ students/studentsRoutes.js
@@ -16,4 +16,21 @@ router.get("/", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+// get a student by id
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const student = await students.findById(id);
+
+    if (student) {
+      res.status(200).json(student);
+    } else {
+      res.status(404).json({ message: "Student not found" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
