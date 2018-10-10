c21161b47a9f85b6c74be3f44fb459131c25075d
Michael Agard <mdagard@gmail.com>
2018-10-10 16:09:59 -0400

    routes and models setup for courses and students

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const port = 7100;
 const name = `RDBMS-API-Full`;
-// const cohortsRoutes = require('./routes/cohortsRoutes');
-// const studentsRoutes = require('./routes/studentsRoutes');
+const cohortsRoutes = require('./routes/cohortsRoutes');
+const studentsRoutes = require('./routes/studentsRoutes');
 const server = express();
 
 server.use(helmet());
@@ -13,7 +13,7 @@ server.get('/', (req, res) => {
   res.send(`${name} running on port ${port}`);
 });
 
-// server.use('/api/cohorts', cohortsRoutes);
-// server.use('/api/students', studentsRoutes);
+server.use('/api/cohorts', cohortsRoutes);
+server.use('/api/students', studentsRoutes);
 
 server.listen({ port }, () => console.log(`## ${name} running on port ${port} ##`));
