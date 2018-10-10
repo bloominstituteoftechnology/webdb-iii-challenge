32fea87509aa31a63bd9bb5d04d16a775cac4eaf
m-j-hacker <mjhacker@gmail.com>
2018-10-10 16:18:42 -0500

    added get student by id endpoint

--- students/studentsRoutes.js
+++ students/studentsRoutes.js
@@ -15,6 +15,21 @@ router.get('/', (req, res) => {
 });
 
 // get a student by id
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const student = await students.findById(id);
+
+        if (student) {
+            res.status(200).json(student);
+        } else {
+            res.status(404).json({ message: 'Student with specified ID not found' });
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 // add new student to database
 
