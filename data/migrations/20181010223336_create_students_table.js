302e730df23b6e8cd1a3c132d967b5a7e92d87dd
decagondev <decagondevelopment@gmail.com>
2018-10-10 22:44:17 +0100

    (MVP - SETUP) : added schema to the create students table migration

--- data/migrations/20181010223336_create_students_table.js
+++ data/migrations/20181010223336_create_students_table.js
@@ -1,5 +1,14 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("students", function(table) {});
+  return knex.schema.createTable("students", function(table) {
+    table.increments();
+    table.string("name", 256).notNullable();
+    table
+      .integer("cohort_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("cohorts");
+  });
 };
 
 exports.down = function(knex, Promise) {
