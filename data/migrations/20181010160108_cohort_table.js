049a10690c918c8db8db307bdfa5d410eba62ceb
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-10 16:14:56 -0500

    Knex file created.  Cohort/Student migrations implemented.

---
+++ data/migrations/20181010160108_cohort_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('cohorts', function(table) {
+    table.increments();
+    table
+      .string('name', 128)
+      .notNullable()
+      .unique('name');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('cohorts');
+};
