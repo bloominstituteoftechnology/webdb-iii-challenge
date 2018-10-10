77ccede26de94ebfa9b6265d0077a7c228dad5df
decagondev <decagondevelopment@gmail.com>
2018-10-10 22:37:20 +0100

    (MVP - SETUP) : created skeleton for migration create students table

--- data/migrations/20181010223336_create_students_table.js
+++ data/migrations/20181010223336_create_students_table.js
@@ -1,8 +1,7 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("students", function(table) {});
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("students");
 };
