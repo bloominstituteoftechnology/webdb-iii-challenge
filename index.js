eed43ee57d80de7a81e469517d1476711b8560d2
decagondev <decagondevelopment@gmail.com>
2018-10-10 23:57:46 +0100

    (STRETCH in 45 seconds) : refactored the cohort model to be a student model then linked it all up and your done :)

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const helmet = require("helmet");
 server.use(express.json());
 server.use(helmet());
 
+const cohortsModel = require("./api/cohortsModel.js");
+const studentsModel = require("./api/studentsModel.js");
+
+server.use("/api/cohorts", cohortsModel);
+server.use("/api/students", studentsModel);
+
 server.get("/", (req, res) => {
   res.send("API / endpoint is working");
 });
