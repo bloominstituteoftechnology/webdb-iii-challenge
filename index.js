8480622b7b8531ec00ef94567f5adf6e9684dc3b
Alexandra <alexswartz9753@gmail.com>
2018-10-10 17:12:00 -0400

    changed cohort/students endpoint comparing id to cohort id, still not working

--- index.js
+++ index.js
@@ -62,16 +62,20 @@ server.get('/api/cohorts/:id', async (req, res)=> {
     }
 });
 
-server.get('/api/cohorts/:cohort_id/students', (req, res)=> {
-    const {cohort_id} = req.params;
+server.get('/api/cohorts/:id/students', (req, res)=> {
+    const {id} = req.params;
     db('cohorts')
-        .where({cohort_id})
+        .where({id})
         .first()
         .then(cohortStudents=> {
-           if (cohortStudents === 0) {
-               res.status(404).json({message: "Nothing to display"});
-           } 
-           res.status(200).json(cohortStudents);
+            const {cohort_id} = req.params;
+            db('students')
+                .where({cohort_id})
+                .then(cohortStudents=> {
+                    if ({cohort_id} === {id}) {
+                        res.status(200).json(cohortStudents);
+                    }
+                 })
         })
         .catch(err=> {
             res.status(500).json({error: "This information could not be retrieved from the database"});
