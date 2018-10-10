d1dba4f74e744c8e14000348814c9d08882a1f1a
Andrew <ghobrial.andrew@gmail.com>
2018-10-10 17:51:28 -0400

    completed migrations

---
+++ data/migrations/20181010161522_cohorts_table.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("cohorts", function(tbl) {
+    tbl.increments();
+    tbl.string("name", 128).notNullable();
+    tbl.unique("name");
+  });
+};
+ exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("cohorts");
+};
