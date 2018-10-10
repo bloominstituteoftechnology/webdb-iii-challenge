83d9347281ffc67dbf0451039d49c8eb4f36d59c
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-10 17:14:38 -0400

    Built Cohort data model file

---
+++ cohorts/cohortsModel.js
@@ -0,0 +1,48 @@
+const knex = require("knex");
+
+const knexConfig = require("../knexfile.js");
+const db = knex(knexConfig.development);
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove,
+  findByCohort
+};
+
+function find() {
+  return db("cohorts");
+}
+
+function findById() {
+  return db("cohorts")
+    .where({ id })
+    .first();
+}
+
+function add(cohort) {
+  return db("cohorts")
+    .insert(cohort)
+    .into("cohorts");
+}
+
+function update(id, changes) {
+  return db("cohorts")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("cohorts")
+    .where({ id })
+    .del();
+}
+
+function findByCohort(cohort_id) {
+  return db("students")
+    .join("cohorts", "cohorts.id", "students.cohort_id")
+    .select("students.id", "students.name", "cohorts.name")
+    .where("students.cohort_id", cohort_id);
+}
