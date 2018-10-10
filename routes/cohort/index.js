508cdb586fb209b07fb5099da06547aad40ad8d7
unknown <cornielleandres@gmail.com>
2018-10-10 15:31:01 -0400

    Add GET request handler for cohort with given ID.

--- routes/cohort/index.js
+++ routes/cohort/index.js
@@ -10,4 +10,15 @@ router.get('/', (req, res) => {
 		.catch(err => res.status(500).json({ error: `Server could not get cohort information: ${ err }` }));
 });
 
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	cohortDb
+		.get(id)
+		.then(cohorts => {
+			if (cohorts.length) return res.status(200).json(cohorts);
+			return res.status(404).json({ error: `Cohort with ID ${ id } does not exist.` });
+		})
+		.catch(err => res.status(500).json({ error: `Server could not get information for cohort with ID ${ id }: ${ err }` }));
+});
+
 module.exports = router;
