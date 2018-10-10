a59d93df62d06d51ebd69c1c0599dd051f80adbc
iAmAdamReid <adam@adamreid.me>
2018-10-10 15:42:35 -0700

    CRUD for students endpoints

--- index.js
+++ index.js
@@ -130,6 +130,88 @@ server.get('/api/cohorts/:id/students', (req, res) => {
 
 
 
+/*** STUDENTS API ENDPOINTS ***/
+
+server.get('/students', (req, res) => {
+    db('students')
+    .then(students => {
+        res.status(200).json(students);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+server.get('/students/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const student = await db('students').where({id}).first();
+        if(student){
+            res.status(200).json(student);
+        } else {
+            res.status(404).json({error: `Could not find student with ID ${id}.\n`})
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+server.post('/students', (req, res) => {
+    const student = req.body;
+
+    db.insert(student).into('students')
+    .then(ids => {
+        res.status(201).json(ids[0])
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+server.put('/students/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    db('students').where({id}).update(changes)
+    .then(count => {
+        if(!count || count < 1){
+            res.status(404).json({error: `Could not find student with ID ${id}.\n`})
+        } else {
+            res.status(200).json(count);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+server.delete('/students/:id', (req, res) => {
+    const {id} = req.params;
+
+    db('students').where({id}).del()
+    .then(count => {
+        if(!count || count < 1){
+            res.status(404).json({error: `Could not delete student ${id}.`})
+        } else {
+            res.status(200).json(count);
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+// [POST] /students This route should save a new student to the database.
+// [GET] /students This route will return an array of all students.
+// [GET] /students/:id This route will return the student with the matching id.
+// [PUT] /students/:id This route will update the student with the matching id using information sent in the body of the request.
+// [DELETE] /students/:id This route should delete the specified student.
+
 
 const port = 9000;
 server.listen(port, function() {
