8e67ff2d90f30d3fda8621eb1b35d9b8e620a1f6
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-10 16:03:25 -0600

    Implement CREATE Cohort Endpoint
    which receives a cohort object and creates a new cohort in the database.

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-/// ---- Node Dependencies ----
+/// ----- Node Dependencies -----
 const express = require('express');
 const helmet = require('helmet');
+const knexConfig = require('./knexfile');
+const knex = require('knex');
 
-/// ---- Initialize Server ----
+/// ----- Initialize Server -----
 const server = express();
 
-/// ---- Connect Middleware to Server ----
+/// ----- Initialize Database -----
+const db = knex(knexConfig.development)
+
+/// ----- Connect Middleware to Server -----
 server.use(helmet(), express.json());
+
 
 ///// ---------- CRUD ENDPOINTS ----------
 
-/// ----- Root Server GET Endpoint -----
+/// ----- Root Server READ Endpoint -----
 server.get('/', (request, response) => {
     response.send("Dance magic, dance.")
+})
+
+
+/// ----- CREATE Cohort Endpoint ----- 
+server.post('/api/cohorts', (request, response) => {
+    
+    // Deconstruct Request Body
+    const { name } = request.body;
+
+    // Database Promise Functions
+    db.insert({ name })
+    .into('cohorts')
+    .then( ids => {
+        response.status(201).json(ids)
+    })
+    .catch(error => response.json(error))
 })
 
 const port = 9999;
