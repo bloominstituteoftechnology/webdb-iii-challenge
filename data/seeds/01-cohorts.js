2150c90d36c80710c9cbac46ea146b43bd4ecff6
decagondev <decagondevelopment@gmail.com>
2018-10-10 22:59:51 +0100

    (MVP - SETUP) : added data for seed cohorts file

--- data/seeds/01-cohorts.js
+++ data/seeds/01-cohorts.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("cohorts")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("cohorts").insert([
+        { name: "Full Stack Web Infinity" },
+        { name: "Full Stack Web Delta PI" },
+        { name: "FSW13 ULTIMATE FORM" }
       ]);
     });
 };
