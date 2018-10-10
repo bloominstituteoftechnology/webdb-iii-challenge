1a5cb0ae0af6798e52313a0ff26c3cebed0d3cb3
BrianARuff <thelonewolf198927@gmail.com>
2018-10-10 19:01:04 -0400

    added GET GETONE and POST for stretch goals

--- index.js
+++ index.js
@@ -37,18 +37,10 @@ server.get("/api/cohorts/:id", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-server.get("/api/cohorts/:id/students", (req, res) => {
+server.get("/api/cohorts/:id/students", async (req, res) => {
   const { id } = req.params;
-  db("cohorts")
-    .where({ id })
-    .first()
-    .then(cohort => {
-      db("students")
-        .where({ cohort_id: id })
-        .then(students => res.status(200).json(students))
-        .catch(err => res.status(500).json(err));
-    })
-    .catch(err => res.status(500).json(err));
+  const students = await db("students").where({ cohort_id: id });
+  res.status(200).json(students);
 });
 
 server.put("/api/cohorts/:id", (req, res) => {
@@ -62,14 +54,38 @@ server.put("/api/cohorts/:id", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-server.delete('/api/cohorts/:id', (req, res) => {
+server.delete("/api/cohorts/:id", (req, res) => {
   const { id } = req.params;
-  db('cohorts')
-    .where({id})
+  db("cohorts")
+    .where({ id })
     .del()
     .then(bool => res.status(200).json(bool))
     .catch(err => res.status(200).json(err));
-})
+});
+
+server.get("/api/students", (req, res) => {
+  db("students")
+    .then(students => res.status(200).json(students))
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/api/students/:id", (req, res) => {
+  const { id } = req.params;
+  db("students")
+    .where({ id })
+    .first()
+    .then(student => res.status(200).json(student))
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/api/students/', (req, res) => {
+  const {name, cohort_id} = req.body;
+  const student = {name, cohort_id};
+  db.insert(student)
+    .into('students')
+    .then(newStudent => res.status(201).json(newStudent))
+    .catch(err => res.status(500).json(err));
+});
 
 // init server listener
 server.listen(9000, () => console.log(`API running on port 9000`));
