33a0a098f2f15083d89724f61798351a783b7214
iAmAdamReid <adam@adamreid.me>
2018-10-10 14:22:30 -0700

    Add cohorts table

---
+++ migrations/20181010135421_add_cohorts_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cohorts', function(tbl) {
+        tbl.increments();
+        tbl.string('name', 255)
+        .notNullable()
+        tbl.timestamp('createdAt').defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('cohorts');
+};
+
+// cohorts table
+// id: primary key, autoincrements.
+// name: text, required.
