a9d6888a43c3c66fd2ec8599d18c8e9dd2333430
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-10 14:39:52 -0400

    Seeding/Test used knex seeding feature to add test data to both tables

---
+++ data/seeds/cohort.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('cohorts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cohorts').insert([
+        {name: 'FSW13'},
+        {name: 'FSW14' }
+      ]);
+    });
+};
