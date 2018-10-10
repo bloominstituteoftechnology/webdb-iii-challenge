f176066b44dad7c16174352f3cdcc8315a72e319
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-10 15:52:12 -0600

    Implement Root Server GET Enpoint
    which is intended to ensure that nodemon and express are set up correctly.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+/// ---- Node Dependencies ----
 const express = require('express');
 const helmet = require('helmet');
 
+/// ---- Initialize Server ----
 const server = express();
+
+/// ---- Connect Middleware to Server ----
 server.use(helmet(), express.json());
+
+///// ---------- CRUD ENDPOINTS ----------
+
+/// ----- Root Server GET Endpoint -----
+server.get('/', (request, response) => {
+    response.send("Dance magic, dance.")
+})
 
 const port = 9999;
 server.listen(port, () => {console.log(`#### Server active on port ${port} ####\n`)})
