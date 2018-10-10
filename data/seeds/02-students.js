62f3fc78ac73835f9737878a7066e036df798686
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-10 16:48:04 -0400

    used knex to add seed data for cohorts and student tables

---
+++ data/seeds/02-students.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('students')
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex('students').insert([
+        { name: 'Caleb', cohort_id: 1 },
+        { name: 'John', cohort_id: 3 },
+        { name: 'Debby', cohort_id: 2 },
+      ]);
+    });
+};
