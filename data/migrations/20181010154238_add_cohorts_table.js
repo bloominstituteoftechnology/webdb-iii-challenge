6007bcfc02e6901568e2e94e4027c33f612bedf2
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-10 16:33:26 -0400

    Added migration for creating student table

--- data/migrations/20181010154238_add_cohorts_table.js
+++ data/migrations/20181010154238_add_cohorts_table.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("cohorts", function(tbl) {
+    tbl.increments();
+
+    tbl.string("name").notNullable();
+
+    tbl.unique("name");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("cohorts");
 };
