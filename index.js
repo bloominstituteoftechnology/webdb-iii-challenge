dde00609c15b338867b4e7a6b4371c746608440f
alejandrok93 <alejandrok93@gmail.com>
2018-10-10 14:20:26 -0700

    Finished API endpoints for students

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const helmet = require("helmet");
 const server = express();
 
 const cohortsRoutes = require("./routes/cohortsRouter.js");
+const studentsRoutes = require("./routes/studentsRouter.js");
+
 // middleware
 server.use(helmet());
 server.use(express.json());
 server.use("/api/cohorts/", cohortsRoutes);
-
+server.use("/api/students/", studentsRoutes);
 //connect to db
 // const knexConfig = require("./knexfile.js");
 // const db = knex(knexConfig.development);
