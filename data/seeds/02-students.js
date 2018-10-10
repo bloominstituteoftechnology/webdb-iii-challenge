1680a58eb73014843dd952cec4293532c358f6d5
decagondev <decagondevelopment@gmail.com>
2018-10-10 23:04:00 +0100

    (MVP - SETUP) : added data for seed students file

--- data/seeds/02-students.js
+++ data/seeds/02-students.js
@@ -1,13 +1,18 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("students")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("students").insert([
+        { name: "Bob the Builder", cohort_id: 1 },
+        { name: "Dave the Developer", cohort_id: 1 },
+        { name: "Max the Mariachi Band Leader", cohort_id: 1 },
+        { name: "Pablo the Programmer", cohort_id: 2 },
+        { name: "Steve the SQL Admin", cohort_id: 2 },
+        { name: "Michelle the Fighter", cohort_id: 3 },
+        { name: "Tom the Terrible", cohort_id: 3 },
+        { name: "Charletta the PM", cohort_id: 3 }
       ]);
     });
 };
