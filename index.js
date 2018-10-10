e80431a44b13e02ef8f0bf0e7105248578ba248e
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-10 16:30:50 -0600

    Implement UPDATE Cohort Endpoint
    which receives an id as a URL parameter and a cohort object and updates the cohort in the database, whose id matches the provided id, with the parameters of the cohort object.

--- index.js
+++ index.js
@@ -39,23 +39,25 @@ server.post('/api/cohorts', (request, response) => {
 
 /// ----- READ All Cohorts Endpoint ----- 
 server.get('/api/cohorts', (request, response) => {
+
+    // Database Promise Functions
     db('cohorts')
     .then(cohorts => {
         if (cohorts.length < 1) {
             return response.status(404).json({errorMessage: "No cohorts were found."})
         }
-
         response.status(200).json(cohorts)
     })
     .catch(error => response.status(500).json(error))
 });
 
-/// ----- READ Inidivudal Cohort Enpoint -----
+/// ----- READ Inidivudal Cohort Endpoint -----
 server.get('/api/cohorts/:id', (request, response) => {
 
     // Extract Request URL Parameters
     const { id } = request.params;
 
+    // Database Promise Functions
     db('cohorts')
     .where({ id })
     .then( cohort => {
@@ -66,6 +68,49 @@ server.get('/api/cohorts/:id', (request, response) => {
     })
     .catch(error => {response.status(500).json(error)})
 });
+
+/// ----- READ All Students of Individual Cohort Endpoint -----
+server.get('/api/cohorts/:cohort_id/students', (request, response) => {
+
+    // Extract Request URL Parameters
+    const { cohort_id } = request.params;
+
+    // Database Promise Functions
+    db('students')
+    .where({ cohort_id })
+    .then( students => {
+        if (students.length < 1) {
+            return response.status(404).json({message: "No students were found for that cohort."})
+        }
+        response.status(200).send(students);
+    })
+    .catch(error => {response.status(500).json(error)})
+});
+
+
+/// ----- UPDATE Cohort Endpoint -----
+server.put('/api/cohorts/:id', (request, response) => {
+
+    // Extract URL Parameters
+    const { id } = request.params;
+  
+    // Deconstruct Request Body
+    const { name } = request.body;
+  
+    // Database Promise Functions
+    db('cohorts')
+    .where({ id })
+    .update({ name })
+    .then(updated => {
+    if (!updated || updated < 1) {
+        return response.status(400).send({errorMessage:`Unable to update the name of the cohort with the provided id.`})
+    }
+    response.status(200).json(updated)
+  })
+    .catch(error => response.status(500).send(error))
+  })
+  
+
 
 const port = 9999;
 server.listen(port, () => {console.log(`#### Server active on port ${port} ####\n`)});
