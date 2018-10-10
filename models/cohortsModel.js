184a8d3b37ae7ebc9f59e1b6408cf34ff6408abc
Kevin Nguyen <kevinn858@gmail.com>
2018-10-10 15:03:35 -0700

    created routes for cohorts

---
+++ models/cohortsModel.js
@@ -0,0 +1,31 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+
+module.exports = {
+    post,
+    getAll,
+    getById,
+    put,
+    del
+};
+
+function post(cohort){
+    return db('cohorts').insert(cohort).into('cohorts');
+};
+
+function getAll(){
+    return db('cohorts');
+};
+
+function getById(id){
+    return db('cohorts').where({id}).first();
+};
+
+function put(id, changes){
+    return db('cohorts').where({id}).update(changes);
+};
+
+function del(id){
+    return db('cohorts').where({id}).del();
+};
---
+++ routes/cohortsRoutes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+
+const cohorts = require('../models/cohortsModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    cohorts.getAll()
+        .then(cohorts =>{
+            res.status(200).json(cohorts);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+            console.log('error in get');
+        });
+});
+
+router.get('/:id', (req, res)=>{
+    const {id} = req.params;
+    cohorts.getById(id)
+        .then(cohort =>{
+            res.status(200).json(cohort);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+router.post('/', (req, res)=>{
+    const {name} = req.body;
+    const cohort = {name};
+    cohorts.post(cohort)
+        .then(ids =>{
+            res.status(200).json(ids[0]);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+router.put('/:id', (req, res)=>{
+    const {id} = req.params;
+    const {name} = req.body;
+    const changes = {name};
+    cohorts.put(id,changes)
+        .then(count =>{
+            if(count>0){
+                res.status(200).json(count);
+            }else{
+                res.status(404).json('No cohort with that ID found.');
+            }
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+router.delete('/:id', (req, res)=>{
+    const {id} = req.params;
+    cohorts.del(id)
+        .then(cohort =>{
+            if(cohort){
+                res.status(200).json(cohort);
+            }else{
+                res.status(404).json('No cohort with that ID found.');
+            }
+        })
+        .catch(err => res.status(500).json(err));
+})
+
+module.exports = router;
