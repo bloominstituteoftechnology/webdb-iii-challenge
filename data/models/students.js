2e25de685e91e52069cebc1ac1219c656fa19f07
Andrew <ghobrial.andrew@gmail.com>
2018-10-10 17:59:41 -0400

    completed models

---
+++ data/models/students.js
@@ -0,0 +1,31 @@
+const knex = require("knex");
+const knexConfig = require("../../knexfile.js");
+const db = knex(knexConfig.development);
+ 
+const find = () => {
+  return db("students");
+};
+ 
+const findById = id => {
+  return db("students")
+    .where({ id })
+    .first();
+};
+ 
+const insert = newCohort => {
+  return db("students").insert(newCohort);
+};
+ 
+const update = (id, updatedCohort) => {
+  return db("students")
+    .where({ id })
+    .update(updatedCohort);
+};
+ 
+const remove = id => {
+  return db("students")
+    .where({ id })
+    .del();
+};
+ 
+module.exports = { find, findById, insert, update, remove };
