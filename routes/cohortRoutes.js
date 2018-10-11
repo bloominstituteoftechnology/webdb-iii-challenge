f205ae453c059644062e376d7629a65af7b10150
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-10 19:24:43 -0500

    MVP - Implemented CRUD into cohorts - MVP Completed.

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -14,4 +14,66 @@ router.get('/', (req, res) => {
       .catch(err => res.status(500).json(err));
 });
 
+router.get('/:id', async (req,res) => {
+    try{
+        const {id} = req.params;
+        const cohort = await cohorts.findById(id);
+
+        if (cohort) {
+            res.status(200).json(cohort);
+        }else {
+            res.status(404).json({message: `Cohort Not Found`})
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
+});
+
+//POST
+router.post('/', (req, res) => {
+    const cohort = req.body;
+
+    cohorts
+      .add(cohort)
+      .then(cohortIds => {
+          res.status(201).json(cohortIds[0])
+      })
+      .catch(err => res.status(500).json(err))
+});
+
+//DELETE
+router.delete('/:id', (req,res) => {
+    const {id} = req.params;
+
+    cohorts
+      .remove(id)
+      .then(cohortId => {
+          if (!cohortId || cohortId<1) {
+              res.status(404).json({message: `No Cohort Record Found to Delete`});
+          }else {
+              res.status(200).json(cohortId);
+          }
+      })
+      .catch(err => res.status(500).json(err))
+});
+
+//PUT
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    cohorts
+      .update(id, changes)
+      .then(cohortId => {
+          if (!cohortId || cohortId<1) {
+              res.status(404).json({message: `No Cohort Record Found to Update`});
+          }else {
+              res.status(200).json(cohortId);
+          }
+      })
+      .catch(err => res.status(500).json(err))
+});
+
+
 module.exports = router;
