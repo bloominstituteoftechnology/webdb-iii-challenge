0baceef556fac1e89996c0608015b1781257f505
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-17 17:05:00 -0400

    Imported The Configured Knexfile & Instatiated a DB Object
    #Comment
    
    Instatiated and instant of the database using the configured knexfile

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const knex = require('knex');
 //Server Instantiation
 const appServ = express();
 
+//Import(require) knexfile & Instantiate a database object using knex
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+
+//Third party middleware
+//express.json returns json objects of the response
+//All global middlewares that will be used across enpoints must also be plugged into the app
+//cors and helmet middlewares are not used
+app.use(express.json(), logger('combined'), cors(), helmet());
+
+//Root Request/Route Handler
+app.get('/', (req, res) => {
+    res.send('Test for root endpoint!')
+});
 
 
 
