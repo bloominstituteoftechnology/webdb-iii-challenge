3e7ee40918b85b7428e2978e1edc86233f426dba
lyang9 <lyang9@buffalo.edu>
2018-10-17 18:11:04 -0400

    server.get read all cohorts

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+// endpoints here
+server.get('/', (req, res) => {
+  res.send('It is working!');
+}); 
+
+// read all cohorts
+server.get('/api/cohorts', (req, res) => {
+  db('cohorts')
+    .then(cohorts => {
+      res.status(200).json(cohorts);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
 // listening port
 const port = 3300;
 server.listen(port, function() {
