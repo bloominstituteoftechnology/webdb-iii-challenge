22f320aed05e4fcb5a2c6e9189b7d43ad4e449eb
lyang9 <lyang9@buffalo.edu>
2018-10-17 19:41:23 -0400

    Completed students seed

---
+++ data/seeds/02-students.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('students').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('students').insert([
+        {name: 'Linda', cohort_id: 1},
+        {name: 'Wonjae', cohort_id: 2},
+        {name: 'Emily', cohort_id: 3}
+      ]);
+    });
+};
