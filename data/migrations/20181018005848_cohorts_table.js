32a0b5376b4806b633449c7f47f83a9c35df6292
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-18 01:29:30 -0400

    Added Fields & Migrated to DB
    #Comment
    
    Added fields to the cohorts table using 'tble.increments' and 'tbl.string'

--- data/migrations/20181018005848_cohorts_table.js
+++ data/migrations/20181018005848_cohorts_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('cohorts', tbl => {
+      //Creates PK that auto-increments
+      tbl.increments('id');
+
+      //Creates name field
+      tbl.string('name', 255).notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+    //Rolls back the table created
+  return knex.schema.dropTableIfExists('cohorts');
 };
