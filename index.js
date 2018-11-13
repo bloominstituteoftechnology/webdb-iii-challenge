a5afb30eb935682c761bf59b9be71eef2628a6de
Lee Len Formento <leebatef@gmail.com>
2018-11-13 14:20:50 -0500

    POST cohorts

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ server.get('/api/cohorts', (req, res) => {
       res.status(500).json(err)
     });
   });
+
+server.post('/api/cohorts', (req, res) => {
+  const name = req.body;
+   db
+   .insert(name) 
+   .into('cohorts')
+  .then(name => {
+    res.status(200).json(name[0])
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+})
   
 
 server.listen(7000, () => console.log('\n Party at part 7k '))
