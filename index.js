34625a136a8a1592395a3356331e6e59eea1cdcd
Lee Len Formento <leebatef@gmail.com>
2018-11-13 14:18:33 -0500

    Seed for cohorts. Also added GET cohort to make sure it renders

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex= require('knex');
+
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development); 
 
 const server = express();
 
@@ -9,5 +13,17 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send("It's Alive!");
 })
+
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+    .then(names => {
+      console.log(names)
+      res.status(200).json(names)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
+  });
+  
 
 server.listen(7000, () => console.log('\n Party at part 7k '))
