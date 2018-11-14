3cbef7fac49ea68417b449b110ccd3ad87456f42
Elvis Ibarra <ielvisd@gmail.com>
2018-11-14 11:38:22 -0800

    Project Setup - KNEX wired up and ready to go

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/lambda.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+};
