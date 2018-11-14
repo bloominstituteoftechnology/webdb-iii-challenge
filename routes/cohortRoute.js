385ff800380ec6ede4409f66118916ebe92fe1b7
Gannon <gkleblanc@gmail.com>
2018-11-14 16:21:16 -0500

    inital set up. Dependencies brought in, index.js and routes built, knexfile done too

---
+++ routes/cohortRoute.js
@@ -0,0 +1,19 @@
+
+//dependencies
+const express = require('express');
+const router = express.Router();
+const knex = require('knex');
+
+//set up knex and DB
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig.development);
+
+//router endpoints
+router.get('/', (req, res) => {
+    db('cohorts')
+        .then(cohorts => {
+            res.status(200).json(cohorts);
+        })
+        .catch(err => res.status(500).json(err))
+})
+
