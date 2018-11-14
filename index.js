a195a0d55a1e9e2c61c69f69800ab489596a37fb
AM_Myrick <amyrick.820@gmail.com>
2018-11-14 15:16:08 -0500

    creates get endpoint for cohorts db

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ server.use(helmet());
 server.get("/", (req, res) => {
 
     res.status(200).json({api: "running"});
+})
 
+server.get("/api/cohorts", (req, res) => {
+    db("cohorts")
+        .then(cohorts => res.status(200).json({cohorts}))
+        .catch(err => res.status(500).json({error: err}))
 })
 
 const port = 9001;
+
 server.listen(port, function() {
-console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
