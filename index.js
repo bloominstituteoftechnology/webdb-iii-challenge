bf26aa1449cf0e7efc88c9e67d48d99767e56b16
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-14 15:00:16 -0600

    server up and running connected to db, get req performed

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ const db = knex(knexConfig.development);
 const server = express();
 server.use(express.json());
 
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+        .then(cohorts => {
+            res.status(200).json({ cohorts })
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error processing your request' })
+        })
+})
+
 server.listen(9000, () => console.log('running'))
