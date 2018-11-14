9e63447f5e9477e8135a3773eecb8af126f15d2b
Matthew Wright <matthewbwright@gmail.com>
2018-11-14 15:02:28 -0700

    fixes some issues with the seeds

--- data/seeds/students.js
+++ data/seeds/students.js
@@ -5,13 +5,13 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('students').insert([
-        { name: Matt, cohort_id: 1 },
-        { name: Natalie, cohort_id: 1 },
-        { name: Jonas, cohort_id: 2 },
-        { name: Henry, cohort_id: 2 },
-        { name: Norah, cohort_id: 3 },
-        { name: Gemma, cohort_id: 3 },
-        { name: Evan, cohort_id: 2 }
+        { name: 'Matt', cohort_id: 1 },
+        { name: 'Natalie', cohort_id: 1 },
+        { name: 'Jonas', cohort_id: 2 },
+        { name: 'Henry', cohort_id: 2 },
+        { name: 'Norah', cohort_id: 3 },
+        { name: 'Gemma', cohort_id: 3 },
+        { name: 'Evan', cohort_id: 2 }
       ]);
     });
 };
