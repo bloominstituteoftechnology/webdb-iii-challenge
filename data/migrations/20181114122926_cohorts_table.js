f35603e1a1fb5ffe9111d71d7bdd3be4de27df31
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-14 12:40:15 -0800

    added lamdba database & cohort/students tables

---
+++ data/migrations/20181114122926_cohorts_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('cohorts', function(tbl) {
+      tbl.increments();
+
+      tbl.string('name', 255);
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('cohorts');
+};
