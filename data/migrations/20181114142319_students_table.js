98639ec4a3c702732dc643ab7657f1998d419519
Daniel Ferrer <danfrrr13@gmail.com>
2018-11-14 14:29:31 -0500

    created DB, and cohorts seed file.

--- data/migrations/20181114142319_students_table.js
+++ data/migrations/20181114142319_students_table.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.cretaeTable('students', tbl => {
+    return knex.schema.createTable('students', tbl => {
         tbl.increments();
         tbl.string('name', 128);
         tbl.integer('cohort_id').unsigned().references('id').inTable('cohorts');
