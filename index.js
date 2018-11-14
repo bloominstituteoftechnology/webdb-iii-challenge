35657bfcb1b7c28a5f1764887e0be68b8273c879
samsisle <samko9522@gmail.com>
2018-11-14 15:50:55 -0800

    Add POST single student route handler

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ server.use(helmet());
 // ROUTES
 // ==============================================
 server.use('/api/cohorts', cohortRouter);
-server.use('/api/students', studentRouter);
+server.use('/students', studentRouter);
 
 // START THE SERVER
 // ==============================================
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,6 +2,7 @@
 // ==============================================
 const express = require('express');
 const studentDb = require('../data/helpers/studentDb.js');
+const cohortDb = require('../data/helpers/cohortDb.js');
 
 const router = express.Router();
 
@@ -25,6 +26,21 @@ router.get('/:id', async (req, res) => {
   } catch {
     res.status(500).json({ error: "There was an error retrieving the student's information!" });
   }
+});
+
+router.post('/', async (req, res) => {
+  if (req.body.name && req.body.cohort_id) {
+    const cohort = await cohortDb.get(req.body.cohort_id);
+    if (cohort) {
+      try {
+        const addedStudent = await studentDb.insert(req.body);
+        const student = await studentDb.get(addedStudent.id);
+        res.status(201).json(student);
+      } catch {
+        res.status(500).json({ error: 'There was an error saving the student to the database.' });
+      }
+    } else res.status(404).json({ error: 'The cohort with the specified ID does not exist.' });
+  } else res.status(400).json({ error: 'Please provide a name & cohort_id for the student.' });
 });
 
 module.exports = router;
