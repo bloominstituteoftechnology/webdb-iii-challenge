1fe5f392e758d73ae0461d453dd0bf48270ffb4e
Matthew Wright <matthewbwright@gmail.com>
2018-11-14 15:29:22 -0700

    adds POST and DELETE for cohorts

--- index.js
+++ index.js
@@ -10,32 +10,76 @@ server.get('/', (req, res) => {
   res.json({ api: 'get ready for cohorts!' });
 });
 
-//sanity check
-const port = 8000;
-server.listen(port, function() {
-  console.log(`\n=== Web API listening on http://localhost:${port} ===\n`);
-});
-
 //GET cohorts
 server.get('/api/cohorts', (req, res) => {
   db('cohorts')
     .then(cohorts => res.status(200).json(cohorts))
     .catch(err =>
-      res
-        .status(500)
-        .json({
-          message: 'The requested cohort could not be retrieved.',
-          error: err
-        })
+      res.status(500).json({
+        message: 'The requested cohort could not be retrieved.',
+        error: err
+      })
     );
 });
 
 //GET cohorts by id
+server.get('/api/cohorts/:id', (req, res) => {
+  const { id } = req.params;
+  db('cohorts')
+    .where({ id })
+    .then(cohort => {
+      if (cohort) {
+        res.status(200).json(cohort);
+      } else {
+        res.status(404).json({
+          message: 'The cohort with the specified ID does not exist.',
+          error: err
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The cohort information could not be retrieved.',
+        error: err
+      });
+    });
+});
 
 //GET students by cohort id
 
 //POST add a cohort
+server.post('/api/cohorts', (req, res) => {
+  const cohort = req.body;
+  db('cohorts')
+    .insert(cohort)
+    .returning('id')
+    .then(id => res.status(201).json(id))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: 'Your cohort could not be added.', error: err })
+    );
+});
 
 //DELETE a cohort
+server.delete('/api/cohorts/:id', (req, res) => {
+  const { id } = req.params;
+  db('cohorts')
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ message: 'Your cohort could not be deleted.', error: err })
+    );
+});
 
 //PUT update the cohort
+
+const port = 8000;
+server.listen(port, function() {
+  console.log(`\n=== Web API listening on http://localhost:${port} ===\n`);
+});
