2d1a2530185a45aaed7dc0eb07558603f7a5bb1d
Kat Fries <katherynejf@gmail.com>
2018-11-14 18:31:12 -0500

    fixed seeds and ran seed files

--- data/seeds/001-cohorts.js
+++ data/seeds/001-cohorts.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').truncate()
+  return knex('cohorts').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('cohorts').insert([
         { name: 'FSW14' }, // rowValue1
         { name: 'FSW15' }, // rowValue2
         { name: 'FSW15' }  // rowValue3
--- data/seeds/002-students.js
+++ data/seeds/002-students.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('students').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('students').insert([
         { name: 'Kat', cohort_id: 1 },    // {id: 1, colName: 'rowValue1'},
         { name: 'Katie', cohort_id: 1 }, 
         { name: 'Yanrong', cohort_id: 1 }  
