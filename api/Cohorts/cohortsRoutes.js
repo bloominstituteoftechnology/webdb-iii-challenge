bd44f6eff6752202c79c76b275dd3ce206c8dfd9
Hargobind Atwal <ethaipath@gmail.com>
2018-11-14 15:27:22 -0800

    added migrations

--- api/Cohorts/cohortsRoutes.js
+++ api/Cohorts/cohortsRoutes.js
@@ -83,6 +83,15 @@ const echo = (req, res) => {
   });
 }
 
+/*
+[POST] /api/cohorts This route should save a new cohort to the database.
+[GET] /api/cohorts This route will return an array of all cohorts.
+[GET] /api/cohorts/:id This route will return the cohort with the matching id.
+[GET] /api/cohorts/:id/students returns all students for the cohort with the specified id.
+[PUT] /api/cohorts/:id This route will update the cohort with the matching id using information sent in the body of the request.
+[DELETE] /api/cohorts/:id This route should delete the specified cohort.
+*/
+
 router.post('/', addCohort)
 router.get('/', getAllCohorts)
 router.get('/:id', getCohort)
