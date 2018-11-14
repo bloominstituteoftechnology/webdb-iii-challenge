59cafa99e6d6a065530032afa0f6a86a8859b4e4
Hunter315 <hunter.mckay.smith@gmail.com>
2018-11-14 15:03:32 -0700

    Routed cohorts into separate file, implemented POST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
+const cohortsRoute = require('./api/cohortsRoute')
 
 const dbConfig = require('./knexfile');
 
@@ -14,29 +15,7 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send('API Running...');
 });
-
-//====GET ALL COHORTS====
-server.get('/api/cohorts', async (req, res) => {
-	try {
-		let cohorts = await db('cohorts')
-		res.status(200).json(cohorts)
-	} catch (e) {
-		console.log(e)
-		res.status(500).json({ error: 'There was an error accessing the cohorts from the database.' })
-	}
-});
-
-//====GET COHORT BY ID====
- server.get('/api/cohorts/:id', async (req, res) => {
-	const { id } = req.params
-	try {
-		let cohort = await db('cohorts').where('id', id)
-		cohort.length !== 0 ? res.status(200).json(cohort) : res.status(404).json({ message: 'ID NOT FOUND' })
-	} catch (e) {
-		console.log(e)
-		res.status(500).json({ error: 'There was an error accessing that cohort from the database.' })
-	}
-})
+server.use('/api/cohorts', cohortsRoute);
 
 
 server.listen(9000, () => console.log('Server running on port 9000'));
