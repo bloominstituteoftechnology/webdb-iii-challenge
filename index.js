6c43394af24e1cc75b9390a98317b449228570e4
unknown <brandongrych@gmail.com>
2018-11-14 15:09:08 -0600

    setting up file structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+const server = express();
+server.use(express.json());
+server.use(helmet());
+const port = 9000;
+server.listen(port, function() {
+console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
