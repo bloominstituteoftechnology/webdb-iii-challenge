b73ec8f07f14fc8bcc17ce2809eab0b7b4a43d67
AM_Myrick <amyrick.820@gmail.com>
2018-11-14 15:21:30 -0500

    creates post endpoint for cohorts db

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ server.use(express.json());
 server.use(helmet());
 
 server.get("/", (req, res) => {
-
     res.status(200).json({api: "running"});
 })
 
 server.get("/api/cohorts", (req, res) => {
     db("cohorts")
-        .then(cohorts => res.status(200).json({cohorts}))
+        .then(cohorts => res.status(200).json(cohorts))
+        .catch(err => res.status(500).json({error: err}))
+})
+
+server.post("/api/cohorts", (req, res) => {
+    const changes = req.body;
+
+    if (changes.name === "" || changes.name === undefined) {
+        return res.status(400).json({error: "Please make sure the cohort name is indexed."})
+    }
+
+    db("cohorts")
+        .insert(changes)
+        .then(id => res.status(200).json(id))
         .catch(err => res.status(500).json({error: err}))
 })
 
