eb841f086554a4bab99f161c036ee4a7a7a8311e
Matthew Wright <matthewbwright@gmail.com>
2018-11-14 15:07:40 -0700

    adds GET sanity check

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  res.json({ api: 'get ready for cohorts!' });
+});
+
 const port = 8000;
 server.listen(port, function() {
   console.log(`\n=== Web API listening on http://localhost:${port} ===\n`);
