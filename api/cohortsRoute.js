d661184b71a94ddfe5cfad5bfad9a03e4be2ee55
ryanboris <ryan.boris@gmail.com>
2018-11-14 03:01:45 -0500

    Completes MVP by adding PUT/DELETE cohort Endpoints

--- api/cohortsRoute.js
+++ api/cohortsRoute.js
@@ -1,12 +1,9 @@
 const express = require('express')
+const router = express.Router()
 const knex = require('knex')
 const knexConfig = require('../knexfile')
 
 const db = knex(knexConfig.development)
-
-const router = express.Router()
-
-router.use(express.json())
 
 router.get('/', async (req, res) => {
 	try {
@@ -53,6 +50,42 @@ router.get('/:id/students', async (req, res) => {
 	} catch (e) {
 		console.log(e)
 		res.status(500).json({ error: 'The students could not be retrieved from the database.' })
+	}
+})
+router.delete('/:id', async (req, res) => {
+	const { id } = req.params
+	try {
+		let count = await db('cohorts').where('id', id).del()
+
+		count > 0 ? res.status(200).json(count) : res.status(404).json({ message: 'ID not found' })
+	} catch (e) {
+		console.log(e)
+		res.status(500).json({ error: 'The cohort could not be deleted.' })
+	}
+})
+
+router.put('/:id', (req, res) => {
+	if (!req.body.name) {
+		res.status(400).json({ message: 'Please provide a cohort name.' })
+	}
+	db('cohorts')
+		.where('id', req.params.id)
+		.update(req.body)
+		.then(count => {
+			count !== 0 ? res.status(200).json(ids) : res.status(404).json({ errorMessage: 'ID NOT FOUND' })
+		})
+		.catch(e => res.status(500).json(e))
+})
+
+router.delete('/:id', async (req, res) => {
+	const { id } = req.params
+	try {
+		let count = await db('cohorts').where('id', id).del()
+
+		count > 0 ? res.status(200).json(count) : res.status(404).json({ message: 'ID not found' })
+	} catch (e) {
+		console.log(e)
+		res.status(500).json({ error: 'The cohort could not be deleted.' })
 	}
 })
 
