5fcd964996160a2ea19d20d5f3fcdc8bd15ecde3
Drewsup123c <drewsup123c@gmail.com>
2018-11-14 15:49:16 -0700

    created student and cohorts databases

--- data/migrations/20181114134123_lambda.sqlite3.js
+++ data/migrations/20181114134123_lambda.sqlite3.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('cohorts',function(tbl){
+        //primary key (Id)
+        tbl.increments();
+
+        //name
+        tbl.string('name', 128).unique();
+
+        tbl.timestamps(true,true);
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('cohorts')
 };
