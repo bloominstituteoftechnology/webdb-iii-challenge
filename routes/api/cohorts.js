6e30188d58564c3a151599fe2036263e58b13fab
rytwalker <rytwalker@gmail.com>
2018-11-14 18:05:12 -0500

    Added GET by id and POST request

--- routes/api/cohorts.js
+++ routes/api/cohorts.js
@@ -7,12 +7,38 @@ const db = knex(knexConfig.development);
 
 const router = express.Router();
 
+// GET all cohorts
 router.get('/', async (req, res) => {
   try {
-    const cohorts = await db.select().table('cohorts');
+    const cohorts = await db('cohorts');
     res.status(200).json(cohorts);
   } catch (error) {
     res.status(500).json({ error: 'There was an error getting the cohorts.' });
+  }
+});
+
+// GET cohort by id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const cohort = await db('cohorts').where({ id });
+    res.status(200).json({ ...cohort[0] });
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error getting that cohort.' });
+  }
+});
+
+// POST new cohort
+router.post('/', async (req, res) => {
+  const newCohort = req.body;
+  try {
+    const id = await db.insert(newCohort).into('cohorts');
+    const cohort = await db('cohorts').where({ id: id[0] });
+    res.status(201).json({ ...cohort[0] });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'There was an error creating a new cohort.' });
   }
 });
 
