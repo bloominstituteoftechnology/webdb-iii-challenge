aa92b2d8c592c19833b12b240c75fd03964ef859
Daniel Ferrer <danfrrr13@gmail.com>
2018-11-14 15:06:13 -0500

    finished third get endpoint. Tested with Postman.

--- index.js
+++ index.js
@@ -53,6 +53,23 @@ server.get('/api/cohorts/:cohortid', (req, res) => {
         });
 });
 
+server.get('/api/cohorts/:cohortid/students', (req, res) => {
+    const { cohortid } = req.params;
+
+    db('students')
+        .where({ cohort_id: cohortid })
+        .then(students => {
+            if (!students) {
+                res.status(404).json({ error: 'ID not found.' });
+            } else {
+                res.status(200).json(students);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'There was an error fetching students.', err });
+        });
+});
+
 const port = 3300;
 server.listen(port, function() {
     console.log(`\n---- Lambda API Listening on http://localhost:${port} ----\n`);
