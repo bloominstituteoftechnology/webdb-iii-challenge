c10187ab6e7d685192cb8d5343e65927caa9c5a7
bummings <eabummings@gmail.com>
2018-11-14 17:56:48 -0500

    finished migration file for cohorts

---
+++ data/migrations/20181114174428_cohorts.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("cohorts", function(table) {
+    // primary key: ID
+    table.increments(); //for autoincrement
+    table.string("name", 255).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  // here's the rollback
+  return knex.schema.dropTableIfExists("cohorts");
+};
