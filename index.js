9f661aac3a84204402829380dac7eb664c54dc40
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-14 15:00:28 -0700

    added all CRUD opperations for cohort

--- index.js
+++ index.js
@@ -24,6 +24,52 @@ server.post('/api/cohorts', (req, res) => {
   });
 
 
+  server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+      .then(cohorts => res.status(200).json(cohorts))
+      .catch(err => res.status(500).json(err));
+  });
+
+  server.get("/api/cohorts/:id", (req, res) => {
+    const { id } = req.params;
+    db("cohorts")
+      .where({ id: id })
+      .then(cohort => {
+        res.status(200).json(cohort);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+
+  server.get("/api/cohorts/:id/students", (req, res) => {
+    const { id } = req.params;
+     db("students")
+      .where({ cohort_id: id })
+       .then(students => res.status(200).json(students))
+       .catch(err => res.status(500).json(err))
+  });
+
+  server.put("/api/cohorts/:id", (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+     db("cohorts")
+      .where({ id: id })
+      .update(changes)
+      .then(count => {
+        res.status(200).json(count);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+  
+  server.delete("/api/cohorts/:id", (req, res) => {
+    const { id } = req.params;
+     db("cohorts")
+      .where({ id: id })
+      .del()
+      .then(count => {
+        res.status(200).json(count);
+      })
+      .catch(err => res.status(500).json(err));
+  });
 
 
 server.listen(9000, () => console.log('\n== Port 9k ==\n'));
