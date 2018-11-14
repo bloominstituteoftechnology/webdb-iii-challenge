58836dfdbb4490f3beb5dde663da9b2f2202e834
Matthew Wright <matthewbwright@gmail.com>
2018-11-14 15:11:42 -0700

    adds GET for cohorts

--- index.js
+++ index.js
@@ -10,7 +10,32 @@ server.get('/', (req, res) => {
   res.json({ api: 'get ready for cohorts!' });
 });
 
+//sanity check
 const port = 8000;
 server.listen(port, function() {
   console.log(`\n=== Web API listening on http://localhost:${port} ===\n`);
 });
+
+//GET cohorts
+server.get('/api/cohorts', (req, res) => {
+  db('cohorts')
+    .then(cohorts => res.status(200).json(cohorts))
+    .catch(err =>
+      res
+        .status(500)
+        .json({
+          message: 'The requested cohort could not be retrieved.',
+          error: err
+        })
+    );
+});
+
+//GET cohorts by id
+
+//GET students by cohort id
+
+//POST add a cohort
+
+//DELETE a cohort
+
+//PUT update the cohort
