fa6babbbdbac59a9332d56b8aef0c28e51b97f9c
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-14 16:02:04 -0600

    mvp complete

--- index.js
+++ index.js
@@ -83,4 +83,22 @@ server.delete('/api/cohorts/:id', (req, res) => {
         })
 })
 
+server.get('/api/cohorts/:id/students', (req, res) => {
+    const { id } = req.params
+    db('students')
+        .where({cohort_id: id})
+        .then(students => {
+            if(students.length === 0) {
+                res.status(404).json({ message: 'Could not find students by cohort' })
+            } else {
+                res.status(200).json(students)
+        }})
+        .catch(err => {
+          res.status(500).json({ message: 'error finding student' })
+         })
+
+        
+})
+
 server.listen(9000, () => console.log('running'))
+
