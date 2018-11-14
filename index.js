62376bd92acdd3f1d8171ad1b250a0015b06dc06
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-14 18:31:02 -0500

    most stretch endpoints

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ server.get('/api/students', (req, res) => {
         });
 });
 
+// [GET] /students/:id
+// Return student with cohort name instead of id
+// Use query join to pull cohort name from cohort table
+// {
+//    id: 1,
+//    name: 'Lambda Student',
+//    cohort: 'Full Stack Web Infinity'
+// }
+
 // [GET] /api/cohorts
 server.get('/api/cohorts', (req, res) => {
     db('cohorts')
@@ -87,18 +96,46 @@ server.post('/api/cohorts', (req, res) => {
                 res.status(500).json({ error: err, errorMessage: 'Error creating cohort' });
             });
     } else {
-        res.status(400).json({ errorMessage: 'New cohort must have a non-empty name'});
+        res.status(400).json({ errorMessage: 'New cohort must have a non-empty name' });
     }
 });
 
+// [POST] /students
+// ****** To do
+// - change output to be cohort name not id on return
+server.post('/api/students', (req, res) => {
+    if(req.body.name && req.body.cohort_id){
+        db('cohorts')
+            .where({ id: req.body.cohort_id})
+            .then(cohort => {
+                if(cohort.length) {
+                    return db('students').insert(req.body);
+                } else {
+                    throw new Error();
+                }
+            })
+            .then(id => {
+                return db('students').where({ id: id[0]});
+            })
+            .then(student => {
+                res.status(201).json(student); // refactor to have cohort name not id
+            })
+            .catch(err => {
+                res.status(500).json({ error: err, errorMessage: 'Error creating student, cohort id may not exist' });
+            })
+    } else {
+        res.status(400).json({ errorMessage: 'New student must have a non-empty name and cohort id' });
+    }
+})
+
 // [PUT] /api/cohorts/:id
 server.put('/api/cohorts/:id/', (req, res) => {
-    if(req.body.name){
+    if (req.body.name) {
         db('cohorts')
             .where({ id: req.params.id })
             .update(req.body)
             .then(count => {
-                if(count){
+                if (count) {
                     return db('cohorts').where({ id: req.params.id });
                 } else {
                     throw new Error();
@@ -111,30 +148,38 @@ server.put('/api/cohorts/:id/', (req, res) => {
                 res.status(500).json({ error: err, errorMessage: 'Error updating cohort, cohort id may not exist' });
             })
     } else {
-        res.status(400).json({ errorMessage: 'Updated cohort must have a non-empty name'});
+        res.status(400).json({ errorMessage: 'Updated cohort must have a non-empty name' });
     }
 });
+
+// [PUT] /students/:id
+// Check that student name and cohort_id are valid.
+// Return student with cohort name instead of id
 
 // [DELETE] /api/cohorts/:id
 server.delete('/api/cohorts/:id', (req, res) => {
     db('cohorts')
         .where({ id: req.params.id })
         .then(cohort => {
-            if(cohort.length){
+            if (cohort.length) {
                 return db('cohorts').where({ id: req.params.id }).del();
             }
         })
         .then(count => {
-            if(count){
-                res.status(200).json({ successMessage: 'Successfully deleted cohort'});
+            if (count) {
+                res.status(200).json({ successMessage: 'Successfully deleted cohort' });
             } else {
                 throw new Error();
             }
         })
         .catch(err => {
             res.status(500).json({ error: err, errorMessage: 'Error deleting cohort, cohort id may not exist' });
-        })
-})
+        });
+});
+
+// [DELETE] /students/:id 
+// This route should delete the specified student.
+// Return success message.
 
 const port = 9000;
 server.listen(port, () => {
