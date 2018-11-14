d1645772730619e4edebd3543d3c68ddb070367e
rytwalker <rytwalker@gmail.com>
2018-11-14 18:30:42 -0500

    Updated some of the data

--- data/seeds/002-students.js
+++ data/seeds/002-students.js
@@ -5,11 +5,16 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('students').insert([
-        { cohort_id: 1, name: 'Cohort started in May' },
-        { cohort_id: 2, name: 'Cohort started in June' },
-        { cohort_id: 3, name: 'Cohort started in July' },
-        { cohort_id: 4, name: 'Cohort started in August' },
-        { cohort_id: 5, name: 'Cohort started in September' }
+        { cohort_id: 1, name: 'Ryan' },
+        { cohort_id: 2, name: 'Eric' },
+        { cohort_id: 3, name: 'Emily' },
+        { cohort_id: 4, name: 'Grace' },
+        { cohort_id: 5, name: 'Harry' },
+        { cohort_id: 1, name: 'Marg' },
+        { cohort_id: 2, name: 'Larry' },
+        { cohort_id: 3, name: 'Duke' },
+        { cohort_id: 4, name: 'Wynona' },
+        { cohort_id: 5, name: 'Fin' }
       ]);
     });
 };
