3cabfd5b7a37eb05df4d92fd5fb6c997a5f79c8c
KHollobaugh <khollobaugh@gmail.com>
2018-11-14 15:59:28 -0500

    added GET functionality

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ server.get('/', (req, res) => {
     res.json({ api: 'api is alive!'});
 });
 
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+    .then(cohorts => res.status(200).json(cohorts))
+    .catch(error => res.status(500).json({ message: "failed to get cohorts"}))
+})
+
+server.get('/api/cohorts/:id', (req, res) => {
+    const { id } = req.params;
+    db('cohorts')
+    .where({ id: id })
+    .then(cohorts => res.status(200).json(cohorts))
+    .catch(error => res.status(500).json({ message: "No cohort by that ID found"}))
+})
+
+
+
 
 const port = 4000;
 server.listen(port, function() {
