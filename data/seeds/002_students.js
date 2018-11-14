6cc7c8e73dfec2fe66a841c54c9f5c4d1fcde5d1
Yanrong Xu <xuyanrong@icloud.com>
2018-11-14 16:51:32 -0500

    add more test data and add get endpoint for cohorts

--- data/seeds/002_students.js
+++ data/seeds/002_students.js
@@ -5,10 +5,22 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('students').insert([
-        { cohort_id: 1, name: 'kam'},
-        { cohort_id: 2, name: 'yanrong'},
-        { cohort_id: 3, name: 'katie'},
-        { cohort_id: 4, name: 'shaw'},
+        { cohort_id: 9, name: 'kam'},
+        { cohort_id: 9, name: 'yanrong'},
+        { cohort_id: 9, name: 'katie'},
+        { cohort_id: 9, name: 'shaw'},
+        { cohort_id: 10, name: 'army'},
+        { cohort_id: 10, name: 'navy'},
+        { cohort_id: 10, name: 'air force'},
+        { cohort_id: 10, name: 'marine'},
+        { cohort_id: 11, name: 'google'},
+        { cohort_id: 11, name: 'yahoo'},
+        { cohort_id: 11, name: 'msn'},
+        { cohort_id: 11, name: 'bing'},
+        { cohort_id: 12, name: 'ebay'},
+        { cohort_id: 12, name: 'amazon'},
+        { cohort_id: 12, name: 'newegg'},
+        { cohort_id: 12, name: 'best buy'},
       ]);
     });
 };
--- index.js
+++ index.js
@@ -11,8 +11,20 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+// check if test server are running
 server.get('/', (req, res) => {
     res.status(200).json({message: 'server is running'})
+});
+
+// get all the cohort data
+server.get('/api/cohorts', (req, res) => {
+    db('cohorts')
+        .then(cohorts => {
+            res.status(200).json({cohorts})
+        })
+        .catch(err => {
+            res.status(500).json({message: err})
+        })
 });
 
 server.listen(9000, () => console.log('\n== Port 9k ==\n'));
