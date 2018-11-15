192cb40c994e44f8a77c3cd4c90e102bf42a0048
frankbenevento <frankbenevento123@gmail.com>
2018-11-14 18:43:04 -0800

    added students migration

--- data/migrations/20181114150049_cohorts.js
+++ data/migrations/20181114150049_cohorts.js
@@ -8,5 +8,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('cohorts')
+    return knex.schema.dropTableIfExists('cohorts');
 };
