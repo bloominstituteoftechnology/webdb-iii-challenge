d4a93443b783f96f3645399ccb2f2ec83a6cc843
rytwalker <rytwalker@gmail.com>
2018-11-15 10:18:48 -0500

    Added students router GET and GET by id added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cohorts = require('./routes/api/cohorts');
+const students = require('./routes/api/students');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/cohorts', cohorts);
+server.use('/api/students', students);
 
 server.get('/', (req, res) => {
   res.send('helloooo');
--- routes/api/cohorts.js
+++ routes/api/cohorts.js
@@ -2,7 +2,6 @@ const express = require('express');
 const knex = require('knex');
 
 const knexConfig = require('../../knexfile');
-
 const db = knex(knexConfig.development);
 
 const router = express.Router();
