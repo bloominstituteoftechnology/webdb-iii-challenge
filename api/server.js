136f9e180ec5cf08eaa115daf8a2678740bc4fc5
Kat Fries <katherynejf@gmail.com>
2018-11-15 10:13:43 -0500

    fixed errors related to id added join

--- api/server.js
+++ api/server.js
@@ -12,7 +12,9 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+  res.json({
+    api: 'up'
+  });
 });
 
 // TABLE SCHEMA
@@ -24,16 +26,26 @@ server.get('/', (req, res) => {
 // COHORTS
 // POST: .insert() .into
 server.post('/api/cohorts', nameCheck, (req, res) => {
-  const cohort = req.body;
+  const {
+    name
+  } = req.body;
+  const cohort = {
+    name
+  };
 
   db('cohorts')
     .insert(cohort)
     // .returning('*') // other databases allow other items to be returned
     .then(ids => {
-      res.status(201).json({ id: ids[0]});
+      res.status(201).json({
+        id: ids[0]
+      });
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error inserting', err })
+      res.status(500).json({
+        message: 'Error inserting',
+        err
+      })
     })
 })
 
@@ -41,29 +53,69 @@ server.post('/api/cohorts', nameCheck, (req, res) => {
 server.get('/api/cohorts', (req, res) => {
 
   db('cohorts')
-  //.select()
+    //.select()
     .then(cohorts => res.status(200).json(cohorts))
-    .catch(err => res.status(500).json({ err }));
+    .catch(err => res.status(500).json({
+      err
+    }));
 });
 
 // GET BY ID
 server.get('/api/cohorts/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
 
   db('cohorts')
-    .then(cohorts => res.status(200).json(cohorts[0]))
-    .catch(err => res.status(500).json({ err }));
+    .where({
+      id: id
+    })
+    .then(cohorts =>
+      cohorts[0] ?
+      res.status(200).json(cohorts[0]) :
+      res.status(404).json({
+        error: "there is no cohort with that id",
+        "log": console.log(id)
+      })
+    )
+    .catch(err => res.status(500).json({
+      err
+    }));
 });
 
-// PUT .where() .update()
-server.put('/api/cohorts/:id', (req, res) => {
-  const changes = req.body;
-  const { id } = req.params;
+// GET cohort.id matches students.cohort_id
+server.get('/api/cohorts/:id/students', (req, res) => {
+  const {
+    id
+  } = req.params;
 
   db('cohorts')
-    .where({ id: id }) // 
+    .join('students', 'cohorts.id', '=', 'students.cohort_id')
+    .select('*')
+    .then(students => res.status(200).send(students.filter(student => student.cohort_id === `${id}` / 1)))
+    .catch(err => res.status(500).json({
+      err
+    }));
+});
+
+
+// PUT .where() .update()
+server.put('/api/cohorts/:id', nameCheck, (req, res) => {
+  const changes = req.body;
+  const {
+    id
+  } = req.params;
+
+  db('cohorts')
+    .where({
+      id: id
+    }) // 
     .update(changes)
     .then(count => {
-      res.status(200).json({ count });
+      res.status(200).json({
+        name: changes.name,
+        id: changes.id
+      });
     })
     .catch(err => res.status(500).json(err));
 });
@@ -72,13 +124,19 @@ server.put('/api/cohorts/:id', (req, res) => {
 // Calling .del() without first filtering the records will result on the removal of all the records in the table, be careful!
 
 server.delete('/api/cohorts/:id', (req, res) => {
-  const { id } = req.params;
+  const {
+    id
+  } = req.params;
 
   db('cohorts')
-    .where({ id: id })
+    .where({
+      id: id
+    })
     .del()
     .then(count => {
-      res.status(200).json({ count });
+      res.status(200).json({
+        count
+      });
     })
     .catch(err => res.status(500).json(err));
 });
@@ -87,15 +145,20 @@ server.delete('/api/cohorts/:id', (req, res) => {
 // POST: .insert() .into
 server.post('/api/students', nameCheck, (req, res) => {
   const student = req.body;
-
+  console.log(student)
   db('students')
     .insert(student)
     // .returning('id')
     .then(ids => {
-      res.status(201).json({ id: ids[0]});
+      res.status(201).json({
+        id: ids[0]
+      });
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error inserting', err })
+      res.status(500).json({
+        message: 'Error inserting',
+        err
+      })
     })
 })
 
@@ -104,27 +167,53 @@ server.get('/api/students', (req, res) => {
 
   db('students')
     .then(students => res.status(200).json(students))
-    .catch(err => res.status(500).json({ err }));
+    .catch(err => res.status(500).json({
+      err
+    }));
 });
 
 // GET BY ID
 server.get('/api/students/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
 
   db('students')
-    .then(students => res.status(200).json(students[0]))
-    .catch(err => res.status(500).json({ err }));
+    .where({
+      id: id
+    })
+    .then(students =>
+      students[0] ?
+      res.status(200).json(students[0]) //students[0])
+      :
+      res.status(404).json({
+        error: "there is no student with that id",
+        "log": console.log(id)
+      })
+    )
+    .catch(err => res.status(500).json({
+      err
+    }));
 });
 
 // PUT .where() .update()
 server.put('/api/students/:id', (req, res) => {
   const changes = req.body;
-  const { id } = req.params;
-
+  const {
+    id
+  } = req.params;
+  console.log(changes, id)
   db('students')
-    .where({ id: id }) // 
+    .where({
+      id: id
+    }) // 
     .update(changes)
     .then(count => {
-      res.status(200).json({ count });
+      res.status(200).json({
+        name: changes.name,
+        cohort: changes.cohort_id,
+        id: changes.id
+      });
     })
     .catch(err => res.status(500).json(err));
 });
@@ -133,13 +222,19 @@ server.put('/api/students/:id', (req, res) => {
 // Calling .del() without first filtering the records will result on the removal of all the records in the table, be careful!
 
 server.delete('/api/students/:id', (req, res) => {
-  const { id } = req.params;
+  const {
+    id
+  } = req.params;
 
   db('students')
-    .where({ id: id })
+    .where({
+      id: id
+    })
     .del()
     .then(count => {
-      res.status(200).json({ count });
+      res.status(200).json({
+        count
+      });
     })
     .catch(err => res.status(500).json(err));
 });
