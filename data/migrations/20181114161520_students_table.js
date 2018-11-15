73385b94649d680656ea589d68dab06d1760aa7a
MEBonham <programmer@mckaybonham.com>
2018-11-14 17:04:47 -0700

    Did a rollback and re-created students table with requirements

--- data/migrations/20181114161520_students_table.js
+++ data/migrations/20181114161520_students_table.js
@@ -2,8 +2,13 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("students", tbl => {
       tbl.increments();
-      tbl.string("name", 255);
-      tbl.integer("cohort_id");
+      tbl.string("name", 255).notNullable();
+      tbl
+        .integer("cohort_id")
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('cohorts');
     });
   };
   
