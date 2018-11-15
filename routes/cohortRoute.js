f6ac68c88a8b39083a358397b86b108992647a42
Gannon <gkleblanc@gmail.com>
2018-11-15 09:09:29 -0500

    added end points for cohort route

--- routes/cohortRoute.js
+++ routes/cohortRoute.js
@@ -17,4 +17,80 @@ router.get('/', (req, res) => {
         .catch(err => res.status(500).json(err))
 })
 
+router.get('/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .first()
+        .then(cohort => {
+            if (cohort) {
+                res.status(200).json(cohort)
+            } else {
+                res.status(404).json({ message: 'A cohort with that ID could not be found' })
+            }
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error' }))
+})
+router.get('/:id/students', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .first()
+        .then(cohort => {
+            if (cohort) {
+                db('students')
+                    .where({ cohort_id: req.params.id })
+                    .then(students => {
+                        if (students) {
+                            res.status(200).json(students)
+                        } else {
+                            res.status(404).json({ message: 'No students were found matching that cohort' })
+                        }
+                    })
+                    .catch(err => res.status(500).json({ message: 'The students for that cohort could not be retrieved' }))
+            } else {
+                res.status(404).json({ message: 'A cohort with that ID could not be found' })
+            }
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error accessing the records for that cohort' }))
+})
+router.post('/', (req, res) => {
+    if (!req.body.name) {
+        res.status(400).json({ message: 'Please provide a name for the cohort' })
+    } else {
+        db('cohorts')
+            .insert(req.body)
+            .into('cohorts')
+            .then(id => {
+                res.status(201).json(id)
+            })
+            .catch(err => res.status(500).json({ message: 'The record could not be created' }))
+    }
+})
+router.delete('/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if (count) {
+                res.status(200).json({ message: `${count} records deleted` })
+            } else {
+                res.status(404).json({ message: 'A cohort with that ID could not be found' })
+            }
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error accessing the record' }))
+})
+router.put('/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if (count) {
+                res.status(200).json({ message: 'The cohort information has been updated' })
+            } else {
+                res.status(404).json({ message: 'A cohort with that ID could not be found' })
+            }
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error accessing the record' }))
+})
+
+
 module.exports = router;
