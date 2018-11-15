87b24deabaa7fab0daba68976871b24a7a3eef0d
theryanclausen <theryanclausen@gmail.com>
2018-11-14 18:55:24 -0700

    join

--- config/routeMaker.js
+++ config/routeMaker.js
@@ -17,89 +17,102 @@ module.exports = (db, resource) => {
   route.get("/:id", async (req, res) => {
     try {
       const { id } = req.params;
-      
-      const item = await db(resource).where({id});
+      const item = await db(resource).where({ id });
+      if (resource === "students") {
+        const cohortID = item[0].cohort_id;
+        itemJoin = await db
+          .select(
+              "students.id as student_id",
+            "students.name as student_name",
+            "students.cohort_id",
+            "cohorts.name as cohort_name"
+          )
+          .from("students")
+          .join("cohorts", { "cohorts.id": "students.cohort_id" }).where({'student_id':id});
 
-      if (item.length){
-      res.status(200).json(item);
-      return
+        res.status(200).json(itemJoin);
+        return;
       }
-      throw(err)
+
+      if (item.length) {
+        res.status(200).json(item);
+        return;
+      }
+      throw err;
     } catch (err) {
-        sendError(404, `no ${resource} found at that id.`,res)
+      sendError(404, `no ${resource} found at that id.`, res);
     }
   });
 
-  if (resource==='cohorts') {
-      route.get('/:id/students', async (req,res) =>{
-          try {
-              const {id}= req.params;
-              const students = await db('students').where({'cohort_id' : id})
-              res.status(200).json(students)
-          }catch (err) {
-            sendError(404, `no ${resource} found at that id.`,res)
-        }
-      })
+  if (resource === "cohorts") {
+    route.get("/:id/students", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const students = await db("students").where({ cohort_id: id });
+        res.status(200).json(students);
+      } catch (err) {
+        sendError(404, `no ${resource} found at that id.`, res);
+      }
+    });
   }
-  route.post('/', async (req, res) =>{
-      try{
-        const body = req.body
-        if (resource === 'cohorts'){
-            if (!body.name){
-                sendError(400, `Name required`, res)
-                return;
-            }
+  route.post("/", async (req, res) => {
+    try {
+      const body = req.body;
+      if (resource === "cohorts") {
+        if (!body.name) {
+          sendError(400, `Name required`, res);
+          return;
         }
-        if (resource === 'students'){
-            if (!body.cohort_id  || !body.name){
-                sendError(400, `Student must have name and cohort_id`)
-                return;
-            }
+      }
+      if (resource === "students") {
+        if (!body.cohort_id || !body.name) {
+          sendError(400, `Student must have name and cohort_id`);
+          return;
         }
-        const id = await db(resource).insert(body)
-        const newItem = await db(resource).where({ id: id[0] });
-        res.status(201).json(newItem)
       }
-      catch(err){
-          sendError(500, `${resource} could not be added.`, res)
-      }
-  })
+      const id = await db(resource).insert(body);
+      const newItem = await db(resource).where({ id: id[0] });
+      res.status(201).json(newItem);
+    } catch (err) {
+      sendError(500, `${resource} could not be added.`, res);
+    }
+  });
 
-  route.put('/:id', async (req, res) =>{
-      try{
-          const {id} = req.params;
-          const body = req.body;
-          const count = await db(resource).where({id}).update(body)
-          if (!count){
-              sendError(400, `${resource} was not altered`)
-          }
-          const updatedItem = await db(resource).where({id})
-          res.status(201).json({"updated": updatedItem})
+  route.put("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const count = await db(resource)
+        .where({ id })
+        .update(body);
+      if (!count) {
+        sendError(400, `${resource} was not altered`);
       }
-      catch(err){
-          sendError(500, `${resource} could not be altered.`, res)
-      }
-  })
+      const updatedItem = await db(resource).where({ id });
+      res.status(201).json({ updated: updatedItem });
+    } catch (err) {
+      sendError(500, `${resource} could not be altered.`, res);
+    }
+  });
 
-  route.delete('/:id', async (req,res)=>{
-      try{
-          const {id} = req.params;
-          const item = await db(resource).where({id});
-          if (!item){
-              sendError(400, `No ${resource} at that id.`, res)
-              return
-          }
-          const count = await db(resource).where({id}).del()
-          if (count){
-              res.status(200).json({"deleted": item})
-          }
-          
-
+  route.delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await db(resource).where({ id });
+      if (!item) {
+        sendError(400, `No ${resource} at that id.`, res);
+        return;
       }
-      catch(err){
-          sendError(500, `${resource} could not be removed.`, res )
+      const count = await db(resource)
+        .where({ id })
+        .del();
+      if (count) {
+        res.status(200).json({ deleted: item });
       }
-  })
+    } catch (err) {
+      sendError(500, `${resource} could not be removed.`, res);
+    }
+  });
 
   return route;
 };
