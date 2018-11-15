0341ec385de0adab844860710d28666494b8c815
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-15 07:17:13 -0600

    finished setup of server file

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig.development);
+
+// Imports Routes
+const cohortsRouter = require('./cohortsRoute');
+const studentsRouter = require('./studentsRoute');
+
+const server = express();
+server.use(express.json());
+
+// Route Roots
+server.use('/api/cohorts', cohortsRouter);
+server.use('/api/students', studentsRouter)
+
+module.exports = server;
