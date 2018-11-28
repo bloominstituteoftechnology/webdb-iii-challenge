33a8f01a82478a7fc68342d26180998be6984dc5
Troy Amlee <troyamlee19@gmail.com>
2018-11-28 17:44:06 -0600

    [stretch] All Stretch Done!

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ server.get('/api/cohorts/:id/students', (req, res) => {
     db('students')
     // .where({id})
     .join('cohorts', 'cohorts.id', '=', 'students.cohort_id')
-    .where('students.id', '=', id)
+    .where('students.cohort_id', '=', id)
     .then(student => res.status(200).json(student))
     .catch(error => res.status(500).json({message: "You done f'd up", error}))
 });
@@ -65,6 +65,58 @@ server.put('/api/cohorts/:id', (req, res) => {
 server.delete('/api/cohorts/:id', (req, res) => {
     const { id } = req.params;
 
+    db('cohorts')
+    .where({ id })
+    .del()
+    .then(count => {
+        res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/students', (req, res) => {
+    const student = req.body
+    db('students').insert(student)
+    .then(ids => {
+        res.status(201).json(ids)
+    })
+    .catch(err => res.status(500).json({ message: 'Error Posting New Student', err }))
+});
+
+server.get('/students', (req, res) => {
+    db('students')
+    .then(students => res.status(200).json(students))
+    .catch(error => res.status(500).json({ message: `Can't Retrieve Students Data`, error }))
+
+});
+
+server.get('/students/:id', (req, res) => {
+    const { id } = req.params
+    console.log(id)
+    db('students')
+    .select( 'students.id', 'name', 'cohort_name' )
+    .join('cohorts', 'cohorts.id', '=', 'students.cohort_id')
+    .where('students.id', '=',  id)
+    .then(student => res.status(201).json({student}))
+    .catch(error => res.status(500).json({ message: 'Could not find cohort specified', error }))
+});
+
+server.put('/student/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
+
+    db('students')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+        res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete('/students/:id', (req, res) => {
+    const { id } = req.params;
+
     db('students')
     .where({ id })
     .del()
@@ -73,6 +125,8 @@ server.delete('/api/cohorts/:id', (req, res) => {
     })
     .catch(err => res.status(500).json(err));
 });
+
+
 
 const port = 7777;
 server.listen(port, function() {
