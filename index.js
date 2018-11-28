9dacf759b5f77107cc51230d4a63e2eb91aa36b4
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-11-28 14:49:08 -0500

    Post

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ const knexConfig = require('./knexfile');
 const db = knex(knexConfig.development);
 
 const server = express();
-
 server.use(express.json());
+
+
+server.post('/api/cohorts', (req, res) => {
+  const cohort = req.body;
+
+  db('cohorts')
+    .insert(cohort)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Cannot Insert', err})
+    })
+})
