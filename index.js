f4c50d2f8970a3d957190a4a5d6eb4f3f50dcca5
Angelo Deleon <rudy209@comcast.net>
2018-11-28 15:18:52 -0800

    Finished MVP

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cohorts = require('./routes/api/cohorts');
+const students = require('./routes/api/students');
+
 const server = express();
+
+server.use(express.json());
+server.use('/api/cohorts', cohorts);
+server.use('/api/students', students);
+
 server.get('/', (req, res) => {
-    res.send('hellooooss');
+    res.send('HI!');
 });
+
 server.listen(5000, () => console.log('Server running on port 5000'));
-
-
 // Angelo Deleon FSW14
---
+++ routes/api/cohorts.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const knex = require('knex');
+
+const knexConfig = require('../../knexfile');
+const db = knex(knexConfig.development);
+
+const router = express.Router();
+
+// GET all cohorts
+router.get('/', async (req, res) => {
+  try {
+    const cohorts = await db('cohorts');
+    res.status(200).json(cohorts);
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error getting the cohorts.' });
+  }
+});
+
+// GET cohort by id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const cohort = await db('cohorts').where({ id });
+    return !cohort.length
+      ? res.status(404).json({ message: "That cohort doesn't exist." })
+      : res.status(200).json({ ...cohort[0] });
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error getting that cohort.' });
+  }
+});
+
+// GET students
+router.get('/:id/students', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const students = await db
+      .select('*')
+      .from('cohorts')
+      .where({ 'cohorts.id': id })
+      .join('students', { 'cohorts.id': 'students.cohort_id' });
+
+    return !students.length
+      ? res.status(404).json({ message: "That cohort doesn't exist" })
+      : res.status(200).json(students);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'There was an error getting that cohort student name.' });
+  }
+});
+
+// POST new cohort
+router.post('/', async (req, res) => {
+  const newCohort = req.body;
+  try {
+    const id = await db.insert(newCohort).into('cohorts');
+    const cohort = await db('cohorts').where({ id: id[0] });
+    res.status(201).json({ ...cohort[0] });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'There was an error creating a new cohort.' });
+  }
+});
+
+// PUT update cohort
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+    const count = await db('cohorts')
+      .where({ id })
+      .update(changes);
+    const cohort = await db('cohorts').where({ id });
+    return count
+      ? res.status(200).json({ ...cohort[0] })
+      : res.status(404).json({ message: 'Make sure that id exists.' });
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error updating that cohort.' });
+  }
+});
+
+// DELETE cohort
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const count = await db('cohorts')
+      .where({ id })
+      .del();
+    return count
+      ? res.status(200).json(count)
+      : res.status(404).json({ message: 'Not found.' });
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error deleting that cohort.' });
+  }
+});
+
+module.exports = router;
