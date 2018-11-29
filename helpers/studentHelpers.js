29c550e6ba73a77457f7b4e0b4533a6ecc3fcd14
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-11-29 05:02:28 -0600

    finished MVP and a few stretch endpoints

--- helpers/studentHelpers.js
+++ helpers/studentHelpers.js
@@ -4,11 +4,16 @@ const db = knex(knexConfig.development);
 
 module.exports = {
   find,
-  insert
+  insert,
+  findById
 }
 
 function find() {
   return db('students');
+}
+
+function findById(id) {
+  return db('students').where({ id: Number(id)})
 }
 
 function insert(student) {
--- middleware/students.js
+++ middleware/students.js
@@ -37,4 +37,67 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(student => {
+      if (student.length !== 0) {
+        res
+          .status(200)
+          .json(student)
+      } else {
+        res
+          .status(404)
+          .json({ errorMessage: "There is no student with that ID number in this database." })
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ errorMessage: "Sorry, we are having trouble finding that student...", error})
+    })
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  if(changes.name.length > 0) {
+    db.update(id, changes)
+      .then(count => {
+        if (count) {
+          res
+            .status(200)
+            .json({ message: `The record for ${changes.name} has been updated` })
+        } else {
+          res
+            .status(404)
+            .json({ errorMessage: "Can't update a student that doesn't exist!" })
+        }
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ errorMessage: `Sorry, we had an error updating ${changes.name}`, error })
+      })
+  } else {
+    res
+      .status(406)
+      .json({ errorMessage: "student name cannot be blank!" })
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(count => {
+      res
+        .status(200)
+        .json(count);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessage: "Error removing student from database!"})
+    })
+})
+
 module.exports = router;
