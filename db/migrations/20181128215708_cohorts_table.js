89fa4b815502a55471cc4c16ea6976e33ffbe9fa
jaklundun <govi.escudero@gmail.com>
2018-11-28 22:39:45 -0800

    Created required fields for cohorts_table as well as corresponding rollback.

--- db/migrations/20181128215708_cohorts_table.js
+++ db/migrations/20181128215708_cohorts_table.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('cohorts_table', function(tbl) {
+        // creates primary key
+        tbl.increments(); 
+    
+        tbl
+        .string('name', 255)
+        .notNullable() // sets as required
+        .unique('name'); // creates index
+        
+        
+        });
+    };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('cohorts_table');
 };
+  
