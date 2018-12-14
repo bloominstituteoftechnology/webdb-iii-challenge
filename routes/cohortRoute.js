4c8c4c46e3a253369fd5ded9d0d8a39e2802442a
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-14 16:45:11 -0600

    Added endpoints for cohorts and students

--- routes/cohortRoute.js
+++ routes/cohortRoute.js
@@ -7,13 +7,76 @@ const environment = process.env.NODE_ENV || "development";
 const configuration = require("../knexfile")[environment];
 const db = require("knex")(configuration);
 
-console.log(db("cohorts"));
+// console.log(db("cohorts"));
 
+// Cohort endpoints
 router.get("/", (req, res) => {
   db("cohorts")
     .select()
     .then(cohorts => {
       res.status(200).json(cohorts);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const cohortId = req.params.id;
+
+  db("cohorts")
+    .where("id", cohortId)
+    .select()
+    .then(cohort => {
+      if (cohort.length) {
+        res.status(200).json(cohort);
+      } else {
+        res
+          .status(404)
+          .json({ message: `Could not find cohort with id ${cohortId}` });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not get any cohorts." });
+    });
+});
+
+router.post("/", (req, res) => {
+  const name = req.body;
+
+  db.insert(name)
+    .into("cohorts")
+    .then(cohort => {
+      res.status(201).json(cohort);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const updates = req.body;
+  const { id } = req.params;
+
+  db("cohorts")
+    .where("id", "=", id)
+    .update(updates)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("cohorts")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
     })
     .catch(err => {
       res.status(500).json(err);
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -6,4 +6,78 @@ const router = express.Router();
 const knexConfig = require("../knexfile");
 const db = knex(knexConfig.development);
 
+// Student endpoints
+router.get("/", (req, res) => {
+  db("students")
+    .select()
+    .then(students => {
+      res.status(200).json(students);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const studentId = req.params.id;
+
+  db("students")
+    .where("id", studentId)
+    .select()
+    .then(student => {
+      if (student.length) {
+        res.status(200).json(student);
+      } else {
+        res
+          .status(404)
+          .json({ message: `Could not find student with id ${studentId}` });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not get any students." });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newStudent = req.body;
+
+  db.insert(newStudent)
+    .into("students")
+    .then(student => {
+      res.status(201).json(student);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const updates = req.body;
+  const { id } = req.params;
+
+  db("students")
+    .where("id", "=", id)
+    .update(updates)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("students")
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
