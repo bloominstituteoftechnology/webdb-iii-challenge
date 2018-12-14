7fcfc0a10cb76c4c39e77ca837de0c48a9ed4653
Colin Dismuke <colin.dismuke@gmail.com>
2018-12-14 11:21:16 -0600

    Created basic server and routes in index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const configureMiddleware = require("./config/middleware");
+const cohortRoute = require("./routes/cohortRoute");
+const studentRoute = require("./routes/studentRoute");
+
+// Create server
+const server = express();
+const PORT = 3000;
+
+// Middleware
+configureMiddleware(server);
+
+server.use("./api/cohorts", cohortRoute);
+server.use("./api/students", studentRoute);
+
+server.get("/", (req, res) => {
+  res.send("RDBMS...");
+});
+
+// Start listening
+server.listen(PORT, () =>
+  console.log(`\n=== API Listening on http://localhost:${port} ===\n`)
+);
