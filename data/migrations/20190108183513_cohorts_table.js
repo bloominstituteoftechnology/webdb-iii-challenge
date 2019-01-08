b0697c8f36af00b0afa8ab6676dd9d1bbe9de27f
Kendra Williams <kmonique.williams@gmail.com>
2019-01-08 18:44:01 -0500

    first migration complete cohort table completed

--- data/migrations/20190108183513_cohorts_table.js
+++ data/migrations/20190108183513_cohorts_table.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("cohorts", table => {
+     table.increments();
+     table.string("name").notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("cohorts");
 };
