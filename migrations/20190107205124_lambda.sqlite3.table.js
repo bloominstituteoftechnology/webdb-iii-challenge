401039de8affacfc1cf3da5f99583577558c69d0
marcusbass323 <marcus@bassconsultantgroup.com>
2019-01-07 21:43:37 -0800

    added cohorts table

--- migrations/20190107205124_lambda.sqlite3.table.js
+++ migrations/20190107205124_lambda.sqlite3.table.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('cohorts', table => {
+        table.increments('id');
+        table.string('name').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('id');
 };
