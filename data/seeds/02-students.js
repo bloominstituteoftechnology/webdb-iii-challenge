4d5f5130b4dcae34a62bab47312caf7b4e51b442
Emmanium <pradoe.ep@gmail.com>
2019-01-07 23:01:20 -0700

    made MVP endpoints

--- data/seeds/02-students.js
+++ data/seeds/02-students.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('students').insert([
-        {name: 'Emmanuel', cohort_id: '2'},
-        {name: 'Anon', cohort_id: '1'},
-        {name: 'Craig', cohort_id: '3'}
+        {name: 'Emmanuel', cohort_id: '11'},
+        {name: 'Anon', cohort_id: '12'},
+        {name: 'Craig', cohort_id: '13'}
       ]);
     });
 };
--- index.js
+++ index.js
@@ -14,30 +14,77 @@ server.use(helmet());
 
 //saves a new cohort
 server.post('/api/cohorts', (req, res) => {
-
+  const body = req.body;
+  db('cohorts').insert(body)
+    .then(ids => {
+      res.status(201).json(ids)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to add cohort' })
+    })
 })
 
 //returns array of all cohorts
 server.get('/api/cohorts', (req, res) => {
-  
+  db('cohorts')
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to find cohorts' })
+    })
 })
 
 //returns cohort with matching id
 server.get('/api/cohorts/:id', (req, res) => {
-  
+  const { id } = req.params;
+  db('cohorts').where('id', id)
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Invalid ID' })
+    })
 })
 
 //returns all students for the cohort with the specified id
 server.get('/api/cohorts/:id/students', (req, res) => {
-  
+  const { id } = req.params;
+  db('students').where('id', id)
+  .then(rows => {
+    res.json(rows)
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'Invalid ID' })
+  })
 })
 
 // updates cohort with the matching id using info sent in body of request
 server.put('/api/cohorts/:id', (req, res) => {
-  
+  const { id } = req.params;
+  const body = req.body;
+  db('cohorts').where('id', id).update(body)
+    .then(rowCount => {
+      res.status(201).json(rowCount)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to update cohort' })
+    })
 })
 
 // deletes specified cohort
 server.delete('/api/cohorts/:id', (req, res) => {
-  
+  const { id } = req.params;
+  db('cohorts').where('id', id).delete()
+    .then(rowCount => {
+      res.json(rowCount)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to delete cohort' })
+    })
+})
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`\n=== API Listening on http://localhost${port} ===\n`)
 })
