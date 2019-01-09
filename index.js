45ce760f9f02beec23d2ae58234b5d23ff70f815
Jess <jessdev83@gmail.com>
2019-01-08 21:54:59 -0500

    Stretch goals
    Endpoints created for students table.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = 5050;
 
 server.use(express.json());
 
+//COHORT ENDPOINTS
 server.post("/api/cohorts", (req, res) => {
   const newCohort = req.body;
   db("cohorts")
@@ -76,6 +77,70 @@ server.delete("/api/cohorts/:id", (req, res) => {
     .where({ id: id })
     .del()
     .then(cohort => {
+      res.json({ message: `record #${id} has been deleted` });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "could not delete record" });
+    });
+});
+
+//STUDENT ENDPOINTS
+server.post("/api/students", (req, res) => {
+  const newStudent = req.body;
+  db("students")
+    .insert(newStudent)
+    .then(id => {
+      res.status(201).json({ message: `student with id ${id} created` });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "could not create new student" });
+    });
+});
+
+server.get("/api/students", (req, res) => {
+  db("students")
+    .then(students => {
+      res.json(students);
+    })
+    .catch(err => {
+      res.status(400).json({ message: "students not found, look in slack" });
+    });
+});
+
+server.get("/api/students/:id", (req, res) => {
+  const { id } = req.params;
+  db("students")
+    .join("cohorts", "cohorts.id", "=", "students.cohort_id")
+    .select("students.id", "students.name", "cohorts.name")
+    .where({ id: id })
+    .then(student => {
+      res.json(student);
+    })
+    .catch(err => {
+      res.status(404).json({ message: `student with id ${id} not found.` });
+    });
+});
+
+server.put("/api/students/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedStudent = req.body;
+  db("students")
+    .where({ id: id })
+    .update(updatedStudent)
+    .then(student => {
+      res.json({ message: `student with id #${id} has been updated.` });
+    })
+    .catch(err => {
+      res.status(500).json({ message: `student not updated.` });
+    });
+});
+
+server.delete("/api/students/:id", (req, res) => {
+  const { id } = req.params;
+  db("students")
+    .where({ id: id })
+    .del()
+    .then(student => {
       res.json({ message: `record #${id} has been deleted` });
     })
     .catch(err => {
