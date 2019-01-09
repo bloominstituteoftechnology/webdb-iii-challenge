db1e86afa49f33bc3486ba34ce5adca880720952
efleming111 <efleming111@yahoo.com>
2019-01-08 20:54:57 -0700

    Added get, put, post and delete endpoints for the students

--- index.js
+++ index.js
@@ -110,6 +110,113 @@ server.put('/api/cohorts/:id', (req, res)=>{
     })
 })
 
+// Students
+server.get('/students', (req, res)=>{
+    db('students')
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(err=>{
+        res.status(500).json({error: 'Failed to retrieve data'});
+    })
+})
+
+server.get('/students/:id', (req, res)=>{
+    const {id} = req.params;
+    db('students')
+    .where('id', id)
+    .then(studentData=>{
+        if(studentData.length){
+            db('cohorts')
+            .where('id', studentData[0].cohort_id)
+            .then(cohortData=>{
+                res.json({
+                    id: studentData[0].id,
+                    name: studentData[0].name,
+                    cohort: cohortData[0].name
+                })
+            })
+        }
+        else{
+            res.status(404).json({errorMessage: 'Student not found'})
+        }
+    })
+    .catch(err=>{
+        res.status(500).json({error: 'Failed to retrieve data'});
+    })
+})
+
+server.post('/students', (req, res)=>{
+    const student = req.body;
+    if(student.name && student.cohort_id){
+        db('students')
+        .insert(student)
+        .then(id=>{
+            res.status(201).json({id: id[0]})
+        })
+        .catch(err=>{
+            res.status(500).json({error: 'Failed to add student to database'});
+        })
+    }
+    else{
+        res.status(400).json({errorMessage: 'Please include the name of the student and a valid cohort id'})
+    }
+})
+
+server.delete('/students/:id', (req, res)=>{
+    const {id} = req.params;
+    db('students')
+    .where('id', id)
+    .del()
+    .then(count=>{
+        if(count){
+            res.json({message: 'Deletion successful'});
+        }
+        else{
+            res.status(404).json({errorMessage: 'Student not found'})
+        }
+    })
+    .catch(err=>{
+        res.status(500).json({error: 'Failed to delete data'});
+    })
+})
+
+server.put('/students/:id', (req, res)=>{
+    const {id} = req.params;
+    const body = req.body;
+    db('students')
+    .where('id', id)
+    .update(body)
+    .then(count=>{
+        if(count){
+            db('students')
+            .where('id', id)
+            .then(studentData=>{
+                if(studentData.length){
+                    db('cohorts')
+                    .where('id', studentData[0].cohort_id)
+                    .then(cohortData=>{
+                        res.json({
+                            id: studentData[0].id,
+                            name: studentData[0].name,
+                            cohort: cohortData[0].name
+                        })
+                    })
+                }
+                else{
+                    res.status(404).json({errorMessage: 'Student not found'})
+                }
+            })
+        }
+        else{
+            res.status(404).json({errorMessage: 'Student not found'})
+        }
+    })
+    .catch(err=>{
+        res.status(500).json({error: 'Failed to update student'})
+    })
+})
+
 server.listen(PORT, function(){
     console.log(`Server Listening on http://localhost:${PORT}`)
 })
