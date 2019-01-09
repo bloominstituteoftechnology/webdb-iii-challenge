20e4ec639bf477e7dbd21a63d3c56d7d6102aacc
jbrock1981 <jbrock1981@gmail.com>
2019-01-09 11:37:44 -0600

    stretch completed

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ server.get('/api/cohorts/:id/students', (req, res) => {
             } else {
                 res
                     .status(404)
-                    .json({ message: 'The specified students do not exist' })
+                    .json({ message: 'The specified students do not exist' });
             }
         })
         .catch(err => {
@@ -75,13 +75,13 @@ server.post('/api/cohorts', (req, res) => {
             .catch(err => {
                 res
                     .status(500)
-                    .json({message: 'The cohort could not be added.'});
+                    .json({ message: 'The cohort could not be added.' });
             });
     }
     else {
         res
             .status(400)
-            .json({message: 'Please include name.'});
+            .json({ message: 'Please include name.' });
     }
 })
 
@@ -96,24 +96,24 @@ server.put('/api/cohorts/:id', (req, res) => {
                 if (updatedCohort) {
                     res
                         .status(201)
-                        .json({message: 'The cohort has been updated.'});
+                        .json({ message: 'The cohort has been updated.' });
                 }
                 else {
                     res
                         .status(404)
-                        .json({message: 'The specified cohort does not exist.'})
+                        .json({ message: 'The specified cohort does not exist.' })
                 }
             })
             .catch(err => {
                 res
                     .status(500)
-                    .json({message: 'The update could not be completed.'})
+                    .json({ message: 'The update could not be completed.' })
             });
     }
     else {
         res
             .status(400)
-            .json({message: 'Please include the updated name.'})
+            .json({ message: 'Please include the updated name.' })
     }
 })
 
@@ -125,28 +125,102 @@ server.delete('/api/cohorts/:id', (req, res) => {
         .then(cohort => {
             if (cohort) {
                 res
-                    .json({message: 'The cohort has been deleted.'});
+                    .json({ message: 'The cohort has been deleted.' });
             }
             else {
                 res
                     .status(404)
-                    .json({message: 'The specified cohort does not exist.'})
+                    .json({ message: 'The specified cohort does not exist.' });
             }
         })
         .catch(err => {
             res
                 .status(500)
-                .json({message: 'The specified cohort could not be deleted.'})
+                .json({ message: 'The specified cohort could not be deleted.' });
         });
 })
 
 //STRETCH ENDPOINTS
 server.get('/api/students', (req, res) => {
-
+    db("students")
+        .then(students => {
+            res
+                .status(200)
+                .json(students)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: 'The students could not be retreived.' })
+        });
 })
 
+server.get("/api/students/:id", (req, res) => {
+    const { id } = req.params;
+    db("students")
+        .where({ id: id })
+        .then(name => {
+            res
+                .status(200)
+                .json(name);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: 'The specified student could not be retreived.' });
+        });
+});
 
+server.post("/api/students", (req, res) => {
+    const name = req.body;
+    db("students")
+        .insert(name)
+        .then(name => {
+            res
+                .status(201)
+                .json(name);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: 'The student could not be added.' });
+        });
+});
 
+server.put("/api/students/:id", (req, res) => {
+    const changes = req.body;
+    const {id} = req.params;
+    db("students")
+        .where({ id: id })
+        .update(changes)
+        .then(count => {
+            res
+                .status(200)
+                .json(count);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: 'The student could not be updated.' });
+        });
+});
+
+server.delete("/api/students/:id", (req, res) => {
+    const { id } = req.params;
+    db("students")
+        .where({id:id})
+        .del()
+        .then(ids => {
+            res
+                .status(200)
+                .json(ids);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: 'The student could not be deleted.' });
+        });
+});
 
 server.listen(PORT, () => {
     console.log(`server live on port ${PORT}`);
