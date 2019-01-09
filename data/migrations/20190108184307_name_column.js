10ae6431c9cab60c6ec7d084a63b05e2a3056aaf
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-08 19:34:24 -0700

    Corrected some tabbing formatting issues in migration files for consistency

--- data/migrations/20190108184307_name_column.js
+++ data/migrations/20190108184307_name_column.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.table('cohorts', table => {
-    table.string('name').notNullable();
-  });
+    return knex.schema.table('cohorts', table => {
+        table.string('name').notNullable();
+    });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.table('cohorts', table => {
-      table.dropColumn('name');
-  })
+    return knex.schema.table('cohorts', table => {
+        table.dropColumn('name');
+    })
 };
