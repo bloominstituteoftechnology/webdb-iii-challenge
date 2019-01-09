3f09824de5e08380c58b3c26b45e9bf430d8e83b
Denise Escobar <descobar1490@gmail.com>
2019-01-08 21:15:53 -0600

    Added more dependencies and added my data directory using knex

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lambda_sqlite3.db'
+    },
+    useNullAsDefault: true,
+    migrations:{
+      directory: './data/migrations'
+    }
+  },
+
+
+};
