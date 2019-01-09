f11fde6f33badf82ba5c8223c62853c3473d6095
Abigayle Franklin <abifanklin@gmail.com>
2019-01-08 22:27:29 -0600

    Seeding working

--- knexfile.js
+++ knexfile.js
@@ -10,8 +10,10 @@ module.exports = {
     useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
-  
   },
 
 };
