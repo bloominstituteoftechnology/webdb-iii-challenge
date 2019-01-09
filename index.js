dc4c58692026347c8d98b0dbfffc8cf6fbc1f01c
Ben <bennybooftw@gmail.com>
2019-01-08 22:39:18 -0500

    Seeded the database. Setup endpoints for cohorts. Finished MVP.

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ server.listen(3030, () => {
     console.log("Server started on port 3030.");
 })
 
+//------------------COHORTS----------------------------------
+
+// GET
+
 server.get("/api/cohorts", (req, res) => {
     db("cohorts")
     .then(cohorts => {
@@ -20,5 +24,65 @@ server.get("/api/cohorts", (req, res) => {
     })
     .catch(err => {
         res.status(500).json(err)
-    })
-})
+    });
+});
+
+// GET BY ID
+
+server.get("/api/cohorts/:id", (req, res) => {
+    const { id } = req.params;
+    db("cohorts")
+      .where({ id: id })
+      .then(name => {
+          res.status(200).json(name)
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
+  });
+
+ // POST
+
+ server.post("/api/cohorts", (req, res) => {
+    const name = req.body;
+
+     db("cohorts")
+      .insert(name)
+      .then(name => {
+          res.status(201).json(name)
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
+  });
+
+ // UPDATE
+
+ server.put("/api/cohorts/:id", (req, res) => {
+    const changes = req.body;
+    const {id} = req.params;
+    console.log(id);
+    db("cohorts")
+      .where({ id: id })
+      .update(changes)
+      .then(count => {
+          res.status(200).json(count)
+      })
+      .catch(err => res.status(500).json({ error: err }));
+  });
+
+ // DELETE
+
+ server.delete("/api/cohorts/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    db("cohorts")
+      .where({id:id})
+      .del()
+      .then(ids => {
+          res.status(200).json(ids)
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
+  });
