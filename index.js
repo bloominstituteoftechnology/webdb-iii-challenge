f3789bb5fef413dfc01b8305c221e7a3508e9c72
jbrock1981 <jbrock1981@gmail.com>
2019-01-08 21:51:04 -0600

    knex done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile.js');
+
+const db =knex(dbConfig.development);
+const PORT = 42;
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/cohorts', (req, res) => {
+
+})
+
+server.get('/api/cohorts/:id', (req, res) => {
+
+})
+
+server.post('/api/cohorts', (req, res) => {
+
+})
--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/rdbms.db'
+      filename: './data/lambda.sqlite3.db'
     },
     useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
- };
+};
