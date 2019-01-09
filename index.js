34e628dee9ba07122160eb42335bdcc58a71bbdb
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-08 23:28:17 -0500

    required dbConfig added http get request to root endpoint

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const helmet = require('helmet');
 const logger = require('morgan');
 const knex = require('knex');
 
-// const dbConfig = require('./knexfile.js');
+const dbConfig = require('./knexfile');
 
 const cohortsRoute = require('./routes/cohorts');
-const studentsRoute = require('./routes/students');
+// const studentsRoute = require('./routes/students');
 
 const app = express();
-// const db = knex(dbConfig.development);
+const db = knex(dbConfig.development);
 const PORT = 8080;
 
 app.use(express.json());
@@ -18,7 +18,11 @@ app.use(logger('dev'));
 
 // routes
 app.use('/api/cohorts', cohortsRoute);
-app.use('/api/students', studentsRoute);
+// app.use('/api/students', studentsRoute);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'app is running' });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
