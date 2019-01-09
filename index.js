074649ecfcda7fd098a899d91ec90c744791a5e5
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-01-08 21:33:16 -0600

    Basic server is setup with the root endpoint working

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+const PORT = 5050;
+
+// endpoints
+server.get('/', (req, res) => {
+  res.json('It is working!');
+});
+
+server.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}`);
+});
