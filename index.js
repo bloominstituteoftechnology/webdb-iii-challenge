0a194dfb156e6e1bc1b1b44e128c593a71f92883
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-08 21:54:28 -0500

    required routes and middleware

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
 const knex = require('knex');
 
-// const dbConfig = require('./knexfile.js')
+// const dbConfig = require('./knexfile.js');
+
+const cohortsRoute = require('./routes/cohorts');
+const studentsRoute = require('./routes/students');
 
 const app = express();
 // const db = knex(dbConfig.development);
 const PORT = 8080;
 
 app.use(express.json());
+app.use(helmet());
+app.use(logger('dev'));
 
-// POST /api/cohorts
-
-// GET /api/cohorts
-
-// GET /api/cohorts/:id
-
-// GET /api/cohorts/:id/students
-
-// PUT /api/cohorts/:id
-
-// DELETE /api/cohorts/:id
+// routes
+app.use('/api/cohorts', cohortsRoute);
+app.use('/api/students', studentsRoute);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
