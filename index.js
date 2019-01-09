08d2ab4df463234c16e2bdd01e5a285ffb290268
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-08 19:39:54 -0700

    Added endpoint to post to cohorts table

--- index.js
+++ index.js
@@ -7,6 +7,31 @@ const db = knex(dbConfig.development);
 
 server.use(express.json());
 
+// add to cohorts table
+
+server.post('/api/cohorts', (req, res) => {
+    const cohort = req.body;
+    if (cohort.name) {
+        db('cohorts')
+            .insert(cohort)
+            .then(id => {
+                res
+                    .status(201)
+                    .json(id);
+            })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json({message: 'The new cohort could not be added at this time.'});
+            });
+    }
+    else {
+        res
+            .status(400)
+            .json({message: 'Please include the cohort name.'});
+    }
+});
+
 const PORT = 4400;
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
