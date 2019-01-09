4b91f07b64570f11999afddcc85edbfd309d99e9
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-08 22:14:24 -0500

    defined endpoints for routes

---
+++ routes/cohorts.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+
+const router = express.Router();
+
+// POST /api/cohorts
+router.post('/', (req, res) => {
+
+});
+
+// GET /api/cohorts
+router.get('/', (req, res) => {
+
+});
+
+// GET /api/cohorts/:id
+router.get('/:id', (req, res) => {
+
+});
+
+// GET /api/cohorts/:id/students
+router.get('/:id/students', (req, res) => {
+
+});
+
+// PUT /api/cohorts/:id
+router.put('/:id', (req, res) => {
+  
+});
+
+// DELETE /api/cohorts/:id
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
