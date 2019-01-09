32bf1c4d07ae7d3fc6994522dbfb1fb7c09592da
Freekeyboy <jjhutton81@gmail.com>
2019-01-08 19:09:14 -0800

    Created export up/down functions for cohorts and students.

--- data/migrations/20190108185334_cohorts_table.js
+++ data/migrations/20190108185334_cohorts_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable
+  ('cohorts', table => {
+      table.increments();
+      table.string('name', 100).notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('cohorts');
 };
