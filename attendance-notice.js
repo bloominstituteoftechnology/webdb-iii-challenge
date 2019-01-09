c35f3c27d75a51d0031e170db4fd01c76aae364b
Chris Juelfs <CRJuelfs24@comcast.net>
2019-01-08 22:34:17 -0600

    im sick  :(  bear with me.

---
+++ attendance-notice.js
@@ -0,0 +1,3 @@
+// 1-8-19
+// i'm sick with sore throat and stuffy with a sinus pressure that feels like my heads gonna explode. All while starting a new job for 2019 (timing in my life is great huh lol).
+// like usual tho ill be rdy for sprint challenge this sat.
