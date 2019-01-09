c6e08852dea267c04b09ea7287a3cb1b2e36d474
Rob Salzberg <robsalzberg@gmail.com>
2019-01-08 22:27:25 -0500

    Setup routes and GET endpoint

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const knex = require('knex');
+const express = require("express");
 
-const dbConfig = require('./knexfile');
+const configureMiddleware = require("./config/middleware");
+const cohortRoute = require("./routes/cohortRoute");
+const studentRoute = require("./routes/studentRoute");
 
 const server = express();
-const db = knex(dbConfig.development);
 const PORT = 5400;
 
+configureMiddleware(server);
+
 server.use(express.json());
+
+server.use("./api/cohorts", cohortRoute);
+server.use("./api/students", studentRoute);
+
+server.get("/", (req, res) => {
+    res.send("RDBMS...");
+  });
+
 
 server.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}`);
