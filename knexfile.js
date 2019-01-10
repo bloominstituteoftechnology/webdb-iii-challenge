3c2af5fd646af0a6be96617e55fdcc265119e5a1
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-01-09 22:20:49 -0600

    Almost done

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,12 @@ module.exports = {
     connection: {
       filename: './data/lambda.sqlite3'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
