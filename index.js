5a6e05c4c4d16897b1107e165359370d4e8eb63c
hraya <hraya@lssu.edu>
2019-01-10 21:49:39 -0600

    Beginning endpoints
    experimenting with db.Config file

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const cohortRouter = require('./routers/cohort_router.js');
+const studentRouter = require('./routers/students_router.js');
+
+
+const server = express();
+const PORT = 4567;
+
+server.use(express.json());
+server.use('/api/cohorts', cohortRouter);
+server.use('/api/students', studentRouter);
+
+
+server.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}!`)
+})
