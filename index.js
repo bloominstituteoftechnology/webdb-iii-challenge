4b822e85df04420b6bdc9abaa62673598337e99f
vy3191 <venkyafhs@gmail.com>
2019-01-10 21:43:38 -0500

    Added get method for cohorts

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ const PORT = 5000;
 //Knex 
 const dbConfig = require('./knexfile');
 const db = knex(dbConfig.development);
+
+
+server.get('/api/cohorts', (req,res) => {
+      db('cohorts').then( cohortInfo => {
+           res.send(cohortInfo);
+      })
+});
+
+server.get('/api/cohorts/:id', (req,res)=> {
+     const id = req.params.id;
+     db('cohorts').where('id',id)
+                  .then( cohort => {
+                      res.json(cohort);
+                  })
+})
+
+
+server.listen(PORT, ()=> {
+    console.log(`Listening at localhost ${PORT}`);
+})
