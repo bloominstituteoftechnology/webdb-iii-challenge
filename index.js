1ef0ff7a32fa46e80a97627f9209a7c6cc63e8e5
Denise Escobar <descobar1490@gmail.com>
2019-01-10 20:18:00 -0600

    Added the delete and put request

--- index.js
+++ index.js
@@ -77,8 +77,56 @@ server.get('/api/cohorts/:id/students', (req, res)=>{
 
 //PUT /api/cohorts/:id
 
+server.put('/api/cohorts/:id', (req, res) =>{
+    const {id} = req.params;
+    const cohort = req.body;
+    db('cohorts').where('id', id).update(cohort)
+    .then(count =>{
+        if(count ===1){
+            if(cohort.name){
+                res
+                .status(200)
+                .json({message: `Cohort ${id} updated`})
+            } else {
+                res
+                .status(400)
+                .json({errorMessage: "Please provide a name for the cohort"})
+            }
+        } else {
+            res
+            .status(404)
+            .json({message: "The post with the specified ID does not exist"})
+        }
+    })
+    .catch(err =>{
+        res
+        .status(500)
+        .json({error: "The cohorts information could not be modified "})
+    })
+});
+
 //DELETE /api/cohorts/:id
 
+server.delete('/api/cohorts/:id', (req, res) =>{
+    const {id} = req.params;
+    db('cohorts').where('id', id).del()
+    .then(count =>{
+        if(count === 1){
+            res
+            .status(200)
+            .json({message: "Specified cohort deleted"})
+        } else {
+            res
+            .status(404)
+            .json({message: "The cohort with the specified ID does not exist"})
+        }
+    })
+    .catch(err =>{
+        res
+        .status(500)
+        .json({error: "The cohort could not be removed"})
+    })
+});
 
 server.listen(PORT, ()=>{
     console.log("we are a go")
