fde22662b68753a4a207710f301865df1a161349
Denise Escobar <descobar1490@gmail.com>
2019-01-10 19:35:30 -0600

    Added the get request for cohorts by id

--- index.js
+++ index.js
@@ -8,14 +8,49 @@ const PORT = 8050;
 
 server.use(express.json());
 
+// POST /api/cohorts
+
+
+
+//GET
+
 server.get('/api/cohorts', (req, res) =>{
     db('cohorts')
-    .then(rows => res.json(rows))
+    .then(rows => res.status(200).json(rows))
     .catch(err =>{
-        res.status(500)
-        json({message: 'unable to get cohorts'})
+        res
+        .status(500)
+        .json({error: 'Unable to get cohorts'})
     })
 });
+
+// /api/cohorts/:id
+
+server.get('/api/cohorts/:id', (req,res) =>{
+    const {id} = req.params;
+    db('cohorts').where('id', id)
+    .then(cohort =>{
+        if(cohort.length !==0){
+            res
+            .status(200)
+            .json(cohort)
+        } else {
+            res
+            .status(404)
+            .json({error: "The cohort with the specified ID does not exist"})
+        }
+    })
+    .catch(err =>{
+        res.status(500).json({error: "The Cohort could not be retrieved"})
+    })
+});
+
+// /api/cohorts/:id/students
+
+//PUT /api/cohorts/:id
+
+//DELETE /api/cohorts/:id
+
 
 server.listen(PORT, ()=>{
     console.log("we are a go")
