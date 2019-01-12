b37dc1def516e74642c55317acc26b3baaf43579
vy3191 <venkyafhs@gmail.com>
2019-01-12 00:47:09 -0500

    Completed stretch goals

--- index.js
+++ index.js
@@ -1,90 +1,24 @@
 const express = require('express');
-const knex = require('knex');
-const server = express();
-server.use(express.json());
+const helmet = require('helmet');
+const logger = require('morgan')
 
-//PORT
+
 const PORT = 5000;
-//Knex 
-const dbConfig = require('./knexfile');
-const db = knex(dbConfig.development);
+const server = express();
+const cohorts = require('./routes/cohorts.js');
+const students = require('./routes/students');
 
 
 
-server.get('/api/cohorts', (req,res) => {
-      db('cohorts')
-      .then( cohortInfo => {
-           res.json(cohortInfo);
-      })
-      .catch(err=> {
-          res.status(500).json({err: "Failed to get cohorts"})
-      });
-});
+server.use(express.json());
+server.use(helmet());
+server.use(logger('dev'));
+server.use('/', cohorts);
+server.use('/', students);
 
-server.get('/api/cohorts/:id', (req,res)=> {
-     const id = req.params.id;
-     db('cohorts')
-     .where('id',id)
-     .then( cohort => {
-         res.json(cohort);
-      })
-      .catch(err=> {
-        res.status(500).json({err: `Failed to get cohort with ${id}`})
-    });
-});
 
-server.get('/api/cohorts/:id/students', (req,res) => {
-      const id = req.params.id;
-      db('students').where('cohort_id', id )
-      .then( students => {
-          if(students.length > 0) {
-               res.status(200).json(students)
-          } else {
-               res.status(404).json({message: `The specified students with ID ${id} does not exists`})
-          }
-      }) .catch(err=> {
-        res.status(500).json({err: `Failed to get the students with the cohort ID ${id}`})
-    });
-
-})
-
-server.post('/api/cohorts/', (req,res) => {
-      const cohort = req.body;
-      console.log(cohort);
-      db('cohorts')
-      .insert(cohort)
-      .then( cohortID => {
-         res.status(201).json(cohortID);
-      })
-      .catch(err=> {
-        res.status(500).json({err: "Failed to post cohort"})
-    });
-
-});
-
-server.put('/api/cohorts/:id', (req,res) => {
-     const {id} = req.params;
-     const cohort = req.body;
-     db('cohorts').where('id', id)
-     .update(cohort)
-     .then( newCohortRowCount => {
-         res.json(newCohortRowCount)
-     }) .catch(err=> {
-      res.status(500).json({err: `Failed to update the cohort with the ${id}`})
-  });
-});
-
-server.delete('/api/cohorts/:id', (req,res) => {
-     const {id} = req.params;
-     db('cohorts').where('id', id).del()
-     .then( count => {
-         res.status(201).json({Message: `Deleted cohort with the ID ${id} successfully`});
-     }) .catch(err=> {
-      res.status(500).json({err: `Failed to delete the cohort with the ID ${id}`})
-  });
-})
 
 
 server.listen(PORT, ()=> {
     console.log(`Listening at localhost ${PORT}`);
-})
+});
