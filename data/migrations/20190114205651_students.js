466561622bd8fea7d00c7e0c581e366076ec9a6e
cocoitali <cmburatto@gmail.com>
2019-01-14 21:26:20 -0700

    MVP : created database via migrations, created function for undone migration

--- data/migrations/20190114205651_students.js
+++ data/migrations/20190114205651_students.js
@@ -1,8 +1,10 @@
+exports.up = function(knex, Promise) { //make changes
+  return knex.schema.createTable('students', table => {
+    table.increments() //autoincrement
+    table.integer('count').notNullable() //integer and not null
+  })
+}
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise) { //code that undos the changes
+   return knex.schema.dropTableIfExists('students')
+}
