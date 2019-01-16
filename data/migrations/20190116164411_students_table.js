2031c0e64ed044c44186b54842c7c2002a5a9215
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-01-16 16:51:19 -0600

    migrations for the cohorts and students table done, it reflects columns on DBrowser

--- data/migrations/20190116164411_students_table.js
+++ data/migrations/20190116164411_students_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('students',
+    table => {
+      table.increments()
+      table.string('name').notNullable()
+      table.integer('cohort_id').unsigned().references('id').inTable('cohorts')
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('students')
 };
+
