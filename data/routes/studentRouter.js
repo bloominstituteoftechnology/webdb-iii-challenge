dea79487ef7c4c56db0acec902c25d18b9eb87e0
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-01-15 20:35:05 -0500

    Finished writing endpoints for student routers.

--- data/routes/studentRouter.js
+++ data/routes/studentRouter.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
   .pull()
   .then((students) => {
    res
+    .status(202)
     .json(students)
   })
   .catch(() => {
@@ -23,28 +24,62 @@ router.get('/:id', (req, res) => {
    .pullById()
    .then((student) => {
     res
+     .status(202)
      .json(student)
+   })
+   .catch(() => {
+    res
+     .status(500)
+     .json({error: "There was an error fetching student from DB."})
    })
  }
 })
 
 router.post('/', (req, res) => {
+ const student = req.body
  nodeDB
-  .place()
-  .then()
-  .catch()
+  .place(student)
+  .then((student) => {
+   res
+    .status(201)
+    .json({message: "Student was successfully added to DB."})
+  })
+  .catch(() => {
+   res 
+   .status(500)
+   .json({error: "There was an error adding student to DB."})
+  })
 })
 
 router.put('/:id', (req, res) => {
+ const { id } = req.params
+ const student = req.body
  nodeDB
-  .alter()
-  .then()
-  .catch()
+  .alter(id, student)
+  .then(() => {
+   res
+    .status(201)
+    .json({message: "Student was successfully altered in DB."})
+  })
+  .catch(() => {
+   res
+    .status(500)
+    .json({error: ""})
+  })
 })
 
 router.delete('/:id', (req, res) => {
+ const { id } = req.params
  nodeDB
-  .clear()
-  .then()
-  .catch()
+  .clear(id)
+  .then(() => {
+   res
+    .status(202)
+    .json({message: "Student was successfully cleared from DB."})
+  })
+  .catch(() => {
+   res
+    .status(500)
+    .json({error: "There was an error clearing student from DB."})
+  })
 })
