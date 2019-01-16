25b15b7b2be82c5629e91d8869914cad196e879b
ToMMaaY <thuynh629@gmail.com>
2019-01-16 14:56:57 -0800

    Hooked up the students endpoints

--- students/studentsRoutes.js
+++ students/studentsRoutes.js
@@ -12,4 +12,75 @@ route.get("/", async (req, res) => {
   }
 });
 
+route.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const student = await db("students")
+      .where({ id })
+      .first();
+    !student
+      ? res
+          .status(404)
+          .json({ error: "The student with this id does not exist." })
+      : res.json(student);
+  } catch (err) {
+    res.json({ error: "Unable to fetch the student with that id." });
+  }
+});
+
+route.post("/", async (req, res) => {
+  const student = req.body;
+
+  try {
+    await db("students").insert(student);
+    res.json({ message: "A new student has been created." });
+  } catch (err) {
+    res.json({ error: "Unable to add a new student." });
+  }
+});
+
+route.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const student = await db("students")
+      .where({ id })
+      .first();
+    !student
+      ? res
+          .status(404)
+          .json({ error: "The student with this id does not exist." })
+      : await db("students")
+          .where({ id })
+          .del();
+
+    res.json({ message: "The student has been deleted." });
+  } catch (err) {
+    res.json({ error: "Unable to delete the student." });
+  }
+});
+
+route.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  try {
+    const student = await db("students")
+      .where({ id })
+      .first();
+    !student
+      ? res
+          .status(404)
+          .json({ error: "The student with this id does not exist." })
+      : await db("students")
+          .where({ id })
+          .update(changes);
+
+    res.json({ message: "The student has been updated." });
+  } catch (err) {
+    res.json({ error: "Unable to update the student." });
+  }
+});
+
 module.exports = route;
