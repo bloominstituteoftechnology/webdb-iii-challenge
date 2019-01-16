da35bdb01097327b2b8c6ec970e068dfe416503c
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-01-16 17:51:53 -0600

    created enpoints for the cohort server route -- MVP done

--- data/routes/cohortsRoute.js
+++ data/routes/cohortsRoute.js
@@ -6,6 +6,120 @@ const dbConfig = require('../../knexfile')
 const db = knex(dbConfig.development)
 
 // endpoints
+router.post('/', (req, res) => {
+  const cohort = req.body;
+  if(cohort.name) {
+    db('cohorts').insert(cohort)
+      .then(id => {
+        res
+          .status(201)
+          .json(id)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({err: 'Failed to add Cohort'})
+      })
+  } else {
+    res
+      .status(400)
+      .json({err: 'Missing Cohort name'})
+  }
+})
 
+router.get('/', (req, res) => {
+  db('cohorts')
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({err: 'Failed to get Cohorts'})
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+  db('cohorts').where('id', id)
+    .then(row => {
+      if(row.length !==0){
+        res.json(row)
+      } else {
+        res
+          .status(404)
+          .json({err: 'No cohort under current id'})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({err: 'Failed to get Cohort'})
+    })
+})
+
+router.get('/:id/students', (req, res) => {
+  const {id} = req.params
+  db('students').where('cohort_id', id)
+    .then(rows => {
+      if(rows.length !== 0) {
+        res.json(rows)
+      } else {
+        res
+          .status(404)
+          .json({err: 'No students under current cohort id'})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({err: 'Failed to get students'})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+  db('cohorts').where('id', id).del()
+    .then(rowCount => {
+      if(rowCount !== 0) {
+        res.json(rowCount)
+      } else {
+        res
+          .status(404)
+          .json({err: 'No cohort under current id'})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({err: 'Failed to delete Cohort'})
+    })
+})
+
+router.put('/:id', (req,res) => {
+  const {id} = req.params;
+  const cohort = req.body;
+  if(cohort.name) {
+    db('cohorts').where('id', id).update(cohort)
+    .then(rowCount => {
+      if(rowCount !== 0) {
+        res.json(rowCount)
+      } else {
+        res
+          .status(404)
+          .json({err: 'No cohort under current id'})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({err: 'Failed to update Cohort'})
+    })
+  } else {
+    res
+      .status(500)
+      .json({err: 'No cohort name provided'})
+  }
+})
 
 module.exports = router;
