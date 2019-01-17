448c0049ebf3cbadf345d9090aa127c39a314c8e
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-16 21:27:16 -0800

    completed mvp with some stretches done

--- Routes/cohortsRoutes.js
+++ Routes/cohortsRoutes.js
@@ -53,12 +53,12 @@ router.get('/:id', (req, res) => {
 });
 
 // retrive students
-// this one not finished
-router.get('/students', (req, res) => {
-  // need to add way to retrive students from cohort
-  db('cohorts')
-    .then(cohorts => {
-      res.status(200).json(cohorts);
+router.get('/:id/students', (req, res) => {
+  const {id} = req.params;
+
+  db('students').where({cohort_id: id})
+    .then(students => {
+      res.status(200).json(students);
     })
     .catch(err => res.status(500).json(err));
 });
