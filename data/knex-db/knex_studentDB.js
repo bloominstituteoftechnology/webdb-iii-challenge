0a0d8f5ca2e9ee6a16cdf186460848f564ef467d
chasefulks <chase.fulks@gmail.com>
2019-01-16 18:26:50 -0600

    works

---
+++ data/knex-db/knex_studentDB.js
@@ -0,0 +1,43 @@
+const knex = require("knex")
+const config = require('./knexfile.js')
+const knexDB = knex(config.development)
+
+ const pull = () => {
+ return knexDB('students')
+}
+
+ const pullById = (id) => {
+ return knexDB('students')
+              .from('students')
+              .select('name', 'id', 'track')
+              .where({id: id})
+}
+
+ const place = (student) => {
+ return knexDB('students')
+             .insert(student)
+             .then(ids => ({id: ids[0]}))
+}
+
+ const alter = (id, student) => {
+ return knexDB('students')
+              .where({id: id})
+              .update(student)
+}
+
+ const clear = (id) => {
+ return knexDB('students')
+              .where({ id })
+              .del()
+}
+
+
+
+
+ module.exports = {
+ pull,
+ pullById,
+ place,
+ alter,
+ clear
+} 
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const helmet = require("helmet")
+const logger = require("morgan")
+const server = express()
+const cohortRouter = require("./data/routes/cohortRouter")
+const studentRouter = require("./data/routes/studentRouter")
+const port = process.env.port || 5000
+
+ server.use(
+ helmet(),
+ logger('tiny'),
+ express.json()
+)
+
+ server.use('/api/cohorts', cohortRouter)
+server.use('/api/students', studentRouter)
+
+ server.listen(port, () => {
+ console.log(`Server is running live on ${port}`)
+})
