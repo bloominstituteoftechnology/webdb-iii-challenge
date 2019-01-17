f5b2413cc381e65d97bd34e70bf7800eebe4cbe6
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-16 20:04:47 -0800

    saved changes

--- Routes/cohortsRoutes.js
+++ Routes/cohortsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const knex = require('knex');
-const knexConfig = require('./knexfile.js');
+const knexConfig = require('../knexfile.js');
 
 const router = express.Router();
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
-const cohortsRoutes = require('./cohortsRoutes.js');
-const studentsRoutes = require('./studentsRoutes.js');
+const cohortsRoutes = require('./Routes/cohortsRoutes.js');
+const studentsRoutes = require('./Routes/studentsRoutes.js');
 
 // json middleware
 server.use(express.json());
