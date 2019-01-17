0661ab41ce6824e9cbcbc2c4d88eba61d163ab6b
cocoitali <cmburatto@gmail.com>
2019-01-16 21:10:25 -0700

    MVP : created POST and GET for cohorts, tested

--- index.js
+++ index.js
@@ -7,6 +7,38 @@ const PORT = 5222
 
 server.use(express.json())
 
+server.post('/api/cohorts', (req, res) => {
+  const cohort = req.body
+  if (cohort.name) {
+    db('cohorts')
+      .insert(cohort)
+      .then(ids => {
+        res.status(201).json(ids)
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ error: 'Failed to insert the cohort into the database' })
+      })
+  } else {
+    res.status(400).json({ error: 'Please provide a name for the cohort' })
+  }
+})
+
+server.get('/api/cohorts', (req, res) => {
+  db('cohorts')
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({
+          error:
+            'Information for this table could not be retrieved from the database.' })
+    })
+})
+
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+  console.log(`Listening on port ${PORT}`)
 })
