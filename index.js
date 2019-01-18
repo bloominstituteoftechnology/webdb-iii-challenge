8c1d14808dd1dcf5e267a1ab15ab3fb6f7af702a
cocoitali <cmburatto@gmail.com>
2019-01-17 19:40:30 -0700

    STRETCH : basic CRUD endpoints, need to add cohort name and remove cohort id from GET

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ server.post('/api/cohorts', (req, res) => {
   }
 })
 
+
 server.get('/api/cohorts', (req, res) => {
   db('cohorts')
     .then(rows => {
@@ -49,7 +50,7 @@ server.get('/api/cohorts/:id', (req, res) => {
         .catch(() => {
             res
                 .status(500)
-                .json({ error: 'Failed to find a zohort with this id in the database.'})
+                .json({ error: 'Failed to find a cohort with this id in the database.'})
         })
 })
 
@@ -75,7 +76,7 @@ server.put('/api/cohorts/:id', (req, res) => {
 
 server.delete('/api/cohorts/:id', (req, res) => {
     const { id } = req.params
-    db('cohorts').where({ id }).del()
+    db('cohorts').where({ id }).del() //also can use (id : id) or ('id', id)
     .then(count => {
         if (count) {
             res.json({ message: 'The cohort was successfully deleted from the database.'})
@@ -87,6 +88,85 @@ server.delete('/api/cohorts/:id', (req, res) => {
         res.status(500).json({ error: 'The cohort could not be removed from the database.'})
     })
 })
+
+//students endpoints
+
+server.post('/api/students', (req, res) => {
+    const student = req.body
+    if (student.name && student.cohort_id) {
+      db('students')
+        .insert(student)
+        .then(ids => {
+          res.status(201).json(ids)
+        })
+        .catch(() => {
+          res
+            .status(500)
+            .json({ error: 'Failed to insert the student into the database' })
+        })
+    } else {
+      res.status(400).json({ error: 'Please provide a name and cohort id for the student' })
+    }
+  })
+  
+  server.get('/api/students', (req, res) => {
+    db('students')
+      .then(rows => {
+        res.json(rows)
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({
+            error:
+              'Information for this table could not be retrieved from the database.' })
+      })
+  })
+  
+  server.get('/api/students/:id', (req, res) => {
+      const { id } = req.params
+      db('students')
+          .where('id', id)
+          .then(rows => {
+              res.json(rows)
+          })
+          .catch(() => {
+              res
+                  .status(500)
+                  .json({ error: 'Failed to find a student with this id in the database.'})
+          })
+  })
+  
+  server.put('/api/students/:id', (req, res) => {
+      const { id } = req.params
+      const student = req.body
+      db('students')
+          .where('id', id)
+          .update(student)
+          .then(rowCount => {
+              res.status(200).json(rowCount)
+          })
+          .catch(() => {
+              res
+                  .status(500)
+                  .json({ error: 'Failed to update informtation about this student.'})
+          })
+  })
+  
+  server.delete('/api/students/:id', (req, res) => {
+      const { id } = req.params
+      db('students').where({ id }).del() //also can use (id : id) or ('id', id)
+      .then(count => {
+          if (count) {
+              res.json({ message: 'The student was successfully deleted from the database.'})
+          } else {
+              res.status(404).json({ error: 'The student with the specified id does not exist in the database.'})
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ error: 'The student could not be removed from the database.'})
+      })
+  })
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
