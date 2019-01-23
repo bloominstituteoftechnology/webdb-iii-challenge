6752a8063c428e85fadb6da83fdebec39c251bc0
CJ <CJ@CJs-MacBook.local>
2019-01-23 16:09:58 -0600

    put endpoints added

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-const express = require('express');
-const knex = require('knex');
-const knexConfig = require('./knexfile');
+const express = require("express");
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-const db = knex(knexConfig.development)
+const db = knex(knexConfig.development);
 
-
-
-server.post('/api/cohorts',(req, res) =>{
-    db('cohorts')
+server.post("/api/cohorts", (req, res) => {
+  db("cohorts")
     .insert(req.body)
     .then(ids => {
-      db('cohorts')
+      db("cohorts")
         .where({ id: ids[0] })
         .then(cohort => {
           res.status(201).json(cohort);
         });
     })
-    .catch(err => res.status(500).json(err))
-})
+    .catch(err => res.status(500).json(err));
+});
 
+server.get("/api/cohorts", (req, res) => {
+  db("cohorts")
+    .then(results => {
+      res.status(200).json(results);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
+server.get("/api/cohorts/:id", (req, res) => {
+  db("cohorts")
+    .where({ id: req.params.id })
+    .then(results => {
+      if (results) {
+        res.status(200).json(results);
+      } else {
+        res.status(404).json({ message: "Cohort not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
-server.listen(5000, () => console.log('up and at em!'));
+server.put("/api/cohorts/:id", (req, res) => {
+  const edit = req.body;
+  db('cohorts')
+    .where({ id: req.params.id })
+    .update(edit)
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: "Cohort not found" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
 
-
+server.listen(5000, () => console.log("up and at em!"));
