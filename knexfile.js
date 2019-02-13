f41b7c0645d00ee9b81bc533434690a39291104b
Jesse Theisen <theiseja@gmail.com>
2019-02-13 13:11:51 -0600

    MVP: Added knex

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,44 @@
 // Update with your config settings.
 
 module.exports = {
+
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/lambda.sqlite3'
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
-      directory: './data/migrations'
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    seeds: {
-      directory: './data/seeds'
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   }
-}
 
+};
