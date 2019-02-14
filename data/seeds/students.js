6074899eb12f93eda3959cb47035732144badd59
Jesse Theisen <theiseja@gmail.com>
2019-02-13 20:36:23 -0600

    MVP: Added seeds

--- data/seeds/students.js
+++ data/seeds/students.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('students').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('students').insert([
+        {name: 'Jesse', cohort_id: 1},
+        {name: 'Alex', cohort_id: 2},
+        {name: 'Oliver', cohort_id: 3},
+        {name: 'Marguel', cohort_id: 4},
+        {name: 'Kelly', cohort_id: 5},
+        {name: 'Lorenzo', cohort_id: 6}
       ]);
     });
 };
