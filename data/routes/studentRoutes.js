cf8ea3747cfdc9ee392bd0f6bb51c87eb04e7b47
Jesse Theisen <theiseja@gmail.com>
2019-02-16 13:39:01 -0600

    MVP: MVP & Stretch complete and tested

--- data/routes/studentRoutes.js
+++ data/routes/studentRoutes.js
@@ -69,5 +69,47 @@ router.get("/:id/students", (req, res) => {
     })
 });
 
+router.put('/:id', (req,res) => {
+    const {id} = req.params;
+    const studentData = req.body;
+  
+    if( studentData.name && studentData.cohort_id ){
+      db('students').where('id', id).update(studentData)
+        .then( (rowCount) => {
+          res.json(rowCount);
+        })
+        .catch( (err) => {
+          res.status(500).json({ error: "Could not update student." });
+        });
+      // end-db
+    } else {
+      res.status(400).json({ error: "Please provide the name & cohort of the student."});
+    }
+  });
+
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    db('students')
+      .where({ id })
+      .del() 
+      .then(count => {
+        if (count) {
+          res.json({
+            message: 'The student was successfully deleted from the database.'
+          })
+        } else {
+          res.status(404).json({
+            error:
+              'The student with the specified id does not exist in the database.'
+          })
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: 'The student could not be removed from the database.' })
+      })
+  })
+
 
 module.exports = router;
