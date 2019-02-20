b29dd753b5713f37f6811d145a510f9eae2779e0
john2796 <jbmiranda22796@gmail.com>
2019-02-20 13:48:50 -0800

    gs

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,4 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile");
-let environment = process.env.ENV || "development";
+const environment = process.env.ENV || "production";
 module.exports = knex(knexConfig[environment]);
