df5688f0fdbf7197c1dda16dd6320d4d1a2f2bcb
john2796 <jbmiranda22796@gmail.com>
2019-02-20 15:05:52 -0800

    dev

--- knexfile.js
+++ knexfile.js
@@ -16,22 +16,6 @@ module.exports = {
       directory: "./data/seeds"
     }
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
