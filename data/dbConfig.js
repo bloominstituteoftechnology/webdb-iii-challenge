ad0ff5e29feaf670f46ed6fa5d053a8b0812f7b9
john2796 <jbmiranda22796@gmail.com>
2019-02-21 09:16:50 -0800

    ss

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,7 +2,6 @@ const knex = require("knex");
 const paginator = require("knex-paginator");
 const knexConfig = require("../knexfile");
 const environment = process.env.ENV || "development";
-console.log(environment);
-const db = knex(knexConfig.development);
+const db = knex(knexConfig[environment]);
 paginator(db);
 module.exports = db;
