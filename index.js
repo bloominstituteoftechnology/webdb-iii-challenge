7778b5e27b2ef389305675df6b40581ba64302a1
Taylorbelk1 <belktaylor12@gmail.com>
2019-03-19 22:52:49 -0500

    completed first half of stretch

--- index.js
+++ index.js
@@ -100,6 +100,81 @@ server.delete('/api/cohorts/:id', async (req, res) => {
   });
 
 
+  //STRETCH
+
+  //STUDENTS
+    //[POST] /students This route should save a new student to the database.
+    server.post('/api/students', async (req, res) => {
+        try {
+          const [id] = await db('students').insert(req.body);
+      
+          const students = await db('students')
+            .where({ id })
+            .first();
+      
+          res.status(201).json(students);
+        } catch (error) {
+          res.status(500).json(error);
+        }
+      });
+
+    //[GET] /students This route will return an array of all students.
+    server.get('/api/students', async (req, res) => {
+        // get the cohorts from the database
+        try {
+          const students = await db('students'); // all the records from the table
+          res.status(200).json(students);
+        } catch (error) {
+          res.status(500).json(error);
+        }
+      });
+
+    //[GET] /students/:id This route will return the student with the matching id.
+    server.get('/api/students/:id', async (req, res) => {
+        // get the roles from the database
+        try {
+          const students = await db('students')
+            .where({ id: req.params.id })
+            .first();
+          res.status(200).json(students);
+        } catch (error) {
+          res.status(500).json(error);
+        }
+      });
+    //[PUT] /students/:id This route will update the student with the matching id using information sent in the body of the request.
+    server.put('/api/students/:id', async (req, res) => {
+        try {
+          const count = await db('students')
+            .where({ id: req.params.id })
+            .update(req.body);
+      
+          if (count > 0) {
+            const students = await db('students')
+              .where({ id: req.params.id })
+              .first();
+      
+            res.status(200).json(students);
+          } else {
+            res.status(404).json({ message: 'Records not found' });
+          }
+        } catch (error) {}
+      });
+
+    //[DELETE] /students/:id This route should delete the specified student.
+    server.delete('/api/students/:id', async (req, res) => {
+        try {
+          const count = await db('students')
+            .where({ id: req.params.id })
+            .del();
+      
+          if (count > 0) {
+            res.status(204).json(count);
+          } else {
+            res.status(404).json({ message: 'Records not found' });
+          }
+        } catch (error) {}
+      });
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
