f11018d9667ba924ba28e6c0d41cc42469e39761
Kseniya Platonava <ksu2008@gmail.com>
2019-03-19 22:52:42 -0400

    Add GET cohorts

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+// list all roles
+server.get('/api/cohorts', async (req, res) => {
+  // get the roles from the database
+  try {
+    const cohorts = await db('cohorts'); // all the records from the table
+    res.status(200).json(cohorts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+
+
 
 const port = process.env.PORT || 9090;
 server.listen(port, () => console.log(`\nrunning on ${port}\n`));
