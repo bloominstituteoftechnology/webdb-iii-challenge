c74fb9809dea46c8acaaaae58d6203a17d44cc4e
Kevin Smith <keveightysev@gmail.com>
2019-03-27 13:45:27 -0500

    Set up basic server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () =>
+	console.log(`\n*** Server running at http://localhost:${port} ***\n`),
+);
