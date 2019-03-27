4f9eb5a010983e4fe3894956e3d28929278dae8d
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-27 16:49:07 -0400

    Files for cohort and student routers added and imported to server.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
+const cohortRouter = require("./cohortRouter.js");
+const studentRouter = require("./studentRouter.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use("/api/cohorts", cohortRouter);
+server.use("/api/students", studentRouter);
 
 module.exports = server;
