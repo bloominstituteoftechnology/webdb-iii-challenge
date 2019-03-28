a3e0ee6dcca07a318a39b935c98574cd41e225e1
Anthony Venturini <anthonydventurini@gmail.com>
2019-03-28 12:44:29 -0700

    adds cohorts table through knex migrate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const knex = require('knex');
 const knexConfig = {
     client: 'sqlite3',
     connection: {
-        filename: '.database/lambda.sqlite3'
+        filename: './data/lambda.sqlite3'
     },
     useNullAsDefault: true
 }
@@ -17,3 +17,7 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+  console.log(`\n** API running on http://localhost:${port} **\n`)
+);
