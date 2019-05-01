e54670062a330172e40a271b411c9231b43755dc
Alexnadria Supriana <alexa9410@gmail.com>
2019-05-01 18:49:43 -0400

    almost there

---
+++ cohort/cohort-router.js
@@ -0,0 +1,121 @@
+const router = require("express").Router();
+
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  }
+};
+
+const db = knex(knexConfig);
+
+//check
+router.get("/", (req, res) => {
+  db("cohorts")
+    .then(cohorts => {
+      res.status(200).json(cohorts);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+//check
+router.get("/:id", (req, res) => {
+  db("cohorts")
+    .where({ id: req.params.id })
+    .first()
+    .then(cohort => {
+      if (cohort) {
+        res.status(200).json(cohort);
+      } else {
+        res.status(404).json({ message: "Cohort was not found!" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const [id] = await db("cohorts").insert(req.body);
+    const cohort = await db("cohorts")
+      .where({ id })
+      .first();
+    res.status(201).json(cohort);
+  } catch (error) {
+    res.status(500).json({ error: "There was an error posting that!" });
+  }
+});
+
+//check
+router.put("/:id", (req, res) => {
+  db("cohorts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        db("cohorts")
+          .where({ id: req.params.id })
+          .first()
+          .then(cohort => {
+            res.status(200).json(cohort);
+          });
+      } else {
+        res
+          .status(404)
+          .json({ message: "Cohort information was not updated! " });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//check
+router.delete("/:id", (req, res) => {
+  db("cohorts")
+    .where({ id: req.params.id })
+    .del(req.body)
+    .then(count => {
+      if (count > 0) {
+        db("cohorts")
+          .where({ id: req.params.id })
+          .first()
+          .then(cohort => {
+            res.status(200).json(cohort);
+          });
+      } else {
+        res.status(404).json({ message: "Cohort was not found! " });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+//check
+router.get("/:id/students", (req, res) => {
+  const id = req.params.id;
+  db("cohorts")
+    .join("students", "students.cohort_id", "cohorts.id")
+    .select("students.id", "students.name")
+    .where("cohorts.id", id)
+    .first()
+    .then(stu => {
+      if (stu) {
+        res.status(200).json(stu);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No students were found, please try again" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+module.exports = router;
